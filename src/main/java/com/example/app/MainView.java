fe079cd92a11eef6a6656f1d2c45bf3256283dc7
Tomi Virkki <virkki@vaadin.com>
2020-01-21 14:49:10 +0200

    Init as Vaadin app

---
+++ src/main/java/com/example/app/MainView.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(button);
+    }
+}
