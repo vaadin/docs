c6488aac6b16f559db55b8560deb6c73b445b5dc
Tomi Virkki <virkki@vaadin.com>
2020-01-29 18:39:09 +0200

    Remove syso

--- src/main/java/com/vaadin/demo/component/grid/GridEditorExporter.java
+++ src/main/java/com/vaadin/demo/component/grid/GridEditorExporter.java
@@ -6,11 +6,10 @@ import com.vaadin.flow.component.webcomponent.WebComponent;
 public class GridEditorExporter extends WebComponentExporter<GridEditor> {
   public GridEditorExporter() {
     super("grid-editor");
-    System.out.println("regristered");
   }
 
   @Override
   protected void configureInstance(WebComponent<GridEditor> webComponent, GridEditor demo) {
-    System.out.println("demo:"+ demo);
+
   }
 }
