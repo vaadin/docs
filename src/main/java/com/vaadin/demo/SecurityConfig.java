b9f18905054633b20bda868a36c34d57105bedf5
Tomi Virkki <virkki@vaadin.com>
2020-02-17 11:43:28 +0200

    Disable csrf

--- src/main/java/com/vaadin/demo/SecurityConfig.java
+++ src/main/java/com/vaadin/demo/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.vaadin.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,13 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private Environment environment;
-
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    if (environment.getActiveProfiles().length > 0 && "dev".equals(environment.getActiveProfiles()[0])) {
-      http.csrf().disable();
-    }
+    http.csrf().disable();
   }
 }
