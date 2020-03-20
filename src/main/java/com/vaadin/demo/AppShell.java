1b3ddd921328733cac75f86fae1eb46d346d780a
Tomi Virkki <virkki@vaadin.com>
2020-03-20 09:38:23 +0200

    Remove unused files

--- src/main/java/com/vaadin/demo/AppShell.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.demo;
-
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
-
-@PWA(name = "Docs", shortName = "docs")
-public class AppShell implements AppShellConfigurator {
-}
