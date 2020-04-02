3d35aad1f011b608351767abde504692887e5f57
Tomi Virkki <virkki@vaadin.com>
2020-04-02 10:57:16 +0300

    Add accordion example

---
+++ frontend/demo/component/accordion/accordion-basic.ts
@@ -0,0 +1,43 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement } from 'lit-element';
+import '@vaadin/vaadin-accordion/vaadin-accordion';
+import '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout';
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+
+// tag::snippet[]
+export class Example extends LitElement {
+  render() {
+    return html`
+      <vaadin-accordion>
+        <vaadin-accordion-panel>
+          <div slot="summary">Personal Information</div>
+
+          <vaadin-vertical-layout theme="padding spacing">
+            <vaadin-text-field label="Name"></vaadin-text-field>
+            <vaadin-text-field label="Phone"></vaadin-text-field>
+            <vaadin-text-field label="Email"></vaadin-text-field>
+          </vaadin-vertical-layout>
+        </vaadin-accordion-panel>
+
+        <vaadin-accordion-panel>
+          <div slot="summary">Billing Address</div>
+
+          <vaadin-vertical-layout theme="padding spacing">
+            <vaadin-text-field label="Address"></vaadin-text-field>
+            <vaadin-text-field label="City"></vaadin-text-field>
+            <vaadin-text-field label="State"></vaadin-text-field>
+            <vaadin-text-field label="Zip Code"></vaadin-text-field>
+          </vaadin-vertical-layout>
+        </vaadin-accordion-panel>
+
+        <vaadin-accordion-panel disabled>
+          <div slot="summary">Payment</div>
+
+          <span>Not yet implemented</span>
+        </vaadin-accordion-panel>
+      </vaadin-accordion>
+    `;
+  }
+}
+// end::snippet[]
