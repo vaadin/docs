b517aa6ceaf92a84f45273ff8d86d6b61abcb5b9
Tomi Virkki <virkki@vaadin.com>
2020-04-04 17:08:55 +0300

    Add confirm dialog example

---
+++ frontend/demo/component/confirmdialog/confirm-dialog-basic.ts
@@ -0,0 +1,40 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement, property, customElement } from 'lit-element';
+import '@vaadin/vaadin-confirm-dialog/vaadin-confirm-dialog';
+
+// tag::snippet[]
+@customElement('confirm-dialog-basic')
+export class Example extends LitElement {
+  @property() dialogOpened = false;
+
+  @property() status = '';
+
+  render() {
+    return html`
+      <vaadin-button @click=${() => (this.dialogOpened = true)}
+        >Open dialog</vaadin-button
+      >
+
+      <vaadin-confirm-dialog
+        header="Meeting starting"
+        confirm-text="OK"
+        .opened=${this.dialogOpened}
+        @opened-changed=${this.openedChanged}
+        @confirm=${() => (this.status = 'Confirmed')}
+      >
+        Your next meeting starts in 5 minutes
+      </vaadin-confirm-dialog>
+
+      <span>${this.status}</span>
+    `;
+  }
+
+  openedChanged(e: CustomEvent) {
+    this.dialogOpened = e.detail.value;
+    if (this.dialogOpened) {
+      this.status = '';
+    }
+  }
+}
+// end::snippet[]
