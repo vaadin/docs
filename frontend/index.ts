f1c55d36fcb6e5cc9faa9dae63584d54d153a8cc
Tomi Virkki <virkki@vaadin.com>
2020-06-05 12:42:14 +0300

    Bump Vaadin to 16 stable. Dedicated package.json for scripts.

--- frontend/index.ts
+++ frontend/index.ts
@@ -3,10 +3,8 @@ import { Router } from '@vaadin/router';
 
 import './main-layout';
 
-import './demo/dashboard-view';
-
 const { serverSideRoutes } = new Flow({
-  imports: () => import('../target/frontend/generated-flow-imports')
+  imports: () => import('../target/frontend/generated-flow-imports'),
 });
 
 const routes = [
@@ -14,12 +12,10 @@ const routes = [
     path: '',
     component: 'main-layout',
     children: [
-      { path: '', component: 'dashboard-view' },
-      { path: 'Dashboard', component: 'dashboard-view' },
       // fallback to server-side Flow routes if no client-side route matches
-      ...serverSideRoutes
-    ]
-  }
+      ...serverSideRoutes,
+    ],
+  },
 ];
 
 const router = new Router(document.querySelector('#outlet'));
