4ec008772be02f5edd23090a50d5ec612d49b000
Jouni Koivuviita <jouni@vaadin.com>
2020-06-16 20:15:43 +0300

    Preliminary documentation for Lumo colors and typography

---
+++ frontend/demo/design-tokens.ts
@@ -0,0 +1,5 @@
+import '@vaadin/vaadin-lumo-styles/color';
+import '@vaadin/vaadin-lumo-styles/typography';
+import '@vaadin/vaadin-lumo-styles/sizing';
+import '@vaadin/vaadin-lumo-styles/spacing';
+import '@vaadin/vaadin-lumo-styles/style';
