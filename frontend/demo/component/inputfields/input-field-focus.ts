39fc3a8017f2c225b561546cc04278883adf48b0
Tomi Virkki <virkki@vaadin.com>
2020-10-05 15:30:15 +0300

    Update input field focus example

--- frontend/demo/component/inputfields/input-field-focus.ts
+++ frontend/demo/component/inputfields/input-field-focus.ts
@@ -8,21 +8,21 @@ import '@vaadin/vaadin-text-field';
 @customElement('input-field-focus')
 export class Example extends LitElement {
   steps = [
-    { minWidth: 0, columns: 1 },
+    { minWidth: '0', columns: 1 },
     { minWidth: '30em', columns: 2 }
   ];
 
   render() {
     return html`
-      <!-- tag::snippet[] -->
       <vaadin-form-layout .responsiveSteps="${this.steps}">
+        <!-- tag::snippet[] -->
         <vaadin-text-field label="First name"></vaadin-text-field>
         <vaadin-text-field label="Last name"></vaadin-text-field>
         <vaadin-text-field label="Username" colspan="2"></vaadin-text-field>
         <vaadin-password-field label="Password"></vaadin-password-field>
         <vaadin-password-field label="Confirm password"></vaadin-password-field>
+        <!-- end::snippet[] -->
       </vaadin-form-layout>
-      <!-- end::snippet[] -->
     `;
   }
 }
