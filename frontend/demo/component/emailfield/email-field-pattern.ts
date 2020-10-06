aabc320f3d375047ccdbaef0f87e23d13361382a
Tomi Virkki <virkki@vaadin.com>
2020-10-06 12:12:21 +0300

    Cleanup

--- frontend/demo/component/emailfield/email-field-pattern.ts
+++ frontend/demo/component/emailfield/email-field-pattern.ts
@@ -1,13 +1,10 @@
 import '../../init'; // hidden-full-source-line
 
-import { html, LitElement, customElement, query } from 'lit-element';
-import { EmailFieldElement } from '@vaadin/vaadin-text-field/vaadin-email-field';
+import { html, LitElement, customElement } from 'lit-element';
 import '@vaadin/vaadin-text-field/vaadin-email-field';
 
 @customElement('email-field-pattern')
 export class Example extends LitElement {
-  @query('[name="example-email"]') exampleEmail!: EmailFieldElement;
-
   render() {
     return html`
       <!-- tag::snippet[] -->
@@ -17,14 +14,9 @@ export class Example extends LitElement {
         placeholder="username@example.com"
         error-message="Please enter a valid example.com email address"
         clear-button-visible
+        pattern="^.+@example\\.com$"
       ></vaadin-email-field>
       <!-- end::snippet[] -->
     `;
-  }
-
-  firstUpdated() {
-    if (this.exampleEmail) {
-      this.exampleEmail.pattern = '^.+@example\\.com$';
-    }
   }
 }
