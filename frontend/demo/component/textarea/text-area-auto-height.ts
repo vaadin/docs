8ad0fc3111e8a18d64dcdaa14c12382cdf7c142c
Jouni Koivuviita <jouni@vaadin.com>
2020-10-23 10:31:01 +0300

    Clean up Text Area examples

--- frontend/demo/component/textarea/text-area-auto-height.ts
+++ frontend/demo/component/textarea/text-area-auto-height.ts
@@ -1,22 +1,22 @@
 import '../../init'; // hidden-full-source-line
 
-import { html, LitElement, customElement } from 'lit-element';
+import { html, LitElement, customElement, css } from 'lit-element';
 import { sampleText } from './text-area-sample-text';
 import '@vaadin/vaadin-text-field/vaadin-text-area';
 
-@customElement('text-area-height')
+@customElement('text-area-basic')
 export class Example extends LitElement {
+  static get styles() {
+    return css`
+      vaadin-text-area {
+        width: 100%;
+      }
+    `;
+  }
+
   render() {
     return html`
       <!-- tag::snippet[] -->
-      <style>
-        vaadin-text-area {
-          width: 100%;
-          min-height: 100px;
-          max-height: 150px;
-        }
-      </style>
-
       <vaadin-text-area label="Description" value="${sampleText}"></vaadin-text-area>
       <!-- end::snippet[] -->
     `;
