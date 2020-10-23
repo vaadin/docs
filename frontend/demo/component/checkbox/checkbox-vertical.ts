229c42b03bacfdfad9f243a5a2038622521fa39a
Ibrahim Alkali <ibrahim@vaadin.com>
2020-10-23 11:53:48 +0300

    Add TS example file stubs

---
+++ frontend/demo/component/checkbox/checkbox-vertical.ts
@@ -0,0 +1,13 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-checkbox/vaadin-checkbox';
+
+// tag::snippet[]
+@customElement('checkbox-vertical')
+export class Example extends LitElement {
+  render() {
+    return html`<vaadin-checkbox>Enabled</vaadin-checkbox>`;
+  }
+}
+// end::snippet[]
