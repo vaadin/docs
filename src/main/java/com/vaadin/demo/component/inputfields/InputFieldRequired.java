af30f024b3e054f1920136182093fda9a75d8eea
Tomi Virkki <virkki@vaadin.com>
2020-10-28 16:56:12 +0200

    Add more input fields Java examples

---
+++ src/main/java/com/vaadin/demo/component/inputfields/InputFieldRequired.java
@@ -0,0 +1,29 @@
+package com.vaadin.demo.component.inputfields;
+
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.demo.DemoExporter; // hidden-full-source-line
+
+@Route("input-field-required")
+public class InputFieldRequired extends HorizontalLayout {
+
+  public InputFieldRequired() {
+    // tag::snippet[]
+    TextField textField = new TextField();;
+    textField.setLabel("Name");
+    textField.setRequiredIndicatorVisible(true);
+    textField.setErrorMessage("This field is required");
+
+    DatePicker datePicker = new DatePicker();
+    datePicker.setLabel("Date of birth");
+
+    add(textField, datePicker);
+    // end::snippet[]
+  }
+
+  public static class Exporter extends DemoExporter<InputFieldRequired> { // hidden-full-source-line
+  } // hidden-full-source-line
+}
