20f3dd07b12b39eb012d3202e20486aaef9a1bf6
Tomi Virkki <virkki@vaadin.com>
2020-10-29 14:03:58 +0200

    Add combo-box custom presentation TS example

--- frontend/demo/component/combobox/combo-box-presentation.ts
+++ frontend/demo/component/combobox/combo-box-presentation.ts
@@ -3,41 +3,63 @@ import '@vaadin/flow-frontend/comboBoxConnector'; // hidden-full-source-line
 
 import { html, LitElement, customElement, property } from 'lit-element';
 import '@vaadin/vaadin-combo-box/vaadin-combo-box';
-import country from '../../../../src/main/resources/data/countries.json';
+import { Person } from '../../domain/Person';
+import { getPeople } from '../../domain/DataService';
+import { ComboBoxItemModel } from '@vaadin/vaadin-combo-box/vaadin-combo-box';
+import { render } from 'lit-html';
 
 // tag::snippet[]
 @customElement('combo-box-presentation')
 export class Example extends LitElement {
-  @property() items = country;
+  @property({ type: Array })
+  private allItems: Person[] = [];
+
+  @property({ type: Array })
+  private filteredItems: Person[] = [];
+
+  async firstUpdated() {
+    this.allItems = this.filteredItems = (await getPeople()).map((person) => {
+      return {
+        ...person,
+        displayName: `${person.firstName} ${person.lastName}`
+      };
+    });
+  }
 
   render() {
     return html`
       <vaadin-combo-box
         label="Choose doctor"
-        item-label-path="country"
-        item-value-path="id"
-        .items=${this.items}
-        style="width:100%"
-      >
-        <template>
-          <style>
-            :host {
-              --vaadin-combo-box-overlay-width: 400px;
-            }
-            .avatar {
-              width: 40px;
-              border-radius: 100%;
-              float: left;
-              margin-right: 10px;
-            }
-          </style>
-          <img src="https://randomuser.me/api/portraits/women/43.jpg" class="avatar" />
-          <b>[[item.country]]</b>
-          <br />
-          [[item.abbreviation]]
-        </template>
-      </vaadin-combo-box>
+        .itemLabelPath=${'displayName'}
+        .filteredItems=${this.filteredItems}
+        .renderer=${this.renderer}
+        style="--vaadin-combo-box-overlay-width: 250px"
+        @filter-changed=${this.filterChanged}
+      ></vaadin-combo-box>
     `;
+  }
+
+  private filterChanged(e: CustomEvent) {
+    const filter = e.detail.value as string;
+    this.filteredItems = this.allItems.filter(({ firstName, lastName, profession }) => {
+      return `${firstName} ${lastName} ${profession}`.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  private renderer(root: HTMLElement, _: HTMLElement, { item }: ComboBoxItemModel) {
+    const person = item as Person;
+    render(
+      html`
+        <div style="display: flex;">
+          <img style="height: 2em" src=${person.pictureUrl} alt="User avatar" />
+          <div>
+            ${person.firstName} ${person.lastName}
+            <div>${person.profession}</div>
+          </div>
+        </div>
+      `,
+      root
+    );
   }
 }
 // end::snippet[]
