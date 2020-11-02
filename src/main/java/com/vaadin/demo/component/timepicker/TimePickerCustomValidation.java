cfa0fca7f8fc89ecfdeaeacbd48d7abce6df8609
Ibrahim Alkali <alkalibrahim@gmail.com>
2020-11-02 08:50:32 +0200

    Write timepicker text (#33)

---
+++ src/main/java/com/vaadin/demo/component/timepicker/TimePickerCustomValidation.java
@@ -0,0 +1,20 @@
+package com.vaadin.demo.component.timepicker;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.router.Route;
+import com.vaadin.demo.DemoExporter; // hidden-full-source-line
+
+@Route("time-picker-custom-validation")
+public class TimePickerCustomValidation extends Div {
+
+    public TimePickerCustomValidation() {
+        // tag::snippet[]
+        TimePicker timePicker = new TimePicker();
+        add(timePicker);
+        // end::snippet[]
+    }
+
+    public static class Exporter extends DemoExporter<TimePickerCustomValidation> { // hidden-full-source-line
+    } // hidden-full-source-line
+}
