c4c2f195295441beb83ba4f741f2bf31391e59ba
Jouni Koivuviita <jouni@vaadin.com>
2020-11-03 14:22:04 +0200

    Align time-picker-auto-open TS example with Java example

--- frontend/demo/component/timepicker/time-picker-auto-open.ts
+++ frontend/demo/component/timepicker/time-picker-auto-open.ts
@@ -10,7 +10,7 @@ export class Example extends LitElement {
   render() {
     return html`
       <!-- tag::snippet[] -->
-      <vaadin-time-picker label="Alarm" value="05:30" auto-open-disabled></vaadin-time-picker>
+      <vaadin-time-picker label="Alarm" value="05:30" .step=${60 * 30} auto-open-disabled></vaadin-time-picker>
       <!-- end::snippet[] -->
     `;
   }
