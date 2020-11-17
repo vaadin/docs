ed15c35e6ec9c2b33b44d9aa7f0c8ce031170799
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2020-11-17 17:33:53 +0200

    Update selectPresntation java example

--- src/main/java/com/vaadin/demo/component/select/SelectPresentation.java
+++ src/main/java/com/vaadin/demo/component/select/SelectPresentation.java
@@ -4,7 +4,10 @@ import com.vaadin.demo.DemoExporter;
 import com.vaadin.demo.domain.DataService;
 import com.vaadin.demo.domain.Person;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
 import java.util.List;
@@ -18,7 +21,32 @@ public class SelectPresentation extends Div {
     // tag::snippet[]
     Select<Person> select = new Select<>();
     select.setLabel("Choose doctor");
-    select.setItemLabelGenerator(doctor -> doctor.getFirstName() + " " + doctor.getLastName());
+    select.setRenderer(new ComponentRenderer<>(person -> {
+        FlexLayout wrapper = new FlexLayout();
+
+        // NOTE
+        // We are using inline styles here to keep the example simple.
+        // We recommend placing CSS in a separate style sheet and to
+        // encapsulating the styling in a new component.
+
+        Image image = new Image();
+        image.setSrc(person.getPictureUrl());
+        image.setAlt("Portrait of " + person.getFirstName() + " " + person.getLastName());
+        image.setWidth("var(--lumo-size-m)");
+        image.getStyle().set("margin-right", "var(--lumo-space-s)");
+
+        Div info = new Div();
+        info.setText(person.getFirstName() + " " + person.getLastName());
+
+        Div profession = new Div();
+        profession.setText(person.getProfession());
+        profession.getStyle().set("font-size", "var(--lumo-font-size-s)");
+        profession.getStyle().set("color", "var(--lumo-secondary-text-color)");
+        info.add(profession);
+
+        wrapper.add(image, info);
+        return wrapper;
+    }));
     select.setItems(items);
     add(select);
     // end::snippet[]
