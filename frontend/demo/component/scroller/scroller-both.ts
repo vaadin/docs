130301741a4a695c824da1053c4e28a8be542a9d
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2020-11-19 16:59:23 +0200

    Use local image

--- frontend/demo/component/scroller/scroller-both.ts
+++ frontend/demo/component/scroller/scroller-both.ts
@@ -12,14 +12,22 @@ import '@vaadin/vaadin-lumo-styles/icons';
 
 @customElement('scroller-basic')
 export class Example extends LitElement {
+  constructor() {
+    super();
+
+    import('../../../../src/main/resources/images/scroller-both-img.png').then(res => {
+      const imageElement = this.shadowRoot && this.shadowRoot.querySelector('img');
+      imageElement && (imageElement.src = res.default);
+    });
+  }
 
   render() {
     return html`
-        <!-- tag::snippet[] -->
-        <vaadin-scroller style="height: 300px; width: 300px;">
-          <img src="https://cdn2.hubspot.net/hubfs/1840687/Pages/index/taskmob-laptop-mockup.png" alt="Application">
-        </vaadin-scroller>
-        <!-- end::snippet[] -->
+      <!-- tag::snippet[] -->
+      <vaadin-scroller style="height: 300px; width: 300px;">
+        <img alt="Application" />
+      </vaadin-scroller>
+      <!-- end::snippet[] -->
     `;
   }
 }
--- src/main/java/com/vaadin/demo/component/scroller/ScrollerBoth.java
+++ src/main/java/com/vaadin/demo/component/scroller/ScrollerBoth.java
@@ -5,6 +5,12 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 
 @Route("scroller-both")
 public class ScrollerBoth extends Div {
@@ -14,14 +20,27 @@ public class ScrollerBoth extends Div {
     Scroller scroller = new Scroller();
     scroller.setWidth("300px");
     scroller.setHeight("300px");
-    scroller.getStyle().set("padding", "var(--lumo-space-m)");
 
-    Image img = new Image("https://cdn2.hubspot.net/hubfs/1840687/Pages/index/taskmob-laptop-mockup.png", "Application");
+    StreamResource imageResource = new StreamResource("scroller-both-img+.png",
+            () -> getFileStream("src/main/resources/images/scroller-both-img.png"));
+
+    Image img = new Image(imageResource, "Application");
     scroller.setContent(img);
 
     add(scroller);
+
     // end::snippet[]
   }
+
+  // tag::filestream[]
+    private InputStream getFileStream(String filePath) {
+      try {
+        return new FileInputStream(filePath);
+      } catch (IOException error) {
+        throw new UncheckedIOException(error);
+      }
+    }
+  // end::filestream[]
 
   public static class Exporter extends DemoExporter<ScrollerBoth> { // hidden-full-source-line
   } // hidden-full-source-line
