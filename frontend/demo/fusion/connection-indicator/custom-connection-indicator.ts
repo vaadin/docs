b6af2e15f516e699c0cddfd6bbd506bd862bae6a
Johannes Eriksson <joheriks@vaadin.com>
2020-12-29 10:54:13 +0200

    doc: improved connection indicator documentation

--- frontend/demo/fusion/connection-indicator/custom-connection-indicator.ts
+++ frontend/demo/fusion/connection-indicator/custom-connection-indicator.ts
@@ -1,35 +1,16 @@
 // ...
 import { connectionIndicator } from '@vaadin/flow-frontend';
 
-//
-// Customize the connection indicator
-//
+// Use custom theming
+connectionIndicator.applyDefaultTheme = false;
 
-// Disable default styling to use custom styles for the indicator instead
-// connectionIndicator.applyDefaultTheme = false; // default: `true`
+// Set loading bar stage durations
+connectionIndicator.firstDelay = 100;
+connectionIndicator.secondDelay = 1000;
+connectionIndicator.thirdDelay = 3000;
 
-//
-// Loading bar (`.v-loading-indicator`)
-//
-
-// Delay for showing the indicator and setting the 'first' class name
-connectionIndicator.firstDelay = 300; // 300 ms is the default
-
-// Delay for setting the 'second' class name
-connectionIndicator.secondDelay = 1500; // 1500 ms is the default
-
-// Delay for setting the 'third' class name
-connectionIndicator.thirdDelay = 5000; // 5000 ms is the default
-
-//
-// Connection status message (`.v-status-message`)
-//
-
-// Duration for which the messages are visible
-connectionIndicator.expandedDuration = 2000; // 2000 ms is the default
-// The message for the “connected” connection state
+// Set status message properties
+connectionIndicator.expandedDuration = 1000;
 connectionIndicator.onlineText = 'Online';
-// The message for the “connection lost” state
-connectionIndicator.offlineText = 'Connection lost';
-// The message for the “reconnecting” connection state
-connectionIndicator.reconnectingText = 'Connection lost, trying to reconnect...';
+connectionIndicator.offlineText = 'Offline';
+connectionIndicator.reconnectingText = 'Reconnecting...';
