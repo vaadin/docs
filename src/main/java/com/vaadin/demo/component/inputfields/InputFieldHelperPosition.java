8d84e8a99b1d851c55cbddcae2a9caed01dbd677
Jouni Koivuviita <jouni@vaadin.com>
2021-01-04 13:10:23 +0200

    Document helper-above-field variant

---
+++ src/main/java/com/vaadin/demo/component/inputfields/InputFieldHelperPosition.java
@@ -0,0 +1,25 @@
+package com.vaadin.demo.component.inputfields;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.router.Route;
+import com.vaadin.demo.DemoExporter; // hidden-full-source-line
+
+@Route("input-field-helper-position")
+public class InputFieldHelperPosition extends Div {
+
+  public InputFieldHelperPosition() {
+    // tag::snippet[]
+    TextField textField = new TextField();
+    textField.setLabel("Phone number");
+    textField.setHelperText("Include country and area prefixes");
+    textField.setThemeName(TextFieldVariant.LUMO_HELPER_ABOVE_FIELD.getVariantName());
+    textField.setWidth("15em");
+    add(textField);
+    // end::snippet[]
+  }
+
+  public static class Exporter extends DemoExporter<InputFieldHelperPosition> { // hidden-full-source-line
+  } // hidden-full-source-line
+}
