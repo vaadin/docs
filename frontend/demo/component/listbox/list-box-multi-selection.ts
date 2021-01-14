471eaeef570dacc3be69eefb34545ed25e60de43
Ibrahim Alkali <ibrahim@vaadin.com>
2021-01-14 19:57:51 +0200

    Migrate listbox text and create example files

---
+++ frontend/demo/component/listbox/list-box-multi-selection.ts
@@ -0,0 +1,28 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-list-box';
+import '@vaadin/vaadin-icons/vaadin-icons';
+import '@polymer/iron-icon';
+import { applyTheme } from 'themes/theme-generated.js';
+
+@customElement('list-box-multi-selection')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  render() {
+    return html`
+      <!-- tag::snippet[] -->
+      <vaadin-list-box>
+        <vaadin-item>Show assignee</vaadin-item>
+        <vaadin-item>Show due date</vaadin-item>
+        <vaadin-item>Show status</vaadin-item>
+      </vaadin-list-box>
+      <!-- end::snippet[] -->
+    `;
+  }
+}
