eb5cf639e530e1e896e9d8e954322647c11d59f0
Giovanni Lovato <giovanni@lova.to>
2021-01-27 12:52:29 +0100

    Add additional source files with snippets for code blocks

---
+++ src/main/java/com/vaadin/demo/ce/MainView.java
@@ -0,0 +1,13 @@
+package com.vaadin.demo.ce;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.page.Push;
+
+// tag::enable-push[]
+@Push
+public class MainView extends AppLayout {
+
+    public MainView() {
+    }
+}
+// end::enable-push[]
