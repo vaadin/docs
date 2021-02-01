dbe300e69c98d8609bd8f7eddf832f692083c74e
Pekka Maanpää <pekkamaa@vaadin.com>
2021-02-01 11:36:41 +0200

    Fix Push location in tutorial

--- src/main/java/com/vaadin/demo/ce/MainView.java
+++ src/main/java/com/vaadin/demo/ce/MainView.java
@@ -1,8 +1,12 @@
 package com.vaadin.demo.ce;
 
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.page.Push;
 
+// tag::tutorial-push[]
+@Push
 public class MainView extends AppLayout {
+// end::tutorial-push[]
 
     public MainView() {
     }
