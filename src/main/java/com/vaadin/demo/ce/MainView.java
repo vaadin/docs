19697ad63d7f710d555a069197025cf416508254
Pekka Maanpää <pekkamaa@vaadin.com>
2021-02-01 10:58:55 +0200

    Copy CE source files from latest

---
+++ src/main/java/com/vaadin/demo/ce/MainView.java
@@ -0,0 +1,9 @@
+package com.vaadin.demo.ce;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+
+public class MainView extends AppLayout {
+
+    public MainView() {
+    }
+}
---
+++ src/main/java/com/vaadin/demo/ce/MyVaadinInitListener.java
@@ -0,0 +1,96 @@
+package com.vaadin.demo.ce;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.collaborationengine.CollaborationEngine;
+import com.vaadin.collaborationengine.CollaborationEngineConfiguration;
+import com.vaadin.collaborationengine.LicenseEventHandler;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+
+// tag::init-listener[]
+@SpringComponent
+public class MyVaadinInitListener implements VaadinServiceInitListener {
+    // tag::logger[]
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(MyVaadinInitListener.class);
+    // end::logger[]
+
+    @Override
+    public void serviceInit(ServiceInitEvent serviceEvent) {
+        System.setProperty("vaadin.ce.dataDir",
+                "/Users/steve/vaadin/collaboration-engine/");
+        // tag::configuration[]
+        VaadinService service = serviceEvent.getSource();
+
+        LicenseEventHandler licenseEventHandler = licenseEvent -> {
+            switch (licenseEvent.getType()) {
+            case GRACE_PERIOD_STARTED:
+            case LICENSE_EXPIRES_SOON:
+                LOGGER.warn(licenseEvent.getMessage());
+                break;
+            case GRACE_PERIOD_ENDED:
+            case LICENSE_EXPIRED:
+                LOGGER.error(licenseEvent.getMessage());
+                break;
+            }
+            sendEmail("Vaadin Collaboration Engine license needs to be updated",
+                    licenseEvent.getMessage());
+        };
+
+        CollaborationEngineConfiguration configuration = new CollaborationEngineConfiguration(
+                licenseEventHandler);
+        CollaborationEngine.configure(service, configuration);
+        // end::configuration[]
+    }
+
+    // tag::send-email[]
+    private void sendEmail(String subject, String content) {
+        // Replace the following information:
+        String from = "sender@gmail.com";
+        String password = "*****"; // Read, for example, from encrypted config
+                                   // file.
+        String to = "receiver@gmail.com";
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(content);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+    // end::send-email[]
+}
+// end::init-listener[]
