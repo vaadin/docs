eac77b0a5f1a880e8af9a67482f398064db109f6
Diego Cardoso <diego@vaadin.com>
2021-02-09 15:14:13 +0200

    docs: add App Layout samples (#160)
    Co-authored-by: Jouni Koivuviita <jouni@vaadin.com>

--- frontend/demo/component/app-layout/app-layout-secondary-navigation.ts
+++ frontend/demo/component/app-layout/app-layout-secondary-navigation.ts
@@ -1,10 +1,12 @@
 import '../../init'; // hidden-full-source-line
 
-import { html, LitElement, customElement } from 'lit-element';
+import { html, LitElement, customElement, css } from 'lit-element';
 import '@vaadin/vaadin-app-layout/vaadin-app-layout';
 import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle';
 import '@vaadin/vaadin-tabs/vaadin-tabs';
 import '@vaadin/vaadin-tabs/vaadin-tab';
+import '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import '@vaadin/vaadin-icons/vaadin-icons';
 import { applyTheme } from 'themes/theme-generated.js';
 
@@ -16,47 +18,69 @@ export class Example extends LitElement {
     applyTheme(this.shadowRoot);
   }
 
+  static get styles() {
+    return css`
+      h1,
+      h2 {
+        margin: 0 var(--lumo-space-s);
+        font-size: var(--lumo-font-size-l);
+      }
+
+      h2 {
+        align-self: center;
+      }
+    `;
+  }
+
   render() {
     return html`
       <!-- tag::snippet[] -->
-      <vaadin-app-layout>
-        <vaadin-drawer-toggle slot="navbar"></vaadin-drawer-toggle>
-        <img
-          slot="navbar"
-          src="https://i.imgur.com/GPpnszs.png"
-          alt="Vaadin Logo"
-          width="100"
-          height="31"
-          referrerpolicy="no-referrer"
-        />
-        <vaadin-tabs
-          slot="drawer"
-          orientation="vertical"
-          theme="minimal"
-          style="margin: 0 auto; flex: 1;"
-        >
+      <vaadin-app-layout primary-section="drawer">
+        <vaadin-vertical-layout slot="navbar">
+          <vaadin-horizontal-layout>
+            <vaadin-drawer-toggle></vaadin-drawer-toggle>
+            <h2>Orders</h2>
+          </vaadin-horizontal-layout>
+          <vaadin-tabs>
+            <vaadin-tab>All</vaadin-tab>
+            <vaadin-tab>Open</vaadin-tab>
+            <vaadin-tab>Completed</vaadin-tab>
+            <vaadin-tab>Cancelled</vaadin-tab>
+          </vaadin-tabs>
+        </vaadin-vertical-layout>
+
+        <h1 slot="drawer">MyApp</h1>
+
+        <vaadin-tabs slot="drawer" selected="1" orientation="vertical">
           <vaadin-tab>
-            <iron-icon icon="vaadin:home"></iron-icon>
-            Page 1
+            <iron-icon icon="vaadin:dashboard"></iron-icon>
+            Dashboards
+          </vaadin-tab>
+          <vaadin-tab>
+            <iron-icon icon="vaadin:cart"></iron-icon>
+            Orders
+          </vaadin-tab>
+          <vaadin-tab>
+            <iron-icon icon="vaadin:user-heart"></iron-icon>
+            Customers
+          </vaadin-tab>
+          <vaadin-tab>
+            <iron-icon icon="vaadin:package"></iron-icon>
+            Products
+          </vaadin-tab>
+          <vaadin-tab>
+            <iron-icon icon="vaadin:records"></iron-icon>
+            Documents
           </vaadin-tab>
           <vaadin-tab>
             <iron-icon icon="vaadin:list"></iron-icon>
-            Page 2
+            Tasks
           </vaadin-tab>
           <vaadin-tab>
-            <iron-icon icon="vaadin:options"></iron-icon>
-            Page 3
-          </vaadin-tab>
-          <vaadin-tab>
-            <iron-icon icon="vaadin:question"></iron-icon>
-            Page 4
+            <iron-icon icon="vaadin:chart"></iron-icon>
+            Analytics
           </vaadin-tab>
         </vaadin-tabs>
-
-        <div class="content">
-          <h3>Page title</h3>
-          <p>Page content</p>
-        </div>
       </vaadin-app-layout>
       <!-- end::snippet[] -->
     `;
