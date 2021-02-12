b2e601c46b0d16b8173da95e30e4501609ddb529
Tomi Virkki <virkki@vaadin.com>
2021-02-12 13:00:48 +0200

    fix: use the applyTheme from docs-app bundle in Java examples

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const merge = require('webpack-merge');
 const flowDefaults = require('./webpack.generated.js');
 
@@ -13,6 +14,28 @@ const filteredFileNameOfTheFlowGeneratedMainEntryPoint =
 module.exports = merge(flowDefaults, {
   entry: {
     export: filteredFileNameOfTheFlowGeneratedMainEntryPoint
+  },
+  module: {
+    rules: [
+      {
+        test: /(\\|\/).*example-resources\.ts$/,
+        exclude: path.resolve(__dirname, 'frontend', 'example-resources.ts'),
+      },
+    ],
+  },
+  externals: {
+    // docs-app has its own bundle with all the Vaadin resources.
+    // Polymer etc dependencies have purposefully been excluded from the
+    // docs project (Vaadin) bundle. However, the embedded Flow examples
+    // (like target/frontend/accordion-basic-wc.ts)
+    // now import "applyTheme" which has an indirect dependency to Polymer
+    // so we need to direct the applyTheme function to use the version
+    // bundled with docs-app. Otherwise we'd end up with conflicting imports
+    // (Vaadin/Polymer) originating from two separate bundles loaded on the same page.
+    // We'll do this by declaring generated/theme as an external,
+    // which gets registered to the global namespace by docs-app bundle
+    // (in frontend/demo/example-resources.ts).
+    'generated/theme': '__applyTheme'
   },
   plugins: [
     function(compiler) {
