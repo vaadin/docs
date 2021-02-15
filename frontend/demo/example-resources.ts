894484e26972c586642c9a717b282081515b7ebc
Tomi Virkki <virkki@vaadin.com>
2021-02-15 15:35:46 +0200

    cleanup

--- frontend/demo/example-resources.ts
+++ frontend/demo/example-resources.ts
@@ -1,7 +1,7 @@
 import './init-flow-namespace';
 import 'all-flow-imports-or-empty';
+import { applyTheme } from 'generated/theme';
 // See webpack.config 'externals.generated/theme'
 // This file ends up in the docs-app bundle
-import { applyTheme } from 'generated/theme';
 // @ts-ignore
 window.__applyTheme = { applyTheme };
