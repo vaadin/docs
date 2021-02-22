9afcd5cfe8ac7d0332be90fd8a193495b58d3825
Jouni Koivuviita <jouni@vaadin.com>
2021-02-22 17:43:10 +0200

    fix: notification examples
    - Fix "Rich formatting” example styles
    - Show notifications in the middle position
    - Make the previews non-interactive

--- frontend/demo/component/notification/notification-keyboard-a11y-preview.ts
+++ frontend/demo/component/notification/notification-keyboard-a11y-preview.ts
@@ -9,7 +9,7 @@ const isMac = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'].indexOf(window.navig
 export class Example extends LitElement {
   render() {
     return html`
-      <vaadin-notification-card theme="contrast" style="justify-content: center;">
+      <vaadin-notification-card theme="contrast" slot="middle">
         <div>5 tasks deleted</div>
         <div style="width: 2em"></div>
         <vaadin-button theme="primary">
