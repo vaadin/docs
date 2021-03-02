fd61037967bc14f2ac6ae8bb6800897def2519dc
Tomi Virkki <virkki@vaadin.com>
2021-03-02 15:25:55 +0200

    fix: align theme init.js with the other branches

--- dspublisher/theme/init.js
+++ dspublisher/theme/init.js
@@ -1,29 +1,45 @@
-// This js-file gets run whenever dspublisher is started
-
 const path = require(`path`);
-const fs = require(`fs-extra`);
-const fetch = require('node-fetch').default;
+const fs = require(`fs`);
+const https = require('https');
 
 async function fetchAntlersStyleSheets() {
   // Full collection of available style sheets: https://gitlab.vaadin.com/dion/vcom-design-system/
   const antlersStyleSheets = [
     'https://cdn.vaadin.com/vaadin-design-system/latest/assets/css/haas-bundle.css',
-    'https://cdn.vaadin.com/vaadin-design-system/latest/assets/css/3-patterns/card.css'
+    'https://cdn.vaadin.com/vaadin-design-system/latest/assets/css/3-patterns/card.css',
   ];
 
   // No try-catch, let the build fail if there is an error fetching the style sheets
   for (let i = 0; i < antlersStyleSheets.length; i++) {
     const url = antlersStyleSheets[i];
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch Antlers stylesheet "${url}": ${response.status} ${response.statusText}`
-      );
-    }
-    const css = await response.text();
+    const css = await new Promise((resolve) => {
+      https
+        .get(url, (res) => {
+          res.setEncoding('utf8');
+          let body = '';
+          res.on('data', (data) => {
+            body += data;
+          });
+          res.on('end', () => {
+            resolve(body);
+          });
+        })
+        .on('error', (e) => {
+          throw new Error(
+            `Failed to fetch Antlers stylesheet "${url}": ${response.status} ${response.statusText}`
+          );
+        });
+    });
+
     let localStyleSheetPath =
       './styles/antlers/' + url.split('https://cdn.vaadin.com/vaadin-design-system/latest/')[1];
-    await fs.outputFile(path.resolve(__dirname, localStyleSheetPath), css);
+
+    const localStyleSheetDir = localStyleSheetPath.split('/').slice(0, -1).join('/');
+    fs.mkdirSync(path.resolve(__dirname, localStyleSheetDir), { recursive: true });
+
+    await fs.writeFileSync(path.resolve(__dirname, localStyleSheetPath), css);
   }
 }
-fetchAntlersStyleSheets();
+
+// The default gets run whenever dspublisher is started
+module.exports = fetchAntlersStyleSheets;
