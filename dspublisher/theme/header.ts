7bccedaab2b8b97af3b0228041fbb3348fafa15b
Tomi Virkki <virkki@vaadin.com>
2021-03-02 15:07:17 +0200

    fix: use native customElement API to init custom header

--- dspublisher/theme/header.ts
+++ dspublisher/theme/header.ts
@@ -1,11 +1,7 @@
-import { html, LitElement } from 'lit-element';
-
 // Use this custom element to modify the dspublisher header content
 // Use an empty file to not show a header
-export default class Example extends LitElement {
-  createRenderRoot() {
-    return this;
-  }
+export default class Example extends HTMLElement {
+  __initialized = false;
 
   private haasImportScript() {
     const script = document.createElement('script');
@@ -17,26 +13,32 @@ export default class Example extends LitElement {
     return script;
   }
 
-  render() {
-    return html`
-      <div class="vcom-header" id="haas-container"></div>
-      <div class="feedback-banner">
-        <div class="feedback-banner-content">
-          <p>
-            <span class="feedback-banner-beta"> Beta<span class="visually-hidden">: </span> </span>
-            Help us improve our new documentation site by giving early feedback
-          </p>
-          <a
-            href=${`https://docs.google.com/forms/d/e/1FAIpQLScn4gtQzGxMDusU6n6UTR_pjghTkyFsNQij_Bb2NqJgiyNMlw/viewform?usp=pp_url&entry.1307305669=${location.href}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Give feedback
-          </a>
-          <a href="/docs/">Back to old docs</a>
+  connectedCallback() {
+    if (!this.__initialized) {
+      this.__initialized = true;
+
+      this.innerHTML = `
+        <div class="vcom-header" id="haas-container"></div>
+        <div class="feedback-banner">
+          <div class="feedback-banner-content">
+            <p>
+              <span class="feedback-banner-beta"> Beta<span class="visually-hidden">: </span> </span>
+              Help us improve our new documentation site by giving early feedback
+            </p>
+            <a
+              href=${`https://docs.google.com/forms/d/e/1FAIpQLScn4gtQzGxMDusU6n6UTR_pjghTkyFsNQij_Bb2NqJgiyNMlw/viewform?usp=pp_url&entry.1307305669=${location.href}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Give feedback
+            </a>
+            <a href="/docs/">Back to old docs</a>
+          </div>
         </div>
-      </div>
-      ${this.haasImportScript()}
-    `;
+      `;
+
+      const haasImportScriptElement = this.haasImportScript();
+      document.head.appendChild(haasImportScriptElement);
+    }
   }
 }
