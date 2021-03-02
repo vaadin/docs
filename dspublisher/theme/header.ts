1a736c6fad63ba82aa9f5acbb0c58831d11f43c0
Tomi Virkki <virkki@vaadin.com>
2021-03-02 09:47:53 +0200

    Move feedback banner to header.ts

--- dspublisher/theme/header.ts
+++ dspublisher/theme/header.ts
@@ -19,7 +19,23 @@ export default class Example extends LitElement {
 
   render() {
     return html`
-      <div className="vcom-header" id="haas-container"></div>
+      <div class="vcom-header" id="haas-container"></div>
+      <div class="feedback-banner">
+        <div class="feedback-banner-content">
+          <p>
+            <span class="feedback-banner-beta"> Beta<span class="visually-hidden">: </span> </span>
+            Help us improve our new documentation site by giving early feedback
+          </p>
+          <a
+            href=${`https://docs.google.com/forms/d/e/1FAIpQLScn4gtQzGxMDusU6n6UTR_pjghTkyFsNQij_Bb2NqJgiyNMlw/viewform?usp=pp_url&entry.1307305669=${location.href}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Give feedback
+          </a>
+          <a href="/docs/">Back to old docs</a>
+        </div>
+      </div>
       ${this.haasImportScript()}
     `;
   }
