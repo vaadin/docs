6513ae620fd9a168631221b826a77e9596104ed5
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2021-03-04 14:40:29 +0200

    Update toolbar example

--- frontend/demo/component/crud/crud-toolbar.ts
+++ frontend/demo/component/crud/crud-toolbar.ts
@@ -4,9 +4,11 @@ import '@vaadin/flow-frontend/gridConnector.js'; // hidden-full-source-line
 import { html, LitElement, customElement, internalProperty } from 'lit-element';
 import '@vaadin/vaadin-crud/vaadin-crud';
 import '@vaadin/vaadin-icons/vaadin-icons';
+import '@vaadin/vaadin-button/vaadin-button';
 import { getPeople } from '../../domain/DataService';
 import Person from '../../../generated/com/vaadin/demo/domain/Person';
 import { applyTheme } from 'generated/theme';
+import { CrudSizeChanged } from '@vaadin/vaadin-crud/vaadin-crud';
 
 @customElement('crud-toolbar')
 export class Example extends LitElement {
@@ -15,6 +17,9 @@ export class Example extends LitElement {
     // Apply custom theme (only supported if your app uses one)
     applyTheme(this.shadowRoot);
   }
+
+  @internalProperty()
+  private size = 0;
 
   @internalProperty()
   private items: Person[] = [];
@@ -26,14 +31,22 @@ export class Example extends LitElement {
   render() {
     return html`
       <!-- tag::snippet[] -->
-      <vaadin-crud include="firstName, lastName" .items=${this.items}>
-        <div slot="toolbar" style="flex: 1;">Total: <b>${this.items.length}</b> employees</div>
-        <a slot="toolbar" new-button href="javascript:"
-          ><iron-icon style="height: calc(var(--lumo-size-xs) / 2);" icon="vaadin:plus"></iron-icon
-          >New employee</a
-        >
+      <vaadin-crud
+        include="firstName, lastName"
+        .items=${this.items}
+        @size-changed=${this.sizeChanged}
+      >
+        <div slot="toolbar" style="flex: 1;">Total: <b>${this.size}</b> employees</div>
+        <vaadin-button style="margin: 0;" theme="tertiary" slot="toolbar" new-button>
+          <iron-icon slot="prefix" icon="vaadin:plus"></iron-icon>
+          New employee
+        </vaadin-button>
       </vaadin-crud>
       <!-- end::snippet[] -->
     `;
+  }
+
+  sizeChanged(e: CrudSizeChanged) {
+    this.size = e.detail.value;
   }
 }
