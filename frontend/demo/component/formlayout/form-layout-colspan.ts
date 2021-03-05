a1bcbf6d55b9679b0a8e25efd9418de85bdd5547
diegocardoso <diego@vaadin.com>
2021-03-05 16:17:59 -0300

    docs: add colspan example

---
+++ frontend/demo/component/formlayout/form-layout-colspan.ts
@@ -0,0 +1,37 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-form-layout/vaadin-form-layout';
+
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import '@vaadin/vaadin-date-picker/vaadin-date-picker';
+import '@vaadin/vaadin-time-picker/vaadin-time-picker';
+import { applyTheme } from 'generated/theme';
+import { FormLayoutResponsiveStep } from '@vaadin/vaadin-form-layout/vaadin-form-layout';
+
+// tag::snippet[]
+@customElement('form-layout-custom-layout')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  private responsiveSteps: FormLayoutResponsiveStep[] = [
+    { minWidth: 0, columns: 1 },
+    { minWidth: '20em', columns: 3 }
+  ];
+
+  render() {
+    return html`
+      <vaadin-form-layout .responsiveSteps=${this.responsiveSteps}>
+        <vaadin-text-field label="Title" colspan="3"></vaadin-text-field>
+        <vaadin-date-picker label="Date"></vaadin-date-picker>
+        <vaadin-time-picker label="From"></vaadin-time-picker>
+        <vaadin-time-picker label="To"></vaadin-time-picker>
+      </vaadin-form-layout>
+    `;
+  }
+}
+// end::snippet[]
