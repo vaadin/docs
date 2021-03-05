9cf4f5d044c034c26ca3e930b6f7eea69ef598d0
diegocardoso <diego@vaadin.com>
2021-03-05 16:18:45 -0300

    docs: add side example

---
+++ frontend/demo/component/formlayout/form-layout-side.ts
@@ -0,0 +1,44 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-form-layout/vaadin-form-layout';
+import '@vaadin/vaadin-form-layout/vaadin-form-item';
+
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import { applyTheme } from 'generated/theme';
+
+// tag::snippet[]
+@customElement('form-layout-side')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  render() {
+    return html`
+      <vaadin-form-layout>
+        <vaadin-form-item>
+          <label slot="label">Revenue</label>
+          <vaadin-text-field>
+            <span slot="suffix">EUR</span>
+          </vaadin-text-field>
+        </vaadin-form-item>
+        <vaadin-form-item>
+          <label slot="label">Expenses</label>
+          <vaadin-text-field>
+            <span slot="suffix">EUR</span>
+          </vaadin-text-field>
+        </vaadin-form-item>
+        <vaadin-form-item>
+          <label slot="label">Invoices</label>
+          <vaadin-text-field>
+            <span slot="suffix">EUR</span>
+          </vaadin-text-field>
+        </vaadin-form-item>
+      </vaadin-form-layout>
+    `;
+  }
+}
+// end::snippet[]
