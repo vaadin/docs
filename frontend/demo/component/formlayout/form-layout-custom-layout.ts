3c4f33c8b5d2131437760f0526fc82ee10cbfc3c
diegocardoso <diego@vaadin.com>
2021-03-05 15:44:08 -0300

    docs: add custom layout example

---
+++ frontend/demo/component/formlayout/form-layout-custom-layout.ts
@@ -0,0 +1,40 @@
+import '../../init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-form-layout/vaadin-form-layout';
+
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import '@vaadin/vaadin-text-field/vaadin-email-field';
+import '@vaadin/vaadin-split-layout/vaadin-split-layout';
+import { applyTheme } from 'generated/theme';
+import { FormLayoutResponsiveStep } from '@vaadin/vaadin-form-layout/vaadin-form-layout';
+
+// tag::snippet[]
+@customElement('form-layout-custom-layout')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  private responsiveSteps: FormLayoutResponsiveStep[] = [
+    { minWidth: 0, columns: 1 },
+    { minWidth: '20em', columns: 2 },
+    { minWidth: '40em', columns: 3 }
+  ];
+
+  render() {
+    return html`
+      <vaadin-split-layout>
+        <vaadin-form-layout .responsiveSteps=${this.responsiveSteps}>
+          <vaadin-text-field label="First name"></vaadin-text-field>
+          <vaadin-text-field label="Last name"></vaadin-text-field>
+          <vaadin-email-field label="Email"></vaadin-email-field>
+        </vaadin-form-layout>
+        <div></div>
+      </vaadin-split-layout>
+    `;
+  }
+}
+// end::snippet[]
