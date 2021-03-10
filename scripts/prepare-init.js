7434c7f75787cd6272bc236cf0042351d11c00f2
Tomi Virkki <virkki@vaadin.com>
2021-03-10 10:29:07 +0200

    cleanup

--- scripts/prepare-init.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  require('inquirer');
-} catch (e) {
-  console.log('Installing NPM dependencies...');
-  execSync('mvn compile vaadin:prepare-frontend vaadin:build-frontend');
-}
