09b74b2cb9e57343324e049d1b8fda97b08c9792
Jouni Koivuviita <jouni@vaadin.com>
2021-03-22 15:36:17 +0200

    remove feedback banner

--- dspublisher/theme/header.ts
+++ dspublisher/theme/header.ts
@@ -20,22 +20,6 @@ export default class Example extends LitElement {
   render() {
     return html`
       <div class="vcom-header" id="haas-container"></div>
-      <div class="feedback-banner">
-        <div class="feedback-banner-content">
-          <p>
-            <span class="feedback-banner-beta"> Beta<span class="visually-hidden">: </span> </span>
-            Help us improve our new documentation site by giving early feedback
-          </p>
-          <a
-            href=${`https://docs.google.com/forms/d/e/1FAIpQLScn4gtQzGxMDusU6n6UTR_pjghTkyFsNQij_Bb2NqJgiyNMlw/viewform?usp=pp_url&entry.1307305669=${location.href}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Give feedback
-          </a>
-          <a href="/docs/">Back to old docs</a>
-        </div>
-      </div>
       ${this.haasImportScript()}
     `;
   }
