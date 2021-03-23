919f7c011cc2c923d0059314986b540f32df2f00
diegocardoso <diego@vaadin.com>
2021-03-23 14:08:05 -0300

    feat: add basic example

--- frontend/demo/component/custom-field/custom-field-basic.ts
+++ frontend/demo/component/custom-field/custom-field-basic.ts
@@ -4,6 +4,9 @@ import { html, LitElement, customElement } from 'lit-element';
 import '@vaadin/vaadin-custom-field/vaadin-custom-field';
 import '@vaadin/vaadin-date-picker/vaadin-date-picker';
 import { applyTheme } from 'Frontend/generated/theme';
+import { Binder, field } from 'Frontend/../target/flow-frontend/form';
+import AppointmentModel from 'Frontend/generated/com/vaadin/demo/domain/AppointmentModel';
+import { differenceInDays, parseISO, isAfter } from 'date-fns';
 
 @customElement('custom-field-basic')
 export class Example extends LitElement {
@@ -13,6 +16,35 @@ export class Example extends LitElement {
     applyTheme(this.shadowRoot);
   }
 
+  private binder = new Binder(this, AppointmentModel);
+
+  firstUpdated() {
+    this.binder.for(this.binder.model.enrollmentPeriod).addValidator({
+      message: 'Dates must be no longer than 30 days apart',
+      validate: (enrollmentPeriod: string) => {
+        const [from, to] = enrollmentPeriod.split('\t');
+
+        if (from === '' || to === '') {
+          return true;
+        }
+
+        return differenceInDays(parseISO(to), parseISO(from)) <= 30;
+      },
+    });
+    this.binder.for(this.binder.model.enrollmentPeriod).addValidator({
+      message: 'From date should not be after to date',
+      validate: (enrollmentPeriod: string) => {
+        const [from, to] = enrollmentPeriod.split('\t');
+
+        if (from === '' || to === '') {
+          return true;
+        }
+
+        return isAfter(parseISO(to), parseISO(from));
+      },
+    });
+  }
+
   render() {
     return html`
       <!-- tag::snippet[] -->
@@ -20,9 +52,11 @@ export class Example extends LitElement {
         label="Enrollment period"
         helper-text="Cannot be longer than 30 days"
         required
+        ...="${field(this.binder.model.enrollmentPeriod)}"
       >
-        <vaadin-date-picker></vaadin-date-picker>
-        <vaadin-date-picker></vaadin-date-picker>
+        <vaadin-date-picker placeholder="From"></vaadin-date-picker>
+        &ndash;
+        <vaadin-date-picker placeholder="To"></vaadin-date-picker>
       </vaadin-custom-field>
       <!-- end::snippet[] -->
     `;
