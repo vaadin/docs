fc476cb5029e9e9f1f1848537d7cbf6e24a6f27f
Ibrahim Alkali <alkalibrahim@gmail.com>
2021-03-25 12:01:14 +0200

    Add split-layout content and example stubs

---
+++ frontend/demo/component/splitlayout/split-layout-toggle.ts
@@ -0,0 +1,25 @@
+import '../../init'; // hidden-full-source-line
+
+import { customElement, html, LitElement } from 'lit-element';
+import '@vaadin/vaadin-split-layout/vaadin-split-layout';
+import { applyTheme } from 'generated/theme';
+
+@customElement('split-layout-toggle')
+export class Example extends LitElement {
+    constructor() {
+        super();
+        // Apply custom theme (only supported if your app uses one)
+        applyTheme(this.shadowRoot);
+    }
+
+    render() {
+        return html`
+      <!-- tag::snippet[] -->
+      <vaadin-split-layout>
+          <div>First content element</div>
+          <div>Second content element</div>
+      </vaadin-split-layout>
+      <!-- end::snippet[] -->
+    `;
+    }
+}
