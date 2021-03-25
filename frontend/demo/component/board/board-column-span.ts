d1694e6c6ae30690639fb20e75fc464e1a26cd2e
Vlad Rindevich <vladrin@vaadin.com>
2021-03-25 14:02:01 +0300

    feat: move Board article text

---
+++ frontend/demo/component/board/board-column-span.ts
@@ -0,0 +1,50 @@
+import 'Frontend/demo/init'; // hidden-full-source-line
+
+import { html, LitElement, customElement, css } from 'lit-element';
+import '@vaadin/vaadin-board/vaadin-board';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('board-column-span')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  // tag::snippet[]
+  render() {
+    return html`
+      <vaadin-board>
+        <vaadin-board-row>
+          <div class="cell">Cell 1</div>
+          <div class="cell">Cell 2</div>
+          <div class="cell">Cell 3</div>
+          <div class="cell">Cell 4</div>
+        </vaadin-board-row>
+      </vaadin-board>
+    `;
+  }
+  // end::snippet[]
+  static get styles() {
+    return css`
+      .cell {
+        padding: 1em;
+        text-align: center;
+        color: white;
+      }
+      .cell:nth-child(1) {
+        background: #003e53;
+      }
+      .cell:nth-child(2) {
+        background: #00506b;
+      }
+      .cell:nth-child(3) {
+        background: #006c90;
+      }
+      .cell:nth-child(4) {
+        background: #0090c0;
+      }
+    `;
+  }
+}
