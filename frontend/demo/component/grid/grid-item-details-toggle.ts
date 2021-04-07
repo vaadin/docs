b47b2ebdabdd5dd5334793216802cff596ed6a80
diegocardoso <diego@vaadin.com>
2021-04-07 15:21:47 -0300

    feat: add item details examples

--- frontend/demo/component/grid/grid-item-details-toggle.ts
+++ frontend/demo/component/grid/grid-item-details-toggle.ts
@@ -3,11 +3,19 @@ import '@vaadin/flow-frontend/gridConnector.js'; // hidden-full-source-line (Gri
 
 import { customElement, LitElement, internalProperty } from 'lit-element';
 import '@vaadin/vaadin-grid/vaadin-grid';
-import { GridItemModel } from '@vaadin/vaadin-grid/vaadin-grid';
+import '@vaadin/vaadin-form-layout/vaadin-form-layout';
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import {
+  GridActiveItemChanged,
+  GridColumnElement,
+  GridElement,
+  GridItemModel,
+} from '@vaadin/vaadin-grid/vaadin-grid';
 import { getPeople } from 'Frontend/demo/domain/DataService';
 import { render, html } from 'lit-html';
 import Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 import { applyTheme } from 'Frontend/generated/theme';
+import { guard } from 'lit-html/directives/guard';
 
 // tag::snippet[]
 @customElement('grid-item-details-toggle')
@@ -21,38 +29,91 @@ export class Example extends LitElement {
   @internalProperty()
   private items: Person[] = [];
 
+  @internalProperty()
+  private detailsOpenedItem: any[] = [];
+
   async firstUpdated() {
-    const { people } = await getPeople();
+    const people = (await getPeople()).people.map((person) => ({
+      ...person,
+      displayName: `${person.firstName} ${person.lastName}`,
+    }));
     this.items = people;
   }
 
   render() {
     return html`
-      <vaadin-grid .items=${this.items}>
+      <vaadin-grid
+        theme="row-stripes"
+        .items=${this.items}
+        .detailsOpenedItems="${this.detailsOpenedItem}"
+        .rowDetailsRenderer="${guard(
+          [],
+          () => (root: HTMLElement, _: GridElement, model: GridItemModel) => {
+            const person = model.item as Person;
+
+            render(
+              html`<vaadin-form-layout .responsiveSteps="${[{ minWidth: '0', columns: 3 }]}">
+                <vaadin-text-field
+                  label="Email address"
+                  .value="${person.email}"
+                  colspan="3"
+                  readonly
+                ></vaadin-text-field>
+                <vaadin-text-field
+                  label="Phone number"
+                  .value="${person.address.phone}"
+                  colspan="3"
+                  readonly
+                ></vaadin-text-field>
+                <vaadin-text-field
+                  label="Street address"
+                  .value="${person.address.street}"
+                  colspan="3"
+                  readonly
+                ></vaadin-text-field>
+                <vaadin-text-field
+                  label="ZIP code"
+                  .value="${person.address.zip}"
+                  readonly
+                ></vaadin-text-field>
+                <vaadin-text-field
+                  label="City"
+                  .value="${person.address.city}"
+                  readonly
+                ></vaadin-text-field>
+                <vaadin-text-field
+                  label="State"
+                  .value="${person.address.state}"
+                  readonly
+                ></vaadin-text-field>
+              </vaadin-form-layout>`,
+              root
+            );
+          }
+        )}"
+      >
+        <vaadin-grid-column path="displayName"></vaadin-grid-column>
+        <vaadin-grid-column path="profession"></vaadin-grid-column>
         <vaadin-grid-column
-          header="Image"
-          .renderer=${this.avatarRenderer}
-          flex-grow="0"
-          auto-width
+          .renderer="${guard(
+            [],
+            () => (root: HTMLElement, _: GridColumnElement, model: GridItemModel) => {
+              const person = model.item as Person;
+              render(
+                html`<vaadin-button
+                  theme="tertiary"
+                  @click="${() =>
+                    (this.detailsOpenedItem =
+                      this.detailsOpenedItem[0] !== person ? [person] : [null])}"
+                  >View details</vaadin-button
+                >`,
+                root
+              );
+            }
+          )}"
         ></vaadin-grid-column>
-        <vaadin-grid-column path="firstName"></vaadin-grid-column>
-        <vaadin-grid-column path="lastName"></vaadin-grid-column>
-        <vaadin-grid-column path="email"></vaadin-grid-column>
       </vaadin-grid>
     `;
   }
-
-  private avatarRenderer = (root: HTMLElement, _: HTMLElement, model: GridItemModel) => {
-    render(
-      html`
-        <img
-          style="height: var(--lumo-size-m)"
-          src=${(model.item as Person).pictureUrl}
-          alt="User avatar"
-        />
-      `,
-      root
-    );
-  };
 }
 // end::snippet[]
