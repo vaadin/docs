a2108add877bc2512b13bc41ec4bb641d9501b4b
diegocardoso <diego@vaadin.com>
2021-04-09 13:36:28 -0300

    feat: implement alignment example

--- frontend/demo/component/grid/grid-column-alignment.ts
+++ frontend/demo/component/grid/grid-column-alignment.ts
@@ -3,13 +3,13 @@ import '@vaadin/flow-frontend/gridConnector.js'; // hidden-full-source-line (Gri
 
 import { customElement, LitElement, internalProperty } from 'lit-element';
 import '@vaadin/vaadin-grid/vaadin-grid';
-import { GridItemModel } from '@vaadin/vaadin-grid/vaadin-grid';
 import { getPeople } from 'Frontend/demo/domain/DataService';
-import { render, html } from 'lit-html';
 import Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 import { applyTheme } from 'Frontend/generated/theme';
+import { guard } from 'lit-html/directives/guard';
+import { html, render } from 'lit-html';
+import { format } from 'date-fns';
 
-// tag::snippet[]
 @customElement('grid-column-alignment')
 export class Example extends LitElement {
   constructor() {
@@ -22,37 +22,47 @@ export class Example extends LitElement {
   private items: Person[] = [];
 
   async firstUpdated() {
-    const { people } = await getPeople();
+    const people = (await getPeople()).people.map((person) => ({
+      ...person,
+      displayName: `${person.firstName} ${person.lastName}`,
+    }));
     this.items = people;
+  }
+
+  randomDate() {
+    const futureDate = new Date(Date.now() + Math.floor(Math.random() * 10000000000));
+    return format(futureDate, 'P');
+  }
+
+  randomAmount() {
+    return Intl.NumberFormat('en', { style: 'currency', currency: 'EUR' }).format(
+      Math.floor(Math.random() * 1000000)
+    );
   }
 
   render() {
     return html`
+      <!-- tag:snippet[] -->
       <vaadin-grid .items=${this.items}>
+        <vaadin-grid-column path="displayName"></vaadin-grid-column>
         <vaadin-grid-column
-          header="Image"
-          .renderer=${this.avatarRenderer}
-          flex-grow="0"
-          auto-width
+          header="Due"
+          .renderer="${guard([], () => (root: HTMLElement) =>
+            render(html`<span>${this.randomDate()}</span>`, root)
+          )}"
         ></vaadin-grid-column>
-        <vaadin-grid-column path="firstName"></vaadin-grid-column>
-        <vaadin-grid-column path="lastName"></vaadin-grid-column>
-        <vaadin-grid-column path="email"></vaadin-grid-column>
+        <vaadin-grid-column
+          header="Amount"
+          text-align="end"
+          .renderer="${guard([], () => (root: HTMLElement) => {
+            render(
+              html`<span style="font-variant-numeric: tabular-nums">${this.randomAmount()}</span>`,
+              root
+            );
+          })}"
+        ></vaadin-grid-column>
       </vaadin-grid>
+      <!-- end:snippet[] -->
     `;
   }
-
-  private avatarRenderer = (root: HTMLElement, _: HTMLElement, model: GridItemModel) => {
-    render(
-      html`
-        <img
-          style="height: var(--lumo-size-m)"
-          src=${(model.item as Person).pictureUrl}
-          alt="User avatar"
-        />
-      `,
-      root
-    );
-  };
 }
-// end::snippet[]
