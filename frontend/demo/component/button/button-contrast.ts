607ebb0277130a8b38af05f422593d2966cbc7f9
David Sosa <sosa@vaadin.com>
2021-04-12 16:31:13 +0300

    add miscellaneous style variants docs and examples

---
+++ frontend/demo/component/button/button-contrast.ts
@@ -0,0 +1,24 @@
+import 'Frontend/demo/init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-button/vaadin-button';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('button-contrast')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  render() {
+    return html`
+      <!-- tag::snippet[] -->
+      <vaadin-button theme="primary contrast">Primary</vaadin-button>
+      <vaadin-button theme="secondary contrast">Secondary</vaadin-button>
+      <vaadin-button theme="tertiary contrast">Tertiary (avoid)</vaadin-button>
+      <!-- end::snippet[] -->
+    `;
+  }
+}
