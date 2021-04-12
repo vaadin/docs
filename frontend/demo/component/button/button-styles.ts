53dbabfae424568cd93c0312b586d45e4e664187
David Sosa <sosa@vaadin.com>
2021-04-12 15:25:08 +0300

    add button default styles docs and example

---
+++ frontend/demo/component/button/button-styles.ts
@@ -0,0 +1,24 @@
+import 'Frontend/demo/init'; // hidden-full-source-line
+
+import { html, LitElement, customElement } from 'lit-element';
+import '@vaadin/vaadin-button/vaadin-button';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('button-styles')
+export class Example extends LitElement {
+  constructor() {
+    super();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(this.shadowRoot);
+  }
+
+  render() {
+    return html`
+      <!-- tag::snippet[] -->
+      <vaadin-button theme="primary">Primary</vaadin-button>
+      <vaadin-button theme="secondary">Secondary</vaadin-button>
+      <vaadin-button theme="tertiary">Tertiary</vaadin-button>
+      <!-- end::snippet[] -->
+    `;
+  }
+}
