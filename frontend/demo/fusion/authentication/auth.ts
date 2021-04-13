096243491434e4f6b22f02e520427b6ad1f9bdf0
Anton Platonov <platosha@gmail.com>
2021-04-13 19:09:35 +0300

    Add Fusion offline authentication article
    Fixes #349

--- frontend/demo/fusion/authentication/auth.ts
+++ frontend/demo/fusion/authentication/auth.ts
@@ -1,4 +1,4 @@
-// tag::implt[]
+// tag::impl[]
 // Uses the Vaadin provided login an logout helper methods
 import { login as loginImpl, LoginResult, logout as logoutImpl } from '@vaadin/flow-frontend';
 // end::impl[]
@@ -21,9 +21,12 @@ let authentication: Authentication | undefined = undefined;
 
 // end::basic[]
 // tag::offline[]
+// tag::storage-defs[]
 const AUTHENTICATION_KEY = 'authentication';
 const THIRTY_DAYS_MS = 30 * 24 * 60 * 60 * 1000;
+// end::storage-defs[]
 
+// tag::restore[]
 // Get authentication from local storage
 const storedAuthenticationJson = localStorage.getItem(AUTHENTICATION_KEY);
 if (storedAuthenticationJson !== null) {
@@ -40,6 +43,7 @@ if (storedAuthenticationJson !== null) {
   }
 }
 
+// end::restore[]
 // end::offline[]
 /**
  * Forces the session to expire and removes user information stored in
@@ -112,7 +116,7 @@ export function isLoggedIn() {
   return !!authentication;
 }
 
-// tag::isLoggedIn[]
+// end::isLoggedIn[]
 /**
  * Checks if the user has the role.
  */
