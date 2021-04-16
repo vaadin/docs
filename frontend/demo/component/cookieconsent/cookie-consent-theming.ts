a92915871d1f9c558acbe1c63efdd53dcc073ce5
Vladimir Rindevich <vladrin@vaadin.com>
2021-04-16 13:22:26 +0300

    refactor: move CSS code for themable example to the global styles

--- frontend/demo/component/cookieconsent/cookie-consent-theming.ts
+++ frontend/demo/component/cookieconsent/cookie-consent-theming.ts
@@ -9,33 +9,7 @@ import '@vaadin/vaadin-cookie-consent/vaadin-cookie-consent';
 export class Example extends LitElement {
   // tag::snippet[]
   connectedCallback() {
-    const style = document.createElement('style');
-    style.innerHTML = `
-      div.cc-window.cc-theme-block {
-        background-color: var(--lumo-base-color);
-        box-shadow: var(--lumo-box-shadow-s);
-        color: var(--lumo-body-text-color);
-        font-family: var(--lumo-font-family);
-        font-size: var(--lumo-font-size-m);
-      }
-
-      a.cc-link[role=button] {
-        color: var(--lumo-primary-text-color);
-        opacity: 1;
-        padding: 0 var(--lumo-space-s);
-      }
-
-      a.cc-btn.cc-dismiss {
-        border: none;
-        border-radius: var(--lumo-border-radius);
-        color: var(--lumo-primary-contrast-color);
-        font-size: var(--lumo-font-size-m);
-        line-height: var(--lumo-size-m);
-        min-width: 0;
-        padding: 0 calc(var(--lumo-size-m) / 3 + var(--lumo-border-radius) / 2);
-      }
-    `;
-    document.body.appendChild(style);
+    document.documentElement.classList.add('themable');
     super.connectedCallback();
   }
   // end::snippet[]
