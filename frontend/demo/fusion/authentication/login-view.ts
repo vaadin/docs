fde266855352feae8d0c7faa4077fb9a83f31955
haijian <haijian@vaadin.com>
2021-04-29 15:46:35 +0300

    fix lint

--- frontend/demo/fusion/authentication/login-view.ts
+++ frontend/demo/fusion/authentication/login-view.ts
@@ -17,8 +17,7 @@ export class LoginView extends LitElement implements AfterEnterObserver {
     // If a login redirect was initiated by the client router, this.returnUrl knows where to go.
     // If login was opened directly, use the default URL provided by the server.
     // As we do not know if the target is a resource or a Fusion view or a Flow view, we cannot just use Router.go
-    window.location.href =
-      result.redirectUrl || this.returnUrl || result.defaultUrl || '/';
+    window.location.href = result.redirectUrl || this.returnUrl || result.defaultUrl || '/';
   };
 
   render() {
