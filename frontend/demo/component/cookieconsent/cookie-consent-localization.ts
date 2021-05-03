ceefb6ebeab46149a2dffde9ca7b26001fe976b2
Joacim Päivärinne <joacim@vaadin.com>
2021-05-03 13:12:17 +0300

    Cookie consent examples

--- frontend/demo/component/cookieconsent/cookie-consent-localization.ts
+++ frontend/demo/component/cookieconsent/cookie-consent-localization.ts
@@ -1,7 +1,6 @@
 import 'Frontend/demo/init'; // hidden-full-source-line
 import '@vaadin/flow-frontend/contextMenuConnector.js'; // hidden-full-source-line
 import './example-cleanup'; // hidden-full-source-line
-
 import { html, LitElement, customElement } from 'lit-element';
 import '@vaadin/vaadin-cookie-consent/vaadin-cookie-consent';
 
--- src/main/java/com/vaadin/demo/component/cookieconsent/CookieConsentTheming.java
+++ src/main/java/com/vaadin/demo/component/cookieconsent/CookieConsentTheming.java
@@ -1,21 +1,20 @@
 package com.vaadin.demo.component.cookieconsent;
 
+import com.vaadin.demo.DemoExporter; // hidden-full-source-line
 import com.vaadin.flow.component.cookieconsent.CookieConsent;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
-import com.vaadin.demo.DemoExporter; // hidden-full-source-line
 
 @Route("cookie-consent-theming")
 public class CookieConsentTheming extends Div {
 
     public CookieConsentTheming() {
         // tag::snippet[]
-        CookieConsent cookieConsent = new CookieConsent();
+        add("cookie-consent-theming");
 
+        CookieConsent cookieConsent = new CookieConsent();
         add(cookieConsent);
         // end::snippet[]
     }
-
-    public static class Exporter extends DemoExporter<CookieConsentTheming> { // hidden-full-source-line
-    } // hidden-full-source-line
+    public static class Exporter extends DemoExporter<CookieConsentTheming> {} // hidden-full-source-line
 }
