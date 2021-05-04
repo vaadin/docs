e334b03226d8c3ce33459d8fc2aad69a4cef7dbb
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2021-05-04 14:46:45 +0300

    mention security changes in upgrade guide, reduce in-line code. (#429)
    Co-authored-by: Johannes Eriksson <joheriks@vaadin.com>
    Co-authored-by: David Sosa <76832183+sosa-vaadin@users.noreply.github.com>

--- src/main/java/com/vaadin/demo/SecurityConfig.java
+++ src/main/java/com/vaadin/demo/SecurityConfig.java
@@ -1,16 +1,36 @@
 package com.vaadin.demo;
 
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
 
+  // tag::login[]
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+    super.configure(http);
+    setLoginView(http, "/login");
   }
+  // end::login[]
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // Configure users and roles in memory
+    auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+  }
+
+  // tag::public-resources[]
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+      super.configure(web);
+      web.ignoring().antMatchers("/images/**"); 
+  }
+  // end::public-resources[]
 }
