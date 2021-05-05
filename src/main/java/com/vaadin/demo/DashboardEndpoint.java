960cdb7e4861cc2af7f0b9e09d80fd241ba3d1f7
Artur Signell <artur@vaadin.com>
2021-05-05 14:31:14 +0300

    Fix imports

--- src/main/java/com/vaadin/demo/DashboardEndpoint.java
+++ src/main/java/com/vaadin/demo/DashboardEndpoint.java
@@ -1,7 +1,7 @@
 package com.vaadin.demo;
 
 import com.vaadin.flow.server.connect.Endpoint;
-import com.vaadin.flow.server.connect.auth.AnonymousAllowed;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 /**
  * The endpoint for the client-side Form View.
