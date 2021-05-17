807f9144a2ca0d84e967bf128da7aed30329df96
Kari Söderholm <haprog@vaadin.com>
2021-05-17 13:11:20 +0300

    fix: missing quotes in imports

--- frontend/demo/fusion/lit-basics/minimal.ts
+++ frontend/demo/fusion/lit-basics/minimal.ts
@@ -1,4 +1,4 @@
-import { html, LitElement, TemplateResult } from 'lit;
+import { html, LitElement, TemplateResult } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 @customElement('minimal-view')
