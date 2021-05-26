60cc2105fd70a72112df776344c0885397339195
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2021-05-26 16:32:27 +0300

    Reload on session expiration when included example is loaded

--- frontend/demo/example-resources.ts
+++ frontend/demo/example-resources.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+import { withPrefix } from 'gatsby';
 import './init-flow-namespace';
 import { applyTheme } from 'Frontend/generated/theme';
 // See webpack.config 'externals.Frontend/generated/theme'
@@ -6,3 +8,23 @@ import { applyTheme } from 'Frontend/generated/theme';
 window.__applyTheme = { applyTheme };
 // @ts-ignore
 import('all-flow-imports-or-empty').catch(() => {});
+
+window.addEventListener('included-example-loaded', (e) => {
+  const includeUrl = (e as any).detail;
+  // Make sure example includes Vaadin component
+  if (includeUrl && includeUrl!.indexOf('vaadin') > -1) {
+    // Make sure Vaadin server is up and running
+    fetch(withPrefix(includeUrl)).then((urlFetch) => {
+      if (urlFetch.ok) {
+        fetch(withPrefix('/vaadin/?v-r=heartbeat&v-uiId=0'), {
+          method: 'POST',
+        }).then((data) => {
+          // If session is expired reload the page
+          if (!data.ok) {
+            location.reload();
+          }
+        });
+      }
+    });
+  }
+});
