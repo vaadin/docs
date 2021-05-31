342039f8cb0b7d0fd4f1ffa6604efb14afbda9d8
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2021-05-31 16:57:35 +0300

    Add comments

--- frontend/demo/example-resources.ts
+++ frontend/demo/example-resources.ts
@@ -12,7 +12,9 @@ import('all-flow-imports-or-empty').catch(() => {});
 function testHeartbeat() {
   fetch(withPrefix('/vaadin/?v-r=heartbeat&v-uiId=0'), { method: 'POST' }).then((data) => {
     const reloadTimestamp = localStorage.getItem('reloadTimestamp');
+    // Make sure session is still opened, otherwise reload the page
     if (!data.ok && (!reloadTimestamp || Date.now() - parseInt(reloadTimestamp) > 5 * 60 * 1000)) {
+      // Save the previous reload timestamp to avoid reloading in less than 5 minutes
       localStorage.setItem('reloadTimestamp', Date.now().toString());
       location.reload();
     }
@@ -21,7 +23,9 @@ function testHeartbeat() {
 
 const initialListener = (e: any) => {
   window.removeEventListener('included-example-loaded', initialListener);
+  // Make sure flow example is upgraded and requested content from server
   customElements.whenDefined(e.detail).then(() => {
+    // Make sure server is up and running
     fetch(withPrefix('/vaadin/index.html')).then((serverData) => {
       if (serverData.ok) {
         window.addEventListener('included-example-loaded', () => testHeartbeat());
@@ -31,4 +35,5 @@ const initialListener = (e: any) => {
   });
 };
 
+// Examples are not available when session is expired. Logic prevents that by reloading the page.
 window.addEventListener('included-example-loaded', initialListener);
