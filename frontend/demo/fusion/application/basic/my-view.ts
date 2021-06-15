0031d813c7aaa50936629ff23216a913ea2afad8
Marko Grönroos <magi@iki.fi>
2021-06-15 18:51:28 +0300

    Revised Fusion application basics

---
+++ frontend/demo/fusion/application/basic/my-view.ts
@@ -0,0 +1,31 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, query } from 'lit/decorators.js';
+import '@vaadin/vaadin-button/vaadin-button';
+import '@vaadin/vaadin-notification/vaadin-notification';
+
+@customElement('my-view')
+export class MyView extends LitElement {
+  @query('#notification')
+  private notification: any;
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <vaadin-button theme="primary" @click="${this.clickHandler}"> Click me </vaadin-button>
+      <vaadin-notification id="notification" duration="2000">
+        <template> Hello, World! </template>
+      </vaadin-notification>
+    `;
+  }
+
+  private clickHandler() {
+    this.notification.open();
+  }
+}
---
+++ frontend/demo/fusion/application/basic/reactive-view.ts
@@ -0,0 +1,23 @@
+import 'Frontend/demo/init'; // hidden-source-line
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import '@vaadin/vaadin-button/vaadin-button';
+
+// tag::snippet[]
+@customElement('reactive-view')
+export class ReactiveView extends LitElement {
+  @property({ type: Number })
+  count = 0;
+
+  render() {
+    return html` <div>
+      <div>The button has been clicked ${this.count} times</div>
+      <vaadin-button @click="${this.increment}">Click me!</vaadin-button>
+    </div>`;
+  }
+
+  private increment() {
+    this.count++;
+  }
+}
+// end::snippet[]
