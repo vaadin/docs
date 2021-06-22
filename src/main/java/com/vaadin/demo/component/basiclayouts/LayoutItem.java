f006df314375b81a7eb40b4921fc544741232017
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-06-22 16:23:35 +0200

    Reuse layout-item web component for Java examples

--- src/main/java/com/vaadin/demo/component/basiclayouts/LayoutItem.java
+++ src/main/java/com/vaadin/demo/component/basiclayouts/LayoutItem.java
@@ -1,9 +1,11 @@
 package com.vaadin.demo.component.basiclayouts;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
 
 @Tag("layout-item")
+@JsModule("demo/component/basiclayouts/layout-item.ts")
 public class LayoutItem extends Div {
     public LayoutItem(String label) {
         this(label, false);
@@ -11,13 +13,8 @@ public class LayoutItem extends Div {
 
     public LayoutItem(String label, boolean isDisabled) {
         this.setText(label);
-        getStyle().set("display", "flex").set("align-items", "center")
-                .set("justify-content", "center")
-                .set("padding", "var(--lumo-space-wide-xs)").set("background",
-                isDisabled ?
-                        "var(--lumo-contrast-80pct)" :
-                        "var(--lumo-primary-color)")
-                .set("border-radius", "var(--lumo-border-radius-m)")
-                .set("color", "var(--lumo-base-color)");
+        if (isDisabled) {
+            this.getElement().setAttribute("theme", "inactive");
+        }
     }
 }
