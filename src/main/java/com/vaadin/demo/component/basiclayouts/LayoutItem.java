b2be9f00ef1f164a39ffc1e423696c23277dfda4
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-06-23 10:44:47 +0200

    Make LayoutItem extend from Component

--- src/main/java/com/vaadin/demo/component/basiclayouts/LayoutItem.java
+++ src/main/java/com/vaadin/demo/component/basiclayouts/LayoutItem.java
@@ -1,18 +1,18 @@
 package com.vaadin.demo.component.basiclayouts;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.html.Div;
 
 @Tag("layout-item")
 @JsModule("demo/component/basiclayouts/layout-item.ts")
-public class LayoutItem extends Div {
+public class LayoutItem extends Component {
     public LayoutItem(String label) {
         this(label, false);
     }
 
     public LayoutItem(String label, boolean isDisabled) {
-        this.setText(label);
+        this.getElement().setText(label);
         if (isDisabled) {
             this.getElement().setAttribute("theme", "inactive");
         }
