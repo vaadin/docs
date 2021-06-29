8e33a316b501868e1e0046d2f87653b8f197bce3
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-06-29 11:50:18 +0200

    docs: add grid sorting flow example

---
+++ src/main/java/com/vaadin/demo/component/grid/GridSorting.java
@@ -0,0 +1,45 @@
+package com.vaadin.demo.component.grid;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.demo.domain.DataService;
+import com.vaadin.demo.domain.Person;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.data.renderer.LocalDateRenderer;
+import com.vaadin.flow.router.Route;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+
+@Route("grid-sorting")
+public class GridSorting extends Div {
+
+    public GridSorting() {
+        // tag::snippet[]
+        Grid<Person> grid = new Grid<>(Person.class, false);
+        grid.addColumn(Person::getId).setHeader("Id").setSortable(true);
+        grid.addColumn(Person::getFullName).setHeader("Name").setSortable(true);
+        grid.addColumn(Person::getEmail).setHeader("Email").setSortable(true);
+        grid.addColumn(Person::getProfession).setHeader("Profession")
+                .setSortable(true);
+        grid.addColumn(new LocalDateRenderer<>(GridSorting::getPersonBirthday,
+                "YYYY-MM-dd")).setHeader("Birthday").setSortable(true)
+                .setComparator(Person::getBirthday);
+        // end::snippet[]
+
+        List<Person> people = DataService.getPeople();
+        grid.setItems(people);
+
+        add(grid);
+    }
+
+    private static LocalDate getPersonBirthday(Person person) {
+        return person.getBirthday().toInstant().atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    public static class Exporter // hidden-source-line
+            extends DemoExporter<GridSorting> { // hidden-source-line
+    } // hidden-source-line
+}
