45f85ab271f059dfcc4a74cb0df1b186d5af78c0
Farhad <farhad@vaadin.com>
2021-07-07 15:58:26 +0300

    feat: add first batch of flow Button examples (#624)
    * feat: add first batch of flow Button examples
    
    * address review comments
    
    * use variants instead of strings for adding themes
    
    Co-authored-by: Sascha Ißbrücker <sissbruecker@vaadin.com>

---
+++ src/main/java/com/vaadin/demo/component/button/ButtonContrast.java
@@ -0,0 +1,29 @@
+package com.vaadin.demo.component.button;
+
+import com.vaadin.flow.component.button.Button;
+import static com.vaadin.flow.component.button.ButtonVariant.*;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+
+@Route("button-contrast")
+public class ButtonContrast extends Div {
+    public ButtonContrast() {
+        // tag::snippet[]
+        Button primaryButton = new Button("Primary");
+        primaryButton.addThemeVariants(LUMO_PRIMARY, LUMO_CONTRAST);
+
+        Button secondaryButton = new Button("Secondary");
+        secondaryButton.addThemeVariants(LUMO_CONTRAST);
+
+        Button tertiaryButton = new Button("Tertiary (avoid)");
+        tertiaryButton.addThemeVariants(LUMO_TERTIARY, LUMO_CONTRAST);
+        // end::snippet[]
+        HorizontalLayout horizontalLayout = new HorizontalLayout(primaryButton, secondaryButton, tertiaryButton);
+        add(horizontalLayout);
+    }
+
+    public static class Exporter extends DemoExporter<ButtonContrast> { // hidden-source-line
+    } // hidden-source-line
+}
---
+++ src/main/java/com/vaadin/demo/component/button/ButtonDisabled.java
@@ -0,0 +1,31 @@
+package com.vaadin.demo.component.button;
+
+import com.vaadin.flow.component.button.Button;
+import static com.vaadin.flow.component.button.ButtonVariant.*;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+
+@Route("button-disabled")
+public class ButtonDisabled extends Div {
+    public ButtonDisabled() {
+        // tag::snippet[]
+        Button primaryButton = new Button("Primary");
+        primaryButton.setEnabled(false);
+        primaryButton.addThemeVariants(LUMO_PRIMARY);
+
+        Button secondaryButton = new Button("Secondary");
+        secondaryButton.setEnabled(false);
+
+        Button tertiaryButton = new Button("Tertiary");
+        tertiaryButton.setEnabled(false);
+        tertiaryButton.addThemeVariants(LUMO_TERTIARY);
+        // end::snippet[]
+        HorizontalLayout horizontalLayout = new HorizontalLayout(primaryButton, secondaryButton, tertiaryButton);
+        add(horizontalLayout);
+    }
+
+    public static class Exporter extends DemoExporter<ButtonDisabled> { // hidden-source-line
+    } // hidden-source-line
+}
