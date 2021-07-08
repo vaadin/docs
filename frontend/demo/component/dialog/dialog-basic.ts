78aa59b26e8b65b0d93097fb0c1b9edde8b1e910
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-07-08 09:24:34 +0200

    Add docs: add dialog basic flow example

--- frontend/demo/component/dialog/dialog-basic.ts
+++ frontend/demo/component/dialog/dialog-basic.ts
@@ -1,4 +1,5 @@
 import 'Frontend/demo/init'; // hidden-source-line
+import '@vaadin/vaadin-template-renderer/src/vaadin-template-renderer.js'; // hidden-source-line (Legacy template renderer)
 
 import { html, LitElement, render } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
--- src/main/java/com/vaadin/demo/component/dialog/DialogBasic.java
+++ src/main/java/com/vaadin/demo/component/dialog/DialogBasic.java
@@ -1,25 +1,63 @@
 package com.vaadin.demo.component.dialog;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.demo.DemoExporter; // hidden-source-line
 
 @Route("dialog-basic")
 public class DialogBasic extends Div {
 
-  public DialogBasic() {
-    // tag::snippet[]
-    Dialog dialog = new Dialog();
-    dialog.add(new Text("This simple dialog will close by pressing the Esc key, or by a mouse click anywhere outside the dialog area"));
+    public DialogBasic() {
+        // tag::snippet[]
+        Dialog dialog = new Dialog();
 
-    Button button = new Button("Show dialog", e -> dialog.open());
-    add(dialog, button);
-    // end::snippet[]
-  }
+        VerticalLayout dialogContent = createDialogContent(dialog::close);
+        dialog.add(dialogContent);
 
-  public static class Exporter extends DemoExporter<DialogBasic> { // hidden-source-line
-  } // hidden-source-line
+        Button button = new Button("Show dialog", e -> dialog.open());
+        add(dialog, button);
+        // end::snippet[]
+    }
+
+    private static VerticalLayout createDialogContent(Runnable closeHandler) {
+        H2 headline = new H2("New employee");
+        headline.getStyle().set("margin", "var(--lumo-space-m) 0 0 0")
+                .set("font-size", "1.5em").set("font-weight", "bold");
+
+        TextField firstNameField = new TextField("First name");
+        TextField lastNameField = new TextField("Last name");
+        VerticalLayout fieldLayout = new VerticalLayout(firstNameField,
+                lastNameField);
+        fieldLayout.setSpacing(false);
+        fieldLayout.setPadding(false);
+        fieldLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+
+        Button cancelButton = new Button("Cancel", e -> closeHandler.run());
+        Button saveButton = new Button("Save", e -> closeHandler.run());
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        HorizontalLayout buttonLayout = new HorizontalLayout(cancelButton,
+                saveButton);
+        buttonLayout
+                .setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+
+        VerticalLayout dialogLayout = new VerticalLayout(headline, fieldLayout,
+                buttonLayout);
+        dialogLayout.setPadding(false);
+        dialogLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        dialogLayout.getStyle().set("width", "300px").set("max-width", "100%");
+
+        return dialogLayout;
+    }
+
+    public static class Exporter // hidden-source-line
+            extends DemoExporter<DialogBasic> { // hidden-source-line
+    } // hidden-source-line
 }
