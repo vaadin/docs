683fe3887ebf7b786753a4cb5d94ef7a5bc599b0
Farhad <farhad@vaadin.com>
2021-07-09 14:33:27 +0300

    remove JsModule annotation
    somehow it was preventing lumo styles of vaadin-progress-bar from
    loading properly

--- src/main/java/com/vaadin/demo/component/button/FakeProgressBar.java
+++ src/main/java/com/vaadin/demo/component/button/FakeProgressBar.java
@@ -2,11 +2,9 @@ package com.vaadin.demo.component.button;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.dom.DomEventListener;
 
 @Tag("fake-progress-bar")
-@JsModule("demo/component/button/fake-progress-bar.ts")
 public class FakeProgressBar extends Component {
     public void simulateProgress() {
         this.getElement().callJsFunction("simulateProgress");
