1eda174fd89105fbb4c2b3a05fcaabdeceb8138f
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-07-13 15:57:01 +0200

    add upload basic drag and drop flow example

---
+++ src/main/java/com/vaadin/demo/component/upload/UploadDragAndDrop.java
@@ -0,0 +1,49 @@
+package com.vaadin.demo.component.upload;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
+import com.vaadin.flow.router.Route;
+
+@Route("upload-drag-and-drop")
+public class UploadDragAndDrop extends Div {
+
+    public UploadDragAndDrop() {
+        MemoryBuffer buffer1 = new MemoryBuffer();
+        MemoryBuffer buffer2 = new MemoryBuffer();
+
+        // tag::snippet[]
+        Upload dropEnabledUpload = new Upload(buffer1);
+        dropEnabledUpload.setDropAllowed(true);
+
+        Upload dropDisabledUpload = new Upload(buffer2);
+        dropDisabledUpload.setDropAllowed(false);
+        // end::snippet[]
+
+        Label dropEnabledLabel = new Label("Drag and drop enabled");
+        dropEnabledLabel.getStyle().set("font-weight", "bold");
+        dropEnabledUpload.setId("upload-drop-enabled");
+        dropEnabledLabel.setFor(dropEnabledUpload.getId().get());
+
+        Label dropDisabledLabel = new Label("Drag and drop disabled");
+        dropDisabledLabel.getStyle().set("font-weight", "bold");
+        dropDisabledUpload.setId("upload-drop-disabled");
+        dropDisabledLabel.setFor(dropDisabledUpload.getId().get());
+
+        Div leftSection = new Div(dropEnabledLabel, dropEnabledUpload);
+        Div rightSection = new Div(dropDisabledLabel, dropDisabledUpload);
+
+        FormLayout formLayout = new FormLayout(leftSection, rightSection);
+        formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep("520px", 2));
+
+        add(formLayout);
+    }
+
+    public static class Exporter // hidden-source-line
+            extends DemoExporter<UploadDragAndDrop> { // hidden-source-line
+    } // hidden-source-line
+}
