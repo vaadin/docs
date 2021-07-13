803778f7c0223ea8f87316ee929c98734eb21cf6
Sergey Vinogradov <mr.vursen@gmail.com>
2021-07-13 09:40:08 +0300

    docs: use horizontal layout in notification link example

--- frontend/demo/component/notification/notification-link.ts
+++ frontend/demo/component/notification/notification-link.ts
@@ -1,4 +1,5 @@
 import 'Frontend/demo/init'; // hidden-source-line
+import '@vaadin/vaadin-template-renderer/src/vaadin-template-renderer'; // hidden-source-line (Legacy template renderer)
 import { html, LitElement, render } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { guard } from 'lit/directives/guard.js';
@@ -6,12 +7,17 @@ import '@vaadin/vaadin-icon/vaadin-icon';
 import '@vaadin/vaadin-lumo-styles/vaadin-iconset';
 import '@vaadin/vaadin-button/vaadin-button';
 import '@vaadin/vaadin-notification/vaadin-notification';
+import {
+  NotificationOpenedChangedEvent,
+  NotificationRenderer,
+} from '@vaadin/vaadin-notification/vaadin-notification';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
 import { applyTheme } from 'Frontend/generated/theme';
 
 @customElement('notification-link')
 export class Example extends LitElement {
   @state()
-  private notificationOpen = false;
+  private notificationOpened = false;
 
   protected createRenderRoot() {
     const root = super.createRenderRoot();
@@ -23,32 +29,40 @@ export class Example extends LitElement {
   render() {
     return html`
       <vaadin-button
-        @click="${() => (this.notificationOpen = true)}"
-        .disabled="${this.notificationOpen}"
+        @click="${() => (this.notificationOpened = true)}"
+        .disabled="${this.notificationOpened}"
       >
         Try it
       </vaadin-button>
 
       <!-- tag::snippet[] -->
       <vaadin-notification
-        .opened="${this.notificationOpen}"
-        @opened-changed="${(e: any) => (this.notificationOpen = e.detail.value)}"
-        .renderer="${guard([], () => (root: HTMLElement) => {
-          render(
-            html`
-              <div>Jason Bailey mentioned you in <a href="#">Project Q4</a></div>
-              <vaadin-button
-                theme="tertiary-inline"
-                @click="${() => (this.notificationOpen = false)}"
-                aria-label="Close"
-              >
-                <vaadin-icon icon="lumo:cross"></vaadin-icon>
-              </vaadin-button>
-            `,
-            root
-          );
-        })}"
+        duration="0"
         position="middle"
+        .opened="${this.notificationOpened}"
+        @opened-changed="${(e: NotificationOpenedChangedEvent) => {
+          this.notificationOpened = e.detail.value;
+        }}"
+        .renderer="${guard(
+          [],
+          (): NotificationRenderer => (root) => {
+            render(
+              html`
+                <vaadin-horizontal-layout theme="spacing" style="align-items: center;">
+                  <div>Jason Bailey mentioned you in <a href="#">Project Q4</a></div>
+                  <vaadin-button
+                    theme="tertiary-inline"
+                    aria-label="Close"
+                    @click="${() => (this.notificationOpened = false)}"
+                  >
+                    <vaadin-icon icon="lumo:cross"></vaadin-icon>
+                  </vaadin-button>
+                </vaadin-horizontal-layout>
+              `,
+              root
+            );
+          }
+        )}"
       ></vaadin-notification>
       <!-- end::snippet[] -->
     `;
