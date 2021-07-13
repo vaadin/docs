d95b8a8ec883a85fc7f4b9e39e7e9a13f4a93817
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-07-13 13:27:17 +0200

    improve upload usage sections

--- src/main/java/com/vaadin/demo/component/upload/UploadMemoryBuffer.java
+++ src/main/java/com/vaadin/demo/component/upload/UploadMemoryBuffer.java
@@ -3,24 +3,46 @@ package com.vaadin.demo.component.upload;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 
 import java.io.InputStream;
 
 public class UploadMemoryBuffer extends Div {
     public UploadMemoryBuffer() {
         // tag::snippet[]
+        /* Example for MemoryBuffer */
         MemoryBuffer memoryBuffer = new MemoryBuffer();
-        Upload upload = new Upload(memoryBuffer);
+        Upload singleFileUpload = new Upload(memoryBuffer);
 
-        upload.addSucceededListener(event -> {
+        singleFileUpload.addSucceededListener(event -> {
+            // Get information about the uploaded file
             InputStream fileData = memoryBuffer.getInputStream();
             String fileName = event.getFileName();
             long contentLength = event.getContentLength();
             String mimeType = event.getMIMEType();
 
-            // Do something with the uploaded file
+            // Do something with the file data
+            // processFile(fileData, fileName, contentLength, mimeType);
+        });
+
+        /* Example for MultiFileMemoryBuffer */
+        MultiFileMemoryBuffer multiFileMemoryBuffer = new MultiFileMemoryBuffer();
+        Upload multiFileUpload = new Upload(multiFileMemoryBuffer);
+
+        multiFileUpload.addSucceededListener(event -> {
+            // Determine which file was uploaded
+            String fileName = event.getFileName();
+
+            // Get input stream specifically for the finished file
+            InputStream fileData = multiFileMemoryBuffer.getInputStream(fileName);
+            long contentLength = event.getContentLength();
+            String mimeType = event.getMIMEType();
+
+            // Do something with the file data
             // processFile(fileData, fileName, contentLength, mimeType);
         });
         // end::snippet[]
+
+        add(singleFileUpload, multiFileUpload);
     }
 }
