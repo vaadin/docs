26b3c99f16cbfe500cd73fe3ce1209b104e91431
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-07-15 11:32:33 +0200

    add upload file size flow example

--- frontend/demo/component/upload/upload-file-size.ts
+++ frontend/demo/component/upload/upload-file-size.ts
@@ -1,11 +1,12 @@
 import 'Frontend/demo/init'; // hidden-source-line
 import './upload-demo-helpers'; // hidden-source-line
 import { css, html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, query } from 'lit/decorators.js';
 import { showErrorNotification } from 'Frontend/demo/notification-helper';
 import '@vaadin/vaadin-upload/vaadin-upload';
 import type { UploadFileRejectEvent } from '@vaadin/vaadin-upload/vaadin-upload';
 import { applyTheme } from 'Frontend/generated/theme';
+import { UploadElement } from '@vaadin/vaadin-upload';
 
 @customElement('upload-file-size')
 export class Example extends LitElement {
@@ -28,9 +29,19 @@ export class Example extends LitElement {
     return root;
   }
 
+  @query('vaadin-upload')
+  private upload?: UploadElement;
+
+  firstUpdated() {
+    if (this.upload?.i18n) {
+      this.upload.i18n.error.fileIsTooBig = 'The file exceeds the maximum allowed size of 10MB.';
+      this.upload.i18n = { ...this.upload.i18n };
+    }
+  }
+
   // tag::snippet[]
   render() {
-    const maxFileSizeInMB = 100;
+    const maxFileSizeInMB = 10;
     const maxFileSizeInBytes = maxFileSizeInMB * 1024 * 1024;
     return html`
       <h4>Upload file</h4>
@@ -38,13 +49,10 @@ export class Example extends LitElement {
       <vaadin-upload
         max-files="1"
         .maxFileSize="${maxFileSizeInBytes}"
-        @file-reject="${this.fileRejectHandler}"
+        @file-reject="${(event: UploadFileRejectEvent) =>
+          showErrorNotification(event.detail.error)}"
       ></vaadin-upload>
     `;
   }
   // end::snippet[]
-
-  fileRejectHandler(event: UploadFileRejectEvent) {
-    showErrorNotification(`Error: ${event.detail.error} '${event.detail.file.name}'`);
-  }
 }
