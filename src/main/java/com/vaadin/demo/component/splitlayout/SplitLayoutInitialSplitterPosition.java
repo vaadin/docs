7edd56e03a4e7026c075744c76c200ad531ee9dd
Farhad <farhad@vaadin.com>
2021-07-20 10:53:45 +0300

    feat: add SplitLayout Flow examples (#648)
    * feat: add SplitLayout Flow examples
    
    * remove redundant theme variant
    
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>
    
    * use setSplitterPosition
    
    * improve setup for initial splitter position example
    
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>
    Co-authored-by: Sascha Ißbrücker <sascha.issbruecker@googlemail.com>

--- src/main/java/com/vaadin/demo/component/splitlayout/SplitLayoutInitialSplitterPosition.java
+++ src/main/java/com/vaadin/demo/component/splitlayout/SplitLayoutInitialSplitterPosition.java
@@ -1,8 +1,6 @@
 package com.vaadin.demo.component.splitlayout;
 
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.demo.DemoExporter; // hidden-source-line
@@ -12,9 +10,17 @@ public class SplitLayoutInitialSplitterPosition extends Div {
 
     public SplitLayoutInitialSplitterPosition() {
         // tag::snippet[]
-        SplitLayout splitLayout = new SplitLayout(new Span("Side 1"), new Span("Side 2"));
-        add(splitLayout);
+        MasterContent master = new MasterContent();
+        DetailContent detail = new DetailContent();
+
+        SplitLayout splitLayout = new SplitLayout(master, detail);
+        // Sets the width for the first child to 70%, giving
+        // the second child the remaining width of 30%
+        splitLayout.setSplitterPosition(70);
         // end::snippet[]
+
+        splitLayout.setMaxHeight("280px");
+        add(splitLayout);
     }
 
     public static class Exporter extends DemoExporter<SplitLayoutInitialSplitterPosition> { // hidden-source-line
--- src/main/java/com/vaadin/demo/component/splitlayout/SplitLayoutThemeVariants.java
+++ src/main/java/com/vaadin/demo/component/splitlayout/SplitLayoutThemeVariants.java
@@ -1,9 +1,8 @@
 package com.vaadin.demo.component.splitlayout;
 
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayoutVariant;
 import com.vaadin.flow.router.Route;
 import com.vaadin.demo.DemoExporter; // hidden-source-line
 
@@ -12,9 +11,15 @@ public class SplitLayoutThemeVariants extends Div {
 
     public SplitLayoutThemeVariants() {
         // tag::snippet[]
-        SplitLayout splitLayout = new SplitLayout(new Span("Side 1"), new Span("Side 2"));
-        add(splitLayout);
+        MasterContent master = new MasterContent();
+        DetailContent detail = new DetailContent();
+
+        SplitLayout splitLayout = new SplitLayout(master, detail);
+        splitLayout.addThemeVariants(SplitLayoutVariant.LUMO_SMALL);
         // end::snippet[]
+
+        splitLayout.setMaxHeight("280px");
+        add(splitLayout);
     }
 
     public static class Exporter extends DemoExporter<SplitLayoutThemeVariants> { // hidden-source-line
