95dc48251288a614ea46a946364aa1dec8cb671a
Marko Grönroos <magi@iki.fi>
2021-08-18 13:59:18 +0300

    Fixed imports

--- frontend/demo/fusion/application/basic/my-view.ts
+++ frontend/demo/fusion/application/basic/my-view.ts
@@ -1,7 +1,7 @@
 import 'Frontend/demo/init'; // hidden-source-line
 
-import { css, html, LitElement } from 'lit';
-import { customElement, query } from 'lit/decorators.js';
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
 import '@vaadin/vaadin-button/vaadin-button';
 import '@vaadin/vaadin-notification/vaadin-notification';
 
