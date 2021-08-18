11f5c5a39237ed0723bb2bac295ba7b9fab1a004
Marko Grönroos <magi@iki.fi>
2021-08-18 13:18:56 +0300

    Review fixes

--- frontend/demo/fusion/application/basic/my-view.ts
+++ frontend/demo/fusion/application/basic/my-view.ts
@@ -8,28 +8,12 @@ import '@vaadin/vaadin-notification/vaadin-notification';
 // tag::snippet[]
 @customElement('my-view')
 export class MyView extends LitElement {
-  @query('#notification')
-  private notification: any;
-
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-      }
-    `;
-  }
-
   render() {
-    return html`
-      <vaadin-button theme="primary" @click="${this.clickHandler}"> Click me </vaadin-button>
-      <vaadin-notification id="notification" duration="2000">
-        <template>Hello, World!</template>
-      </vaadin-notification>
-    `;
+    return html`<vaadin-button @click="${this.sayHello}">Say hello</vaadin-button>`;
   }
 
-  private clickHandler() {
-    this.notification.open();
+  private sayHello() {
+    alert('Hello');
   }
 }
 // end::snippet[]
