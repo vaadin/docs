aeaa5a022dfd083d4b27e8fdf038ab119e01150e
Jouni Koivuviita <jouni@vaadin.com>
2021-08-18 17:05:33 +0300

    Fixed and improved Antlers integration

--- dspublisher/theme/init.js
+++ dspublisher/theme/init.js
@@ -5,7 +5,7 @@ const https = require('https');
 async function fetchAntlersStyleSheets() {
   // Full collection of available style sheets: https://gitlab.vaadin.com/vaadincom/antlers
   const antlersStyleSheets = [
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/custom-properties.css', 
+    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/custom-properties.css',
     'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/reset.css',
     'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/layout.css',
     'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/typography.css',
@@ -18,37 +18,34 @@ async function fetchAntlersStyleSheets() {
     'https://cdn.vaadin.com/website/antlers/v2/assets/css/3-patterns/navigation.css',
     'https://cdn.vaadin.com/website/antlers/v2/assets/css/5-sections/addsearch.css',
     'https://cdn.vaadin.com/website/antlers/v2/assets/css/5-sections/cookie.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/3-patterns/card.css'
+    'https://cdn.vaadin.com/website/antlers/v2/assets/css/3-patterns/card.css',
   ];
 
   // No try-catch, let the build fail if there is an error fetching the style sheets
   for (let i = 0; i < antlersStyleSheets.length; i++) {
     const url = antlersStyleSheets[i];
-    const css = await new Promise(resolve => {
+    const css = await new Promise((resolve) => {
       https
-        .get(url, res => {
+        .get(url, (res) => {
           res.setEncoding('utf8');
           let body = '';
-          res.on('data', data => {
+          res.on('data', (data) => {
             body += data;
           });
           res.on('end', () => {
             resolve(body);
           });
         })
-        .on('error', e => {
+        .on('error', (e) => {
           console.error(e);
           throw new Error(`Failed to fetch Antlers stylesheet "${url}"`);
         });
     });
 
     let localStyleSheetPath =
-      './styles/antlers/' + url.split('https://cdn.vaadin.com/website/antlers/v2/')[1];
+      './antlers/' + url.split('https://cdn.vaadin.com/website/antlers/v2/')[1];
 
-    const localStyleSheetDir = localStyleSheetPath
-      .split('/')
-      .slice(0, -1)
-      .join('/');
+    const localStyleSheetDir = localStyleSheetPath.split('/').slice(0, -1).join('/');
     fs.mkdirSync(path.resolve(__dirname, localStyleSheetDir), { recursive: true });
 
     await fs.writeFileSync(path.resolve(__dirname, localStyleSheetPath), css);
