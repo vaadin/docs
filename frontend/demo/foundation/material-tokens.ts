2589da1ba6e3b44b2feab06d76e4cf0bcd09c1ce
Jouni Koivuviita <jouni@vaadin.com>
2021-08-26 16:08:54 +0300

    Dark mode support (v20) (#713)

--- frontend/demo/foundation/material-tokens.ts
+++ frontend/demo/foundation/material-tokens.ts
@@ -5,4 +5,7 @@ import '@vaadin/vaadin-material-styles/color';
 // tag::typography[]
 import '@vaadin/vaadin-material-styles/typography';
 // end::typography[]
+import { includeModule } from './include-module'; // hidden-source-line
+// prettier-ignore
+includeModule('material-color-dark', (css) => css.replace(':host', 'html[theme~="dark"]').replace('background-color: var(--material-background-color);', '').replace('color: var(--material-body-text-color);', '')); // hidden-source-line
 window.dispatchEvent(new CustomEvent('custom-properties-changed')); // hidden-source-line
