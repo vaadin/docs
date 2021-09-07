b706b0cb97071bb1382df9bd440db6db4b1a6fab
Giovanni Lovato <giovanni@lova.to>
2021-09-07 14:20:58 +0200

    CE: Add MessageManager documentation

---
+++ src/main/java/com/vaadin/demo/ce/MessageManagerExample.java
@@ -0,0 +1,46 @@
+package com.vaadin.demo.ce;
+
+import com.vaadin.collaborationengine.CollaborationMessage;
+import com.vaadin.collaborationengine.MessageManager;
+import com.vaadin.collaborationengine.UserInfo;
+import com.vaadin.flow.component.messages.MessageInput;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+/**
+ * Code snippets used in MessageManager's reference documentation.
+ */
+public class MessageManagerExample extends VerticalLayout {
+
+    public MessageManagerExample() {
+        // tag::snippet[]
+        UserInfo localUser = new UserInfo("john");
+        String topicId = "notifications";
+
+        MessageManager messageManager = new MessageManager(this, localUser,
+                topicId); // <1>
+
+        messageManager.setNewMessageHandler(context -> {
+            CollaborationMessage message = context.getMessage();
+            UserInfo user = message.getUser();
+            String text = message.getText();
+
+            Notification.show(user.getName() + ": " + text);
+        }); // <2>
+
+        add(new MessageInput(event -> {
+            String text = event.getValue();
+            messageManager.submit(text);
+        })); // <3>
+        // end::snippet[]
+    }
+
+    void persisterExample(MyMessagePersister persister) {
+        UserInfo localUser = new UserInfo("john");
+        String topicId = "notifications";
+        // tag::persister[]
+        MessageManager messageManager = new MessageManager(this, localUser,
+                topicId, persister);
+        // end::persister[]
+    }
+}
