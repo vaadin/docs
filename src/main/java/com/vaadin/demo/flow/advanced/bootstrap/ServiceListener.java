8bf076f84d8533a2029c882de55638e033a5c38d
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2021-09-15 11:36:09 +0300

    fix: update outdated doc for modifying bootstrap page (#733)
    * fix: update outdated doc for modifying bootstrap page
    
    * title case
    
    * move java code to separate file
    
    * move java code to separate file
    
    * fix broken link
    
    * fix broken link
    
    * fix broken internal reference
    
    * fixes
    
    * fix internal references
    
    * hid import and package
    
    * fix vale errors
    
    * fix internal reference link
    
    * fix Vale errors
    
    * address review comment

---
+++ src/main/java/com/vaadin/demo/flow/advanced/bootstrap/ServiceListener.java
@@ -0,0 +1,28 @@
+package com.vaadin.demo.flow.advanced.bootstrap;
+
+import com.vaadin.flow.component.PushConfiguration;
+import com.vaadin.flow.component.ReconnectDialogConfiguration;
+import com.vaadin.flow.component.page.LoadingIndicatorConfiguration;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.shared.communication.PushMode;
+
+// tag::snippet[]
+public class ServiceListener implements VaadinServiceInitListener{
+
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        event.getSource().addUIInitListener(uiInitEvent -> {
+            LoadingIndicatorConfiguration indicator = uiInitEvent.getUI().getLoadingIndicatorConfiguration();
+            indicator.setApplyDefaultTheme(false);
+            indicator.setSecondDelay(700000);
+
+            PushConfiguration push = uiInitEvent.getUI().getPushConfiguration();
+            push.setPushMode(PushMode.AUTOMATIC);
+            
+            ReconnectDialogConfiguration dialog = uiInitEvent.getUI().getReconnectDialogConfiguration();
+            dialog.setDialogText("reconnecting...");
+        });
+    }
+}
+// end::snippet[]
