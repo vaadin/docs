976af042cdff03033d305cf393071151f70913b8
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-09-22 08:33:33 +0200

    docs: add menubar section for internationalization

---
+++ frontend/demo/component/menubar/menu-bar-internationalization.ts
@@ -0,0 +1,52 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import { html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import '@vaadin/vaadin-menu-bar/vaadin-menu-bar';
+import '@vaadin/vaadin-split-layout/vaadin-split-layout';
+import { applyTheme } from 'Frontend/generated/theme';
+import { MenuBarI18n } from '@vaadin/vaadin-menu-bar';
+
+@customElement('menu-bar-internationalization')
+export class Example extends LitElement {
+  protected createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  @state()
+  private items = [
+    { text: 'View' },
+    { text: 'Edit' },
+    {
+      text: 'Share',
+      children: [
+        {
+          text: 'On social media',
+          children: [{ text: 'Facebook' }, { text: 'Twitter' }, { text: 'Instagram' }],
+        },
+        { text: 'By email' },
+        { text: 'Get link' },
+      ],
+    },
+    {
+      text: 'Move',
+      children: [{ text: 'To folder' }, { text: 'To trash' }],
+    },
+    { text: 'Duplicate' },
+  ];
+
+  render() {
+    // tag::snippet[]
+    const customI18n: MenuBarI18n = {
+      // Provide accessible label for the overflow menu button
+      // to screen readers
+      moreOptions: 'More actions',
+    };
+
+    return html` <vaadin-menu-bar .i18n="${customI18n}" .items="${this.items}"></vaadin-menu-bar> `;
+    // end::snippet[]
+  }
+}
