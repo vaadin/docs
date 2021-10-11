b87f3ef7dbf3e947733eeda664bab342135c666d
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-11 18:24:07 +0200

    fix demo init module

--- frontend/demo/init.ts
+++ frontend/demo/init.ts
@@ -1,20 +1,7 @@
-import '@vaadin/flow-frontend/dndConnector-es6.js';
-import '@vaadin/flow-frontend/flow-component-renderer.js';
-// @ts-ignore
-// import Appointment from '../generated/com/vaadin/demo/domain/Appointment';
-// import AppointmentModel from '../generated/com/vaadin/demo/domain/AppointmentModel';
+import './init-flow-namespace';
+import './init-flow-components';
 // import client from '../generated/connect-client.default';
-// @ts-ignore
-client.prefix = __VAADIN_CONNECT_PREFIX__;
-
-// The connectors require window.Vaadin.Flow namespace to exists
-// @ts-ignore
-window.Vaadin = window.Vaadin || {};
-// @ts-ignore
-window.Vaadin.Flow = window.Vaadin.Flow || {};
-
-// @ts-ignore Workaround a Vaadin issue
-// AppointmentModel.createEmptyValue = () => Appointment;
+// client.prefix = __VAADIN_CONNECT_PREFIX__;
 
 document.body.style.setProperty('--docs-example-render-font-family', 'var(--lumo-font-family)');
 document.body.style.setProperty('--docs-example-render-color', 'var(--lumo-body-text-color)');
