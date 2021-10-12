32969086b128d34729c0a43580a6c06075a0ea88
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-12 10:44:19 +0200

    add accordion docs and demos

---
+++ frontend/demo/component/accordion/accordion-basic.ts
@@ -0,0 +1,6 @@
+import '../../init';
+
+import '@vaadin/vaadin-accordion/vaadin-accordion';
+import '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout';
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+
---
+++ src/main/java/com/vaadin/demo/component/accordion/AccordionFilledPanels.java
@@ -0,0 +1,62 @@
+package com.vaadin.demo.component.accordion;
+
+import com.vaadin.demo.DemoExporter;
+import com.vaadin.flow.component.accordion.Accordion;
+import com.vaadin.flow.component.accordion.AccordionPanel;
+import com.vaadin.flow.component.details.DetailsVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("accordion-filled-panels")
+public class AccordionFilledPanels extends Div {
+
+    public AccordionFilledPanels() {
+        // tag::snippet[]
+        Accordion accordion = new Accordion();
+        // end::snippet[]
+
+        Span name = new Span("Sophia Williams");
+        Span email = new Span("sophia.williams@company.com");
+        Span phone = new Span("(501) 555-9128");
+
+        VerticalLayout personalInformationLayout = new VerticalLayout(name, email, phone);
+        personalInformationLayout.setSpacing(false);
+        personalInformationLayout.setPadding(false);
+
+        // tag::snippet[]
+        AccordionPanel personalInfoPanel = accordion.add("Personal information", personalInformationLayout);
+        personalInfoPanel.addThemeVariants(DetailsVariant.FILLED);
+        // end::snippet[]
+
+        Span street = new Span("4027 Amber Lake Canyon");
+        Span zipCode = new Span("72333-5884 Cozy Nook");
+        Span city = new Span("Arkansas");
+
+        VerticalLayout billingAddressLayout = new VerticalLayout();
+        billingAddressLayout.setSpacing(false);
+        billingAddressLayout.setPadding(false);
+        billingAddressLayout.add(street, zipCode, city);
+        
+        AccordionPanel billingAddressPanel = accordion.add("Billing address", billingAddressLayout);
+        billingAddressPanel.addThemeVariants(DetailsVariant.FILLED);
+
+        Span cardBrand = new Span("Mastercard");
+        Span cardNumber = new Span("1234 5678 9012 3456");
+        Span expiryDate = new Span("Expires 06/21");
+
+        VerticalLayout paymentLayout = new VerticalLayout();
+        paymentLayout.setSpacing(false);
+        paymentLayout.setPadding(false);
+        paymentLayout.add(cardBrand, cardNumber, expiryDate);
+        
+        AccordionPanel paymentPanel = accordion.add("Payment", paymentLayout);
+        paymentPanel.addThemeVariants(DetailsVariant.FILLED);
+
+        add(accordion);
+    }
+
+    public static class Exporter extends DemoExporter<AccordionFilledPanels> { // hidden-source-line
+    } // hidden-source-line
+}
