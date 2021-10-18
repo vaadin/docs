a15539904fdb255812a7b2efd4acf442b4353bc1
Sascha Ißbrücker <sissbruecker@vaadin.com>
2021-10-18 11:16:53 +0200

    fix: Typescript compiler errors in 14 (#818)
    * remove DataService.ts that referenced generated Fusion classes that did not exist in 14
    * remove all TS demos that were not yet ported to 14, as those reference generated classes and DataService
    * remove unused index files
    * add TS declaration for theme module

--- frontend/demo/init.ts
+++ frontend/demo/init.ts
@@ -1,7 +1,6 @@
 import './init-flow-namespace';
 import './init-flow-components';
 
-// @ts-ignore
 import { applyTheme } from 'Frontend/generated/theme';
 
 // Apply the theme, so that overlay elements styles and custom property overrides work as expected
