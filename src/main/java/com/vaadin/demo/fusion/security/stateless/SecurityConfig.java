a2eff9221484815327d966243e63388a7e76bf41
Anton Platonov <anton@vaadin.com>
2021-10-22 14:59:28 +0300

    fix: stateless Fusion article DX findings (#837)
    * fix: stateless Fusion article DX findings
    
    Fixes #806
    
    * Add admonition title
    
    * Fix admonition title
    
    * Language review
    
    Co-authored-by: Marko Grönroos <magi@iki.fi>

---
+++ src/main/java/com/vaadin/demo/fusion/security/stateless/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.vaadin.demo.fusion.security.stateless;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+
+// tag::stateless-configure[]
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
+
+    @Value("${my.app.auth.secret}")
+    private String authSecret;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // end::stateless-configure[]
+        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/public").permitAll();
+
+        // tag::stateless-configure[]
+        super.configure(http);
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        setLoginView(http, "/login");
+
+        setStatelessAuthentication(http,
+                new SecretKeySpec(Base64.getDecoder().decode(authSecret), // <1>
+                        JwsAlgorithms.HS256), // <2>
+                "com.example.application" // <3>
+        );
+    }
+}
+// end::stateless-configure[]
