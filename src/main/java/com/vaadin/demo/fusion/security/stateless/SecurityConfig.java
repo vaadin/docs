3c72abdf69177b01a9de1241495b8c110b51fdef
Anton Platonov <platosha@gmail.com>
2021-10-25 14:49:28 +0300

    fix: ignore SecurityConfig.java example in Spring Boot demo application
    This fixes the Spring Boot application startup previously broken
    because of the example SecurityConfig.java conflicting with the
    actual application security config.

--- src/main/java/com/vaadin/demo/fusion/security/stateless/SecurityConfig.java
+++ src/main/java/com/vaadin/demo/fusion/security/stateless/SecurityConfig.java
@@ -10,11 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
 
+import com.vaadin.demo.ExcludeDemoSpringComponent;
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 
 // tag::stateless-configure[]
 @EnableWebSecurity
 @Configuration
+@ExcludeDemoSpringComponent // hidden-source-line
 public class SecurityConfig extends VaadinWebSecurityConfigurerAdapter {
 
     @Value("${my.app.auth.secret}")
