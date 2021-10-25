0f356ac8f64240b9baf4fc78b813b3118b7ad94b
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-25 10:54:48 +0200

    docs: port date picker component docs to 14

---
+++ frontend/demo/component/datepicker/date-picker-date-range.ts
@@ -0,0 +1,4 @@
+import '../../init';
+
+import '@vaadin/vaadin-date-picker/vaadin-date-picker';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
---
+++ frontend/demo/component/datepicker/date-picker-individual-input-fields.ts
@@ -0,0 +1,4 @@
+import '../../init';
+
+import '@vaadin/vaadin-combo-box/vaadin-combo-box';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
