da9f1617fff5b72e908acecdc55144b5d4c31aaa
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-26 09:57:45 +0200

    docs: port details component docs to 14

---
+++ frontend/demo/component/details/details-small.ts
@@ -0,0 +1,3 @@
+import '../../init';
+
+import '@vaadin/vaadin-details/vaadin-details';
---
+++ frontend/demo/component/details/details-summary.ts
@@ -0,0 +1,10 @@
+import '../../init';
+
+import '@vaadin/vaadin-details/vaadin-details';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-form-layout/vaadin-form-layout';
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import '@vaadin/vaadin-combo-box/vaadin-combo-box';
+import '@vaadin/vaadin-lumo-styles/sizing';
+import '@vaadin/vaadin-lumo-styles/color';
+import '@vaadin/vaadin-icons/vaadin-icons';
