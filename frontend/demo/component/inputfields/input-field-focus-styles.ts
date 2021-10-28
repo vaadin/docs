c29225e9f520465807a0e947a4a83ba16954a69c
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-28 10:41:07 +0200

    docs: port input field docs to 14

---
+++ frontend/demo/component/inputfields/input-field-focus-styles.ts
@@ -0,0 +1,27 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import { customElement, html, LitElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-text-field';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('input-field-focus-styles')
+export class Example extends LitElement {
+    protected createRenderRoot() {
+        const root = super.createRenderRoot();
+        // Apply custom theme (only supported if your app uses one)
+        applyTheme(root);
+        return root;
+    }
+
+    render() {
+        return html`
+      <vaadin-horizontal-layout theme="spacing">
+        <!-- tag::snippet[] -->
+        <vaadin-text-field label="Pointer focus" focused></vaadin-text-field>
+        <vaadin-text-field label="Keyboard focus" focused focus-ring></vaadin-text-field>
+        <!-- end::snippet[] -->
+      </vaadin-horizontal-layout>
+    `;
+    }
+}
---
+++ frontend/demo/component/inputfields/input-field-required.ts
@@ -0,0 +1,5 @@
+import '../../init';
+
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-date-picker';
