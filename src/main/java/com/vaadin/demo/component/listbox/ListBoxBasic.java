eb3439d374bc41b7d1bbf45482fafe65a0502e54
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-28 16:38:45 +0200

    docs: port list box docs to 14

---
+++ src/main/java/com/vaadin/demo/component/listbox/ListBoxBasic.java
@@ -0,0 +1,26 @@
+package com.vaadin.demo.component.listbox;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.listbox.MultiSelectListBox;
+import com.vaadin.flow.router.Route;
+
+@Route("list-box-basic")
+public class ListBoxBasic extends Div {
+
+    private final String SHOW_ASSIGNEE = "Show assignee";
+    private final String SHOW_DUE_DATE = "Show due date";
+    private final String SHOW_STATUS = "Show status";
+
+    public ListBoxBasic() {
+        // tag::snippet[]
+        MultiSelectListBox<String> listBox = new MultiSelectListBox<>();
+        listBox.setItems(SHOW_ASSIGNEE, SHOW_DUE_DATE, SHOW_STATUS);
+        listBox.select(SHOW_ASSIGNEE, SHOW_STATUS);
+        // end::snippet[]
+        add(listBox);
+    }
+
+    public static class Exporter extends DemoExporter<ListBoxBasic> { // hidden-source-line
+    } // hidden-source-line
+}
---
+++ src/main/java/com/vaadin/demo/component/listbox/ListBoxSingleSelection.java
@@ -0,0 +1,26 @@
+package com.vaadin.demo.component.listbox;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.listbox.ListBox;
+import com.vaadin.flow.router.Route;
+
+@Route("list-box-single-selection")
+public class ListBoxSingleSelection extends Div {
+
+    private final String IN_PROGRESS = "In progress";
+    private final String DONE = "Done";
+    private final String CANCELLED = "Cancelled";
+
+    public ListBoxSingleSelection() {
+        // tag::snippet[]
+        ListBox<String> listBox = new ListBox<>();
+        listBox.setItems(IN_PROGRESS, DONE, CANCELLED);
+        listBox.setValue(IN_PROGRESS);
+        // end::snippet[]
+        add(listBox);
+    }
+
+    public static class Exporter extends DemoExporter<ListBoxSingleSelection> { // hidden-source-line
+    } // hidden-source-line
+}
