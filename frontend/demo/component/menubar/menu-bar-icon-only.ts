d77e229cf720ecda10f1d9fce6d8d8df4b3bf265
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-29 15:05:44 +0200

    docs: port menu bar docs to 14

---
+++ frontend/demo/component/menubar/menu-bar-icon-only.ts
@@ -0,0 +1,4 @@
+import '../../init';
+
+import '@vaadin/vaadin-menu-bar/vaadin-menu-bar';
+import '@vaadin/vaadin-icons/vaadin-icons';
--- src/main/java/com/vaadin/demo/component/menubar/MenuBarBasic.java
+++ src/main/java/com/vaadin/demo/component/menubar/MenuBarBasic.java
@@ -1,12 +1,14 @@
 package com.vaadin.demo.component.menubar;
 
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.router.Route;
-import com.vaadin.demo.DemoExporter; // hidden-full-source-line
 
 @Route("menu-bar-basic")
 public class MenuBarBasic extends Div {
@@ -15,31 +17,33 @@ public class MenuBarBasic extends Div {
     // tag::snippet[]
     MenuBar menuBar = new MenuBar();
     Text selected = new Text("");
-    Div message = new Div(new Text("Selected: "), selected);
+    ComponentEventListener<ClickEvent<MenuItem>> listener = e -> selected.setText(e.getSource().getText());
+    Div message = new Div(new Text("Clicked item: "), selected);
 
-    MenuItem project = menuBar.addItem("Project");
-    MenuItem account = menuBar.addItem("Account");
-    menuBar.addItem("Sign Out", e -> selected.setText("Sign Out"));
+    menuBar.addItem("View", listener);
+    menuBar.addItem("Edit", listener);
 
-    SubMenu projectSubMenu = project.getSubMenu();
-    MenuItem users = projectSubMenu.addItem("Users");
-    MenuItem billing = projectSubMenu.addItem("Billing");
+    MenuItem share = menuBar.addItem("Share");
+    SubMenu shareSubMenu = share.getSubMenu();
+    MenuItem onSocialMedia = shareSubMenu.addItem("On social media");
+    SubMenu socialMediaSubMenu = onSocialMedia.getSubMenu();
+    socialMediaSubMenu.addItem("Facebook", listener);
+    socialMediaSubMenu.addItem("Twitter", listener);
+    socialMediaSubMenu.addItem("Instagram", listener);
+    shareSubMenu.addItem("By email", listener);
+    shareSubMenu.addItem("Get Link", listener);
 
-    SubMenu usersSubMenu = users.getSubMenu();
-    usersSubMenu.addItem("List", e -> selected.setText("List"));
-    usersSubMenu.addItem("Add", e -> selected.setText("Add"));
+    MenuItem move = menuBar.addItem("Move");
+    SubMenu moveSubMenu = move.getSubMenu();
+    moveSubMenu.addItem("To folder", listener);
+    moveSubMenu.addItem("To trash", listener);
 
-    SubMenu billingSubMenu = billing.getSubMenu();
-    billingSubMenu.addItem("Invoices", e -> selected.setText("Invoices"));
-    billingSubMenu.addItem("Balance Events", e -> selected.setText("Balance Events"));
-
-    account.getSubMenu().addItem("Edit Profile", e -> selected.setText("Edit Profile"));
-    account.getSubMenu().addItem("Privacy Settings", e -> selected.setText("Privacy Settings"));
+    menuBar.addItem("Duplicate", listener);
+    // end::snippet[]
 
     add(menuBar, message);
-    // end::snippet[]
   }
 
-  public static class Exporter extends DemoExporter<MenuBarBasic> { // hidden-full-source-line
-  } // hidden-full-source-line
+  public static class Exporter extends DemoExporter<MenuBarBasic> { // hidden-source-line
+  } // hidden-source-line
 }
