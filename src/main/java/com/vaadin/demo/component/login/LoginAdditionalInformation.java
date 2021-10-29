c61a4e25cec8622d794c809f0e6825b398f8c74e
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-10-29 10:13:32 +0200

    docs: port login docs to 14

---
+++ src/main/java/com/vaadin/demo/component/login/LoginAdditionalInformation.java
@@ -0,0 +1,26 @@
+package com.vaadin.demo.component.login;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.router.Route;
+
+@Route("login-additional-information")
+public class LoginAdditionalInformation extends Div {
+
+    public LoginAdditionalInformation() {
+        // tag::snippet[]
+        LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setAdditionalInformation("Please, contact admin@company.com if you're experiencing issues logging into your account");
+
+        LoginOverlay loginOverlay = new LoginOverlay();
+        loginOverlay.setI18n(i18n);
+        // end::snippet[]
+        add(loginOverlay);
+        loginOverlay.setOpened(true);
+        // Prevent the example from stealing focus when browsing the documentation
+        loginOverlay.getElement().setAttribute("no-autofocus", "");
+    }
+    public static class Exporter extends DemoExporter<LoginAdditionalInformation> {} // hidden-source-line
+}
---
+++ src/main/java/com/vaadin/demo/component/login/LoginInternationalization.java
@@ -0,0 +1,44 @@
+package com.vaadin.demo.component.login;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.router.Route;
+
+@Route("login-internationalization")
+public class LoginInternationalization extends Div {
+
+    public LoginInternationalization() {
+        // Demo purposes only
+        getStyle()
+                .set("background-color", "var(--lumo-contrast-5pct)")
+                .set("display", "flex")
+                .set("justify-content", "center")
+                .set("padding", "var(--lumo-space-l)");
+
+        // tag::snippet[]
+        LoginI18n i18n = LoginI18n.createDefault();
+
+        LoginI18n.Form i18nForm = i18n.getForm();
+        i18nForm.setTitle("Kirjaudu sisään");
+        i18nForm.setUsername("Käyttäjänimi");
+        i18nForm.setPassword("Salasana");
+        i18nForm.setSubmit("Kirjaudu sisään");
+        i18nForm.setForgotPassword("Unohtuiko salasana?");
+        i18n.setForm(i18nForm);
+
+        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
+        i18nErrorMessage.setTitle("Väärä käyttäjätunnus tai salasana");
+        i18nErrorMessage.setMessage("Tarkista että käyttäjätunnus ja salasana ovat oikein ja yritä uudestaan.");
+        i18n.setErrorMessage(i18nErrorMessage);
+
+        LoginForm loginForm = new LoginForm();
+        loginForm.setI18n(i18n);
+        // end::snippet[]
+        add(loginForm);
+        // Prevent the example from stealing focus when browsing the documentation
+        loginForm.getElement().setAttribute("no-autofocus", "");
+    }
+    public static class Exporter extends DemoExporter<LoginInternationalization> {} // hidden-source-line
+}
