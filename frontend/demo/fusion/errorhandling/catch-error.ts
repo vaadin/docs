b704ef9144c1115543454d090a44f89568c6a876
Anton Platonov <platosha@gmail.com>
2021-11-02 18:03:11 +0200

    docs(upgrade): add a note note about TypeScript catch type change
    Fixes vaadin/fusion#108

--- frontend/demo/fusion/errorhandling/catch-error.ts
+++ frontend/demo/fusion/errorhandling/catch-error.ts
@@ -6,8 +6,8 @@ export async function callEndpoint() {
   try {
     await DataEndpoint.getViewData();
   } catch (error: any) {
-    console.warn(error.message); // "Not implemented"
     if (error instanceof EndpointError) {
+      console.warn(error.message); // "Not implemented"
       console.warn(error.type); // "com.vaadin.fusion.exception.EndpointException"
     }
   }
