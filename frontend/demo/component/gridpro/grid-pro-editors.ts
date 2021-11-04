f1dbeac71c50af81a0cbf93cf64a1d2916049743
Sascha Ißbrücker <sissbruecker@vaadin.com>
2021-11-04 14:56:29 +0100

    docs: port grid pro docs to 14 (#863)

---
+++ frontend/demo/component/gridpro/grid-pro-editors.ts
@@ -0,0 +1,5 @@
+import '../../init';
+
+import '@vaadin/vaadin-grid-pro/vaadin-grid-pro';
+import '@vaadin/vaadin-grid-pro/vaadin-grid-pro-edit-column';
+import '@vaadin/vaadin-date-picker/vaadin-date-picker';
---
+++ frontend/demo/component/gridpro/grid-pro-single-cell-edit.ts
@@ -0,0 +1,4 @@
+import '../../init';
+
+import '@vaadin/vaadin-grid-pro/vaadin-grid-pro';
+import '@vaadin/vaadin-grid-pro/vaadin-grid-pro-edit-column';
