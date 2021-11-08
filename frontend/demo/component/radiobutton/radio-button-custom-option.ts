c0f2f604704bce71d09e17fad209c8cac4ae4305
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-11-08 10:50:28 +0100

    docs: port radio button component docs to 14

---
+++ frontend/demo/component/radiobutton/radio-button-custom-option.ts
@@ -0,0 +1,7 @@
+import '../../init';
+
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-text-field/vaadin-text-field';
+import '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
