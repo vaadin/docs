b2c7e4cf3990eded7725efcae619b6d2d0e422bc
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-11-08 16:03:05 +0100

    fix module sources

--- frontend/demo/component/select/select-empty-selection caption.ts
+++ frontend/demo/component/select/select-empty-selection caption.ts
@@ -3,4 +3,3 @@ import 'Frontend/demo/init'; // hidden-source-line
 import '@vaadin/item';
 import '@vaadin/list-box';
 import '@vaadin/select';
-diff --git a/articles/ds/components/select/index.asciidoc b/articles/ds/components/select/index.asciidoc
--- frontend/demo/component/select/select-empty-selection.ts
+++ frontend/demo/component/select/select-empty-selection.ts
@@ -3,4 +3,3 @@ import 'Frontend/demo/init'; // hidden-source-line
 import '@vaadin/item';
 import '@vaadin/list-box';
 import '@vaadin/select';
-diff --git a/frontend/demo/component/select/select-empty-selection caption.ts b/frontend/demo/component/select/select-empty-selection caption.ts
