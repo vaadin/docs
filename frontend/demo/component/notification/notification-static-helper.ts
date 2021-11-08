71bea70b2b7dbd00a7ad921e0b5877aad7d49590
Jouni Koivuviita <jouni@vaadin.com>
2021-11-08 13:19:47 +0200

    fix: use .js extension for Lit decorator/directive imports (#901)

--- frontend/demo/component/notification/notification-static-helper.ts
+++ frontend/demo/component/notification/notification-static-helper.ts
@@ -1,6 +1,6 @@
 import 'Frontend/demo/init'; // hidden-source-line
 import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators';
+import { customElement } from 'lit/decorators.js';
 import '@vaadin/button';
 import { Notification } from '@vaadin/notification';
 import { applyTheme } from 'Frontend/generated/theme';
