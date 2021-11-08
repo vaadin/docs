d2c1a2e745c2a33ba4674c3b59739aa3662a7760
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-11-08 16:00:07 +0100

    fix: add missing imports for select demos

---
+++ frontend/demo/component/select/select-empty-selection caption.ts
@@ -0,0 +1,6 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import '@vaadin/item';
+import '@vaadin/list-box';
+import '@vaadin/select';
+diff --git a/articles/ds/components/select/index.asciidoc b/articles/ds/components/select/index.asciidoc
---
+++ frontend/demo/component/select/select-empty-selection.ts
@@ -0,0 +1,6 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import '@vaadin/item';
+import '@vaadin/list-box';
+import '@vaadin/select';
+diff --git a/frontend/demo/component/select/select-empty-selection caption.ts b/frontend/demo/component/select/select-empty-selection caption.ts
