d1727d8720699013ed36d1b13292d745a200c43b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2021-11-12 11:39:49 +0100

    docs: port tree grid component docs to 14 (#915)

---
+++ frontend/demo/component/tree-grid/tree-grid-column.ts
@@ -0,0 +1,6 @@
+import '../../init';
+
+import '@vaadin/vaadin-button/vaadin-button';
+import '@vaadin/vaadin-grid/vaadin-grid';
+import '@vaadin/vaadin-grid/vaadin-grid-tree-column';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
---
+++ frontend/demo/component/tree-grid/tree-grid-rich-content.ts
@@ -0,0 +1,9 @@
+import '../../init';
+
+import '@vaadin/vaadin-avatar/vaadin-avatar';
+import '@vaadin/vaadin-grid/vaadin-grid';
+import '@vaadin/vaadin-grid/vaadin-grid-tree-toggle';
+import '@vaadin/vaadin-ordered-layout/vaadin-horizontal-layout';
+import '@vaadin/vaadin-ordered-layout/vaadin-vertical-layout';
+import '@vaadin/vaadin-button/vaadin-button';
+import '@vaadin/vaadin-icons/vaadin-icons';
