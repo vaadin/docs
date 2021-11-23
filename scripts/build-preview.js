9d2fc382c5a04e51fcf3b9808ed94b142cf74b12
Jouni Koivuviita <jouni@vaadin.com>
2021-11-24 01:03:14 +0200

    Update build-preview.js

--- scripts/build-preview.js
+++ scripts/build-preview.js
@@ -41,7 +41,7 @@ clonePromise.then(() => {
   execSync('npm i', { cwd: './docs-app', stdio: 'inherit' });
 
   console.log('Prepare content...');
-  execSync('./scripts/mvnw -B compile vaadin:prepare-frontend vaadin:build-frontend', { cwd: '../', stdio: 'inherit' });
+  execSync('./scripts/mvnw -B --no-transfer-progress compile vaadin:prepare-frontend vaadin:build-frontend', { cwd: '../', stdio: 'inherit' });
 
   console.log('Building documentation site...');
   execSync('DOCS_CONTENT_ROOT=../../ npx --unhandled-rejections=strict gatsby build --prefix-paths', { cwd: './docs-app', stdio: 'inherit' });
