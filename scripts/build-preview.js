f5948b26b16929c5100edd62b4a1db1f427bf549
Jouni Koivuviita <jouni@vaadin.com>
2022-01-01 10:17:01 +0200

    Update build-preview.js

--- scripts/build-preview.js
+++ scripts/build-preview.js
@@ -11,7 +11,7 @@ if (!process.env.GITHUB_TOKEN) {
 const url = 'https://github.com/vaadin/docs-app.git';
 const localPath = path.join(__dirname, 'docs-app');
 const opts = {
-  checkoutBranch: 'master',
+  checkoutBranch: 'discussions',
   fetchOpts: {
     callbacks: {
       certificateCheck: function() { return 0; },
