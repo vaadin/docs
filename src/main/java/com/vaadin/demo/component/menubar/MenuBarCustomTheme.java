c5b19ab3b814d263b91ec3ea6ab1f451f7331dce
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2022-01-28 11:49:55 +0200

    feat: style individual menu bar items (#952)
    * feat: style individual menu bar items
    
    * Apply suggestions from code review
    
    * Simplify example
    
    Co-authored-by: Jouni Koivuviita <jouni@vaadin.com>
    Co-authored-by: Yuriy Yevstihnyeyev <yuriyyevstihnyeyev@Yuriys-MBP.home>

---
+++ src/main/java/com/vaadin/demo/component/menubar/MenuBarCustomTheme.java
@@ -0,0 +1,36 @@
+package com.vaadin.demo.component.menubar;
+
+import com.vaadin.demo.DemoExporter; // hidden-source-line
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.router.Route;
+
+@Route("menu-bar-custom-theme")
+public class MenuBarCustomTheme extends Div {
+
+  public MenuBarCustomTheme() {
+    MenuBar menuBar = new MenuBar();
+
+    // tag::snippet[]
+    MenuItem view = menuBar.addItem("View");
+    view.addThemeNames("custom-theme");
+
+    MenuItem edit = menuBar.addItem("Edit");
+
+    MenuItem share = menuBar.addItem("Share");
+    SubMenu shareSubMenu = share.getSubMenu();
+    shareSubMenu.addItem("By email").addThemeNames("custom-theme");
+    shareSubMenu.addItem("Get Link");
+    // end::snippet[]
+
+    add(menuBar);
+  }
+
+  public static class Exporter extends DemoExporter<MenuBarCustomTheme> { // hidden-source-line
+  } // hidden-source-line
+}
