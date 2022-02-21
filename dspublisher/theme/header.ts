2c5aae0c1b6d5c7a5cde1f744881203682b59c8d
Özgür Uysal <ozguruysal@gmail.com>
2022-02-21 13:58:30 +0300

    Load cookie consent script

--- dspublisher/theme/header.ts
+++ dspublisher/theme/header.ts
@@ -20,6 +20,25 @@ export default class Example extends LitElement {
     },
   ];
 
+  protected firstUpdated(): void {
+    this.loadCookieConsentScript();
+  }
+
+  private loadCookieConsentScript(): void {
+    const src = 'https://hilla.dev/cookie-consent.js';
+    const isCookieConsentScriptAdded = document.querySelector(`script[src^="${src}"]`);
+
+    if (isCookieConsentScriptAdded) {
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return html`<div class="hilla-header-wrapper">
       <div class="hilla-header hilla-header--container">
