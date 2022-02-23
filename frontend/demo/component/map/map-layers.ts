b759de02aca659339edcfaa3c596bf853dff83d1
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-02-23 18:17:40 +0100

    docs: add layers section for map component (#1108)
    * docs: add layers section for map component
    
    * address review comments
    
    * improve snippets

---
+++ frontend/demo/component/map/map-layers.ts
@@ -0,0 +1,6 @@
+import 'Frontend/demo/init';
+import '@vaadin/horizontal-layout';
+import '@vaadin/map';
+import '@vaadin/radio-group';
+
+// This file only has the required imports for the Java-only example
