6eb9c736bf7fc3e8941b92205b3a7f16aa9d9bff
Jouni Koivuviita <jouni@vaadin.com>
2022-02-28 13:30:52 +0200

    Update vaadin.com HaaS integration (#1034)

--- dspublisher/theme/header.ts
+++ dspublisher/theme/header.ts
@@ -6,19 +6,34 @@ export default class Example extends HTMLElement {
   private haasImportScript() {
     const script = document.createElement('script');
     script.onload = () => (window as any).haas.loader.initMenu();
+    script.defer = true;
     script.src =
       window.location.hostname == 'preview.vaadin.com'
         ? 'https://preview.vaadin.com/vaadincom/haas-service/v2/haas-loader.js'
         : 'https://vaadin.com/vaadincom/haas-service/v2/haas-loader.js';
     return script;
   }
-
   connectedCallback() {
     if (!this.__initialized) {
       this.__initialized = true;
 
       this.innerHTML = `
-        <div class="vcom-header" id="haas-container"></div>
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Poppins:wght@600;700&display=swap"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://cdn.vaadin.com/website/antlers/v2/assets/icons/css/line-awesome.min.css"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://cdn.vaadin.com/website/hubspot-theme/v2/haas/css/haas.css"
+        />
+        <div id="haas-container"></div>
         <div class="eol-notice">
           <p>
             <span>Vaadin 8 reaches End of Life on February 21, 2022.</span>
