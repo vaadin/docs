e914574d11f5322fd2962e44ea18d50e1b58dbdd
Jouni Koivuviita <jouni@vaadin.com>
2022-03-09 17:46:26 +0200

    refactor: render basic Dialog example in iframe, instead of fake preview

--- frontend/demo/component/dialog/dialog-basic.ts
+++ frontend/demo/component/dialog/dialog-basic.ts
@@ -1,6 +1,6 @@
 import 'Frontend/demo/init'; // hidden-source-line
 
-import { html, LitElement, render } from 'lit';
+import { css, html, LitElement, render } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { guard } from 'lit/directives/guard.js';
 
@@ -22,46 +22,53 @@ export class Example extends LitElement {
   }
 
   @state()
-  private dialogOpened = false;
+  private dialogOpened = true;
 
   render() {
     return html`
       <!-- tag::snippet[] -->
       <vaadin-dialog
-        aria-label="Create new employee"
+        aria-label="New employee"
         .opened="${this.dialogOpened}"
         @opened-changed="${(e: CustomEvent) => (this.dialogOpened = e.detail.value)}"
         .renderer="${guard([], () => (root: HTMLElement) => {
-          render(
-            html`
-              <vaadin-vertical-layout
-                theme="spacing"
-                style="width: 300px; max-width: 100%; align-items: stretch;"
-              >
-                <h2 style="margin: var(--lumo-space-m) 0 0 0; font-size: 1.5em; font-weight: bold;">
-                  Create new employee
-                </h2>
-                <vaadin-vertical-layout style="align-items: stretch;">
-                  <vaadin-text-field label="First name"></vaadin-text-field>
-                  <vaadin-text-field label="Last name"></vaadin-text-field>
-                </vaadin-vertical-layout>
-                <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-end">
-                  <vaadin-button @click="${() => (this.dialogOpened = false)}">
-                    Cancel
-                  </vaadin-button>
-                  <vaadin-button theme="primary" @click="${() => (this.dialogOpened = false)}">
-                    Save
-                  </vaadin-button>
-                </vaadin-horizontal-layout>
-              </vaadin-vertical-layout>
-            `,
-            root
-          );
+          render(this.dialogLayout, root);
         })}"
       ></vaadin-dialog>
-      <!-- end::snippet[] -->
 
-      <vaadin-button @click="${() => (this.dialogOpened = true)}"> Show dialog </vaadin-button>
+      <vaadin-button @click="${() => (this.dialogOpened = true)}">Show dialog</vaadin-button>
+      <!-- end::snippet[] -->
     `;
   }
+
+  dialogLayout = html`
+    <vaadin-vertical-layout style="align-items: stretch; width: 18rem; max-width: 100%;">
+      <h2 style="margin-top: 0;">New employee</h2>
+      <vaadin-text-field label="First name"></vaadin-text-field>
+      <vaadin-text-field label="Last name"></vaadin-text-field>
+      <vaadin-horizontal-layout
+        theme="spacing"
+        style="justify-content: flex-end; margin-top: var(--lumo-space-m);"
+      >
+        <vaadin-button @click="${() => (this.dialogOpened = false)}"> Cancel </vaadin-button>
+        <vaadin-button theme="primary" @click="${() => (this.dialogOpened = false)}">
+          Add
+        </vaadin-button>
+      </vaadin-horizontal-layout>
+    </vaadin-vertical-layout>
+  `;
+
+  static styles = css`
+    /* Center the button within the example */
+    :host {
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      display: flex !important;
+      align-items: center;
+      justify-content: center;
+    }
+  `;
 }
