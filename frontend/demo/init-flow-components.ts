c8d5247eb2e28d43b3e960a8474953d95353d2f8
Sergey Vinogradov <mr.vursen@gmail.com>
2022-05-11 15:41:43 +0300

    fix: import contextMenuTargetConnector manually

--- frontend/demo/init-flow-components.ts
+++ frontend/demo/init-flow-components.ts
@@ -14,6 +14,7 @@ import '@vaadin/flow-frontend/flow-component-renderer.js';
 // Flow component specific modules
 import '@vaadin/flow-frontend/comboBoxConnector.js';
 import '@vaadin/flow-frontend/contextMenuConnector.js';
+import '@vaadin/flow-frontend/contextMenuTargetConnector.js';
 import '@vaadin/flow-frontend/datepickerConnector.js';
 import '@vaadin/flow-frontend/gridConnector.js';
 import '@vaadin/flow-frontend/vaadin-grid-flow-selection-column.js';
