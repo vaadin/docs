d08b8852ae2c81fb3147c0ff00fc28c7d87be05f
Tomi Virkki <virkki@vaadin.com>
2022-06-09 11:50:18 +0300

    refactor: improvements to dspublisher-article-footer

--- dspublisher/theme/init-browser.ts
+++ dspublisher/theme/init-browser.ts
@@ -1,109 +1,133 @@
-import { html, LitElement } from 'lit';
-import { until } from 'lit-html/directives/until';
+import { css, html, LitElement } from 'lit';
+import { state } from 'lit/decorators.js';
 import { iframeResizer } from 'iframe-resizer';
 
-customElements.define(
-  'dspublisher-header',
-  class extends LitElement {
-    createRenderRoot() {
-      return this;
-    }
-
-    private haasImportScript() {
-      const script = document.createElement('script');
-      script.onload = () => (window as any).haas.loader.initMenu();
-      script.defer = true;
-      script.src =
-        window.location.hostname == 'preview.vaadin.com'
-          ? 'https://preview.vaadin.com/vaadincom/haas-service/v2/haas-loader.js'
-          : 'https://vaadin.com/vaadincom/haas-service/v2/haas-loader.js';
-      return script;
-    }
-
-    render() {
-      // Don't render HaaS in development (avoid noise in analytics)
-      if (process.env.NODE_ENV === 'development') {
-        return html``;
-      }
-
-      return html`
-        <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@600;700&display=swap"
-        />
-        <link
-          rel="preload"
-          as="style"
-          href="https://cdn.vaadin.com/website/antlers/v2/assets/icons/css/line-awesome.min.css"
-        />
-        <link
-          rel="preload"
-          as="style"
-          href="https://cdn.vaadin.com/website/hubspot-theme/v2/haas/css/haas.css"
-        />
-
-        <div id="haas-container"></div>
-        ${this.haasImportScript()}
-      `;
-    }
+class Header extends LitElement {
+  createRenderRoot() {
+    return this;
   }
-);
 
-customElements.define(
-  'dspublisher-article-footer',
-  class extends LitElement {
-    createRenderRoot() {
-      return this;
+  private haasImportScript() {
+    const script = document.createElement('script');
+    script.onload = () => (window as any).haas.loader.initMenu();
+    script.defer = true;
+    script.src =
+      window.location.hostname == 'preview.vaadin.com'
+        ? 'https://preview.vaadin.com/vaadincom/haas-service/v2/haas-loader.js'
+        : 'https://vaadin.com/vaadincom/haas-service/v2/haas-loader.js';
+    return script;
+  }
+
+  render() {
+    // Don't render HaaS in development (avoid noise in analytics)
+    if (process.env.NODE_ENV === 'development') {
+      return html``;
     }
 
-    updated() {
-      // TODO any better way to detect when the render is really done?
-      setTimeout(() => iframeResizer({ log: true }, '#discussion-iframe'), 100);
+    return html`
+      <link
+        rel="preload"
+        as="style"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@600;700&display=swap"
+      />
+      <link
+        rel="preload"
+        as="style"
+        href="https://cdn.vaadin.com/website/antlers/v2/assets/icons/css/line-awesome.min.css"
+      />
+      <link
+        rel="preload"
+        as="style"
+        href="https://cdn.vaadin.com/website/hubspot-theme/v2/haas/css/haas.css"
+      />
+
+      <div id="haas-container"></div>
+      ${this.haasImportScript()}
+    `;
+  }
+}
+
+customElements.define('dspublisher-header', Header);
+
+class Footer extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+
+  @state()
+  private documentTitle = document.title;
+
+  private __titleObserver = new MutationObserver(() => {
+    this.documentTitle = document.title;
+  });
+
+  static styles = css`
+    .discussion-wrapper {
+      margin-top: 3rem;
+      padding: 2rem 0;
+      border-top: 1px solid var(--docs-divider-color-1);
     }
 
-    render() {
-      // Drop '/docs' from the beginning of the pathname
-      const id = btoa(document.location.pathname.substring(5));
-      const url = encodeURI(document.location.pathname.substring(5));
-      const content = this.getTitle().then(
-        (name) => html`
-        <style>
-          .discussion-wrapper {
-            margin-top: 3rem;
-            padding: 2rem 0;
-            border-top: 1px solid var(--docs-divider-color-1);
-          }
-
-          .discussion-wrapper p b {
-            color: var(--docs-heading-text-color);
-          }
-
-          .discussion-wrapper iframe {
-            border: 0;
-            margin: 0 -8px;
-            width: calc(100% + 16px);
-            max-width: none;
-          }
-        </style>
-        <section class="discussion-wrapper">
-          <p><b>Was this page helpful?</b><br>Leave a comment or a question below. You can also join the <a href="https://discord.gg/MYFq5RTbBn" rel="noopened">chat on Discord</a> or <a href="https://stackoverflow.com/questions/tagged/vaadin" rel="noopened">ask questions on StackOverflow</a>.</p>
-          <iframe id="discussion-iframe" src="https://preview.vaadin.com/vaadincom/discussion-service/embed.html?root=DOCS&id=${id}&url=${url}&name=${name}&description=">
-        </section>
-      `
-      );
-
-      return html`${until(content, html``)}`;
+    .discussion-wrapper p b {
+      color: var(--docs-heading-text-color);
     }
 
-    async getTitle() {
-      return new Promise(async (resolve) => {
-        let limit = 1000;
-        while (!document.title && limit-- > 0) {
-          await new Promise((resolve) => setTimeout(resolve, 100));
-        }
-        resolve(encodeURI(document.title));
+    .discussion-wrapper iframe {
+      border: 0;
+      margin: 0 -8px;
+      width: calc(100% + 16px);
+      max-width: none;
+    }
+  `;
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    const titleElement = document.head.querySelector('title');
+    if (titleElement) {
+      this.__titleObserver.observe(titleElement, {
+        subtree: true,
+        characterData: true,
+        childList: true,
       });
+    } else {
+      console.warn('No title element found in the document');
     }
   }
-);
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.__titleObserver.disconnect();
+  }
+
+  protected firstUpdated() {
+    iframeResizer({ log: true }, '#discussion-iframe');
+  }
+
+  render() {
+    // Drop '/docs' from the beginning of the pathname
+    const id = btoa(document.location.pathname.substring('/docs'.length));
+    const url = encodeURI(document.location.pathname.substring('/docs'.length));
+
+    return html`
+      <section class="discussion-wrapper">
+        <p>
+          <b>Was this page helpful?</b><br />Leave a comment or a question below. You can also join
+          the <a href="https://discord.gg/MYFq5RTbBn" rel="noopened">chat on Discord</a> or
+          <a href="https://stackoverflow.com/questions/tagged/vaadin" rel="noopened"
+            >ask questions on StackOverflow</a
+          >.
+        </p>
+        <iframe
+          id="discussion-iframe"
+          src="https://preview.vaadin.com/vaadincom/discussion-service/embed.html?root=DOCS&id=${id}&url=${url}&name=${encodeURI(
+            this.documentTitle
+          )}&description)="
+        ></iframe>
+      </section>
+    `;
+  }
+}
+
+customElements.define('dspublisher-article-footer', Footer);
