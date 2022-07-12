d1d46d71c7468b7cb942befd0877634f5509e42a
ugur-vaadin <ugur@vaadin.com>
2022-07-12 11:20:40 +0300

    refactor: change i18n object name and make the example more readable
    Change i18n object name to "germanI18n" to make it more self-explanatory. Edit the line breaks in order to improve the readability of the example.

--- src/main/java/com/vaadin/demo/component/datepicker/DatePickerInternationalization.java
+++ src/main/java/com/vaadin/demo/component/datepicker/DatePickerInternationalization.java
@@ -14,15 +14,17 @@ public class DatePickerInternationalization extends Div {
         DatePicker datePicker = new DatePicker("Sitzungsdatum");
 
         // tag::snippet[]
-        DatePicker.DatePickerI18n customI18n = new DatePicker.DatePickerI18n();
-        customI18n.setMonthNames(List.of("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"));
-        customI18n.setWeekdays(List.of("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"));
-        customI18n.setWeekdaysShort(List.of("So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"));        
-        customI18n.setWeek("Woche");
-        customI18n.setToday("Heute");
-        customI18n.setCancel("Abbrechen");
+        DatePicker.DatePickerI18n germanI18n = new DatePicker.DatePickerI18n();
+        germanI18n.setMonthNames(List.of("Januar", "Februar", "März", "April", "Mai", 
+            "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"));
+        germanI18n.setWeekdays(List.of("Sonntag", "Montag", "Dienstag", "Mittwoch", 
+            "Donnerstag", "Freitag", "Samstag"));
+        germanI18n.setWeekdaysShort(List.of("So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"));        
+        germanI18n.setWeek("Woche");
+        germanI18n.setToday("Heute");
+        germanI18n.setCancel("Abbrechen");
 
-        datePicker.setI18n(customI18n);
+        datePicker.setI18n(germanI18n);
         // end::snippet[]
 
         add(datePicker);
