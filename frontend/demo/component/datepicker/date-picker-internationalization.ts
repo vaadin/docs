dddd03a726b1faaa4f1ed4779b44b33f82a37407
ugur-vaadin <ugur@vaadin.com>
2022-07-12 09:54:49 +0300

    docs: add internationalization documentation for DatePicker
    Add internationalization documentation for DatePicker using German language. Provide examples in TypeScript and Java.

---
+++ frontend/demo/component/datepicker/date-picker-internationalization.ts
@@ -0,0 +1,55 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import { DatePicker } from '@vaadin/date-picker';
+
+import { html, LitElement } from 'lit';
+import { customElement, query } from 'lit/decorators.js';
+import '@vaadin/date-picker';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('date-picker-internationalization')
+export class Example extends LitElement {
+  protected createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  // tag::snippet[]
+  @query('vaadin-date-picker')
+  private datePicker?: DatePicker;
+
+  firstUpdated() {
+    if (this.datePicker) {
+      this.datePicker.i18n = {
+        ...this.datePicker.i18n,
+        monthNames: [
+          'Januar',
+          'Februar',
+          'März',
+          'April',
+          'Mai',
+          'Juni',
+          'Juli',
+          'August',
+          'September',
+          'Oktober',
+          'November',
+          'Dezember',
+        ],
+        weekdays: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+        weekdaysShort: ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
+        week: 'Woche',
+        today: 'Heute',
+        cancel: 'Abbrechen',
+        firstDayOfWeek: 0,
+      };
+    }
+  }
+
+  render() {
+    return html` <vaadin-date-picker label="Sitzungsdatum"></vaadin-date-picker> `;
+  }
+  // end::snippet[]
+}
