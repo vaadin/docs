d48718be99cd090af285665b6fa8636a4a7403ce
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-08-31 16:14:47 +0300

    Add notice to header about viewing old docs version (#1638) (#1652)

--- dspublisher/theme/header.ts
+++ dspublisher/theme/header.ts
@@ -35,6 +35,16 @@ export default class Example extends LitElement {
       />
 
       <div id="haas-container"></div>
+      <div class="old-version-notice">
+        <p>
+          <span>
+            This docs is for an older Vaadin version. Check out the
+            <a href="https://vaadin.com/docs/latest/">
+              documentation for the latest Vaadin version</a
+            >.
+          </span>
+        </p>
+      </div>
       ${this.haasImportScript()}
     `;
   }
