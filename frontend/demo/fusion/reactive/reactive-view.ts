f9e8cefc32fbe99637a3a0fdd0df0eb26e2244a3
Luciano Vernaschi <luciano@vaadin.com>
2022-09-08 11:17:37 +0200

    Add an article (with example) about reactive endpoints

---
+++ frontend/demo/fusion/reactive/reactive-view.ts
@@ -0,0 +1,40 @@
+import { Subscription } from '@hilla/frontend';
+import '@vaadin/button';
+import '@vaadin/notification';
+import '@vaadin/text-field';
+import { ReactiveEndpoint } from 'Frontend/generated/endpoints';
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('reactive-view')
+export class ReactiveView extends LitElement {
+  @property()
+  serverTime = '';
+
+  subscription: Subscription<string> | undefined;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.classList.add('flex', 'p-m', 'gap-m', 'items-end');
+  }
+
+  // tag::snippet[]
+  render() {
+    return html`
+      <vaadin-text-field label="Server time" .value=${this.serverTime}></vaadin-text-field>
+      <vaadin-button @click=${this.toggleServerClock}>Toggle server clock</vaadin-button>
+    `;
+  }
+
+  toggleServerClock() {
+    if (this.subscription) {
+      this.subscription.cancel();
+      this.subscription = undefined;
+    } else {
+      (this.subscription = ReactiveEndpoint.getClockCancellable()).onNext((time) => {
+        this.serverTime = time;
+      });
+    }
+  }
+  // end::snippet[]
+}
