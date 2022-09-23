0503dea66ce27a6b5a19b5b28373b096b4ea0e1b
web-padawan <iamkulykov@gmail.com>
2022-09-23 16:24:22 +0300

    chore: reindent Java examples using 4 spaces (part 1)

--- src/main/java/com/vaadin/demo/component/passwordfield/PasswordFieldRevealButtonHidden.java
+++ src/main/java/com/vaadin/demo/component/passwordfield/PasswordFieldRevealButtonHidden.java
@@ -8,16 +8,17 @@ import com.vaadin.demo.DemoExporter; // hidden-source-line
 @Route("password-field-reveal-button-hidden")
 public class PasswordFieldRevealButtonHidden extends Div {
 
-  public PasswordFieldRevealButtonHidden() {
-    // tag::snippet[]
-    PasswordField passwordField = new PasswordField();
-    passwordField.setRevealButtonVisible(false);
-    passwordField.setLabel("Password");
-    passwordField.setValue("Ex@mplePassw0rd");
-    add(passwordField);
-    // end::snippet[]
-  }
+    public PasswordFieldRevealButtonHidden() {
+        // tag::snippet[]
+        PasswordField passwordField = new PasswordField();
+        passwordField.setRevealButtonVisible(false);
+        passwordField.setLabel("Password");
+        passwordField.setValue("Ex@mplePassw0rd");
+        add(passwordField);
+        // end::snippet[]
+    }
 
-  public static class Exporter extends DemoExporter<PasswordFieldRevealButtonHidden> { // hidden-source-line
-  } // hidden-source-line
+    public static class Exporter extends // hidden-source-line
+            DemoExporter<PasswordFieldRevealButtonHidden> { // hidden-source-line
+    } // hidden-source-line
 }
--- src/main/java/com/vaadin/demo/component/textfield/TextFieldPattern.java
+++ src/main/java/com/vaadin/demo/component/textfield/TextFieldPattern.java
@@ -8,16 +8,17 @@ import com.vaadin.demo.DemoExporter; // hidden-source-line
 @Route("text-field-pattern")
 public class TextFieldPattern extends Div {
 
-  public TextFieldPattern() {
-    // tag::snippet[]
-    TextField textField = new TextField();
-    textField.setPattern("^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$");
-    textField.setHelperText("Format: +(123)456-7890");
-    textField.setLabel("Phone number");
-    add(textField);
-    // end::snippet[]
-  }
+    public TextFieldPattern() {
+        // tag::snippet[]
+        TextField textField = new TextField();
+        textField.setPattern(
+                "^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$");
+        textField.setHelperText("Format: +(123)456-7890");
+        textField.setLabel("Phone number");
+        add(textField);
+        // end::snippet[]
+    }
 
-  public static class Exporter extends DemoExporter<TextFieldPattern> { // hidden-source-line
-  } // hidden-source-line
+    public static class Exporter extends DemoExporter<TextFieldPattern> { // hidden-source-line
+    } // hidden-source-line
 }
