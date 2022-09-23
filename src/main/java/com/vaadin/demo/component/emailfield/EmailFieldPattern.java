9eac83c6c7c3cc1318c1e9259498f68bdd917ab2
Serhii Kulykov <iamkulykov@gmail.com>
2022-09-23 18:01:20 +0300

    chore: reindent Java examples using 4 spaces (part 1) (#1754)

--- src/main/java/com/vaadin/demo/component/emailfield/EmailFieldPattern.java
+++ src/main/java/com/vaadin/demo/component/emailfield/EmailFieldPattern.java
@@ -8,19 +8,20 @@ import com.vaadin.demo.DemoExporter; // hidden-source-line
 @Route("email-field-pattern")
 public class EmailFieldPattern extends Div {
 
-  public EmailFieldPattern() {
-    // tag::snippet[]
-    EmailField emailField = new EmailField();
-    emailField.setLabel("Email address");
-    emailField.getElement().setAttribute("name", "email");
-    emailField.setPlaceholder("username@example.com");
-    emailField.setErrorMessage("Please enter a valid example.com email address");
-    emailField.setClearButtonVisible(true);
-    emailField.setPattern("^.+@example\\.com$");
-    add(emailField);
-    // end::snippet[]
-  }
+    public EmailFieldPattern() {
+        // tag::snippet[]
+        EmailField emailField = new EmailField();
+        emailField.setLabel("Email address");
+        emailField.getElement().setAttribute("name", "email");
+        emailField.setPlaceholder("username@example.com");
+        emailField.setErrorMessage(
+                "Please enter a valid example.com email address");
+        emailField.setClearButtonVisible(true);
+        emailField.setPattern("^.+@example\\.com$");
+        add(emailField);
+        // end::snippet[]
+    }
 
-  public static class Exporter extends DemoExporter<EmailFieldPattern> { // hidden-source-line
-  } // hidden-source-line
+    public static class Exporter extends DemoExporter<EmailFieldPattern> { // hidden-source-line
+    } // hidden-source-line
 }
