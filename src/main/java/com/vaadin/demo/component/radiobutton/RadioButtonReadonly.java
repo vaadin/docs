cba79b51b6795503faabcc775e52604662bc67be
Serhii Kulykov <iamkulykov@gmail.com>
2022-09-24 12:23:09 +0300

    chore: reindent Java examples using 4 spaces (part 2) (#1756)

--- src/main/java/com/vaadin/demo/component/radiobutton/RadioButtonReadonly.java
+++ src/main/java/com/vaadin/demo/component/radiobutton/RadioButtonReadonly.java
@@ -8,17 +8,17 @@ import com.vaadin.demo.DemoExporter; // hidden-source-line
 @Route("radio-button-readonly")
 public class RadioButtonReadonly extends Div {
 
-  public RadioButtonReadonly() {
-    // tag::snippet[]
-    RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
-    radioGroup.setLabel("Status");
-    radioGroup.setItems("In progress", "Done", "Cancelled");
-    radioGroup.setValue("In progress");
-    radioGroup.setReadOnly(true);
-    add(radioGroup);
-    // end::snippet[]
-  }
+    public RadioButtonReadonly() {
+        // tag::snippet[]
+        RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
+        radioGroup.setLabel("Status");
+        radioGroup.setItems("In progress", "Done", "Cancelled");
+        radioGroup.setValue("In progress");
+        radioGroup.setReadOnly(true);
+        add(radioGroup);
+        // end::snippet[]
+    }
 
-  public static class Exporter extends DemoExporter<RadioButtonReadonly> { // hidden-source-line
-  } // hidden-source-line
+    public static class Exporter extends DemoExporter<RadioButtonReadonly> { // hidden-source-line
+    } // hidden-source-line
 }
