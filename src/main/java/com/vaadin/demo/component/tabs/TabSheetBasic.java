179a13655dbac0c58778e19050d7eaf48a9f4aec
Tomi Virkki <virkki@vaadin.com>
2022-09-28 13:36:52 +0300

    feat: add TabSheet examples

--- src/main/java/com/vaadin/demo/component/tabs/TabSheetBasic.java
+++ src/main/java/com/vaadin/demo/component/tabs/TabSheetBasic.java
@@ -3,6 +3,7 @@ package com.vaadin.demo.component.tabs;
 import com.vaadin.demo.DemoExporter; // hidden-source-line
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.router.Route;
 
 @Route("tabsheet-basic")
@@ -10,9 +11,12 @@ public class TabSheetBasic extends Div {
 
 	public TabSheetBasic() {
 		// tag::snippet[]
-		
+		TabSheet tabSheet = new TabSheet();
+		tabSheet.add("Dashboard", new Div(new Text("This is the Dashboard tab content")));
+		tabSheet.add("Payment", new Div(new Text("This is the Payment tab content")));
+		tabSheet.add("Shipping", new Div(new Text("This is the Shipping tab content")));
+		add(tabSheet);
 		// end::snippet[]
-		add(new Text("Tabsheet example"));
 	}
 
 	public static class Exporter extends DemoExporter<TabSheetBasic> { // hidden-source-line
