4a38af8d8b3c5ccd339e1e4d538085cae607301b
Tomi Virkki <virkki@vaadin.com>
2022-09-28 15:05:32 +0300

    chore: remove unused import

--- frontend/demo/component/tabs/tabsheet-lazy-initialization.ts
+++ frontend/demo/component/tabs/tabsheet-lazy-initialization.ts
@@ -1,6 +1,6 @@
 import 'Frontend/demo/init'; // hidden-source-line
 
-import { html, LitElement, TemplateResult } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import '@vaadin/tabs';
 import '@vaadin/tabsheet';
