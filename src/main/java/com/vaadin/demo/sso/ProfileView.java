3cbf478c6d817b20eac7e33ccc1f125882d5f008
Giovanni Lovato <giovanni@lova.to>
2022-10-05 15:37:10 +0200

    SSO Kit documentation (#1715)
    Co-authored-by: Diego Cardoso <diego@vaadin.com>
    Co-authored-by: tamasmak <tamas@vaadin.com>
    Co-authored-by: Jouni Koivuviita <jouni@vaadin.com>

---
+++ src/main/java/com/vaadin/demo/sso/ProfileView.java
@@ -0,0 +1,32 @@
+package com.vaadin.demo.sso;
+
+import javax.annotation.security.PermitAll;
+
+import com.vaadin.demo.flow.routing.Route;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.sso.starter.AuthenticationContext;
+
+// tag::full-class[]
+@PermitAll
+@Route(value = "private")
+// tag::profile-view[]
+public class ProfileView extends VerticalLayout {
+    // tag::class-content[]
+    public ProfileView(AuthenticationContext authContext) {
+        // tag::authenticated-user[]
+        authContext.getAuthenticatedUser().ifPresent(user -> {
+            Avatar userAvatar = new Avatar(user.getFullName());
+            add(userAvatar);
+        });
+        // end::authenticated-user[]
+        // tag::logout[]
+        add(new Button("Logout", e -> authContext.logout()));
+        // end::logout[]
+    }
+    // end::class-content[]
+    // ...
+}
+// end::profile-view[]
+// end::full-class[]
