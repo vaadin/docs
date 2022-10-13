1da32b3ba618d3c9c9a5ccfc45e95b40ea4b2eaf
Manolo Carrasco <manolo@vaadin.com>
2022-10-13 13:54:49 +0200

    add missing path in config files

--- scripts/pick/cherry-pick-config.js
+++ scripts/pick/cherry-pick-config.js
@@ -1,14 +1,14 @@
 
 const config = {
-  "main/resources/vaadin-featureflags.properties": {
-    target: "main/pepe/pepe/vaadin-featureflags.properties",
+  "src/main/resources/vaadin-featureflags.properties": {
+    target: "src/main/foo/bar/vaadin-featureflags.properties",
     find: "com.vaadin.experimental(.*)",
-    replace: "foo.$1"
+    replace: "foo$1"
   }
 }
 
-for (const f of ["LICENSE", "README.md", "node_modules", "package-lock.json"]) {
-  config[f] = {ignore: true};
+for (const file of ["LICENSE", "README.md", "node_modules", "package-lock.json"]) {
+  config[file] = {ignore: true};
 }
 
 exports.config = config;
