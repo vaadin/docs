a2b2e8c8d00238a58785be2e9693500b836582ee
Jouni Koivuviita <jouni@vaadin.com>
2022-10-17 19:28:08 +0300

    fix: front-end build with latest DSP

--- frontend/demo/fusion/forms/field-strategy/my-text-field-strategy.ts
+++ frontend/demo/fusion/forms/field-strategy/my-text-field-strategy.ts
@@ -3,7 +3,11 @@ import type { FieldStrategy } from '@hilla/form';
 import type { MyTextField } from './my-text-field';
 
 export class MyTextFieldStrategy implements FieldStrategy {
-  constructor(public element: MyTextField) {}
+  public element: MyTextField;
+
+  public constructor(element: MyTextField) {
+    this.element = element;
+  }
 
   set required(required: boolean) {
     this.element.mandatory = required;
