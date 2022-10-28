a3bccc25cc62cd2796cabf24381a4ea1d9d11c7e
ugur-vaadin <ugur@vaadin.com>
2022-10-28 14:02:30 +0300

    docs: update example to show the selected tab

--- src/main/java/com/vaadin/demo/component/tabs/TabsAutoselect.java
+++ src/main/java/com/vaadin/demo/component/tabs/TabsAutoselect.java
@@ -2,7 +2,8 @@ package com.vaadin.demo.component.tabs;
 
 import com.vaadin.demo.DemoExporter;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
@@ -10,18 +11,43 @@ import com.vaadin.flow.router.Route;
 @Route("tabs-autoselect")
 public class TabsAutoselect extends Div {
 
+    private final Tab details;
+    private final Tab payment;
+    private final Tab shipping;
+    private final VerticalLayout content;
+
     public TabsAutoselect() {
-        Tab details = new Tab("Details");
-        Tab payment = new Tab("Payment");
-        Tab shipping = new Tab("Shipping");
+        details = new Tab("Details");
+        payment = new Tab("Payment");
+        shipping = new Tab("Shipping");
+
         // tag::snippet[]
         Tabs tabs = new Tabs();
         tabs.setAutoselect(false);
         // end::snippet[]
-        tabs.addSelectedChangeListener(event -> Notification.show("Selected "
-                + event.getSelectedTab().getLabel()));
+
+        tabs.addSelectedChangeListener(
+                event -> setContent(event.getSelectedTab()));
         tabs.add(details, payment, shipping);
-        add(tabs);
+
+        content = new VerticalLayout();
+        content.setSpacing(false);
+
+        add(tabs, content);
+    }
+
+    private void setContent(Tab tab) {
+        content.removeAll();
+        if (tab == null) {
+            return;
+        }
+        if (tab.equals(details)) {
+            content.add(new Paragraph("This is the Details tab"));
+        } else if (tab.equals(payment)) {
+            content.add(new Paragraph("This is the Payment tab"));
+        } else {
+            content.add(new Paragraph("This is the Shipping tab"));
+        }
     }
 
     public static class Exporter extends DemoExporter<TabsAutoselect> { // hidden-source-line
