7bc8a0e22b9b9b2b9c289acb4453639aaafb580d
Tomi Virkki <virkki@vaadin.com>
2022-11-04 17:12:22 +0200

    fix: update license checker API use

--- src/main/java/com/vaadin/dspublisher/licensecheck/Check.java
+++ src/main/java/com/vaadin/dspublisher/licensecheck/Check.java
@@ -6,7 +6,7 @@ import com.vaadin.pro.licensechecker.LicenseChecker;
 public class Check {
     public static void main(String[] args) {
         try {
-            LicenseChecker.checkLicense("vaadin-dspublisher", "2.1.0-alpha.2", null);
+            LicenseChecker.checkLicense("vaadin-dspublisher", "2.1.0-alpha.2", BuildType.DEVELOPMENT);
         } catch (Exception e) {
             System.exit(1);
         }
