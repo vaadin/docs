ff0a1fe9d10e6b00c8f9995c71a781bf9b5ab5e4
Manuel Carrasco Moñino <manolo@vaadin.com>
2022-12-07 09:06:18 +0200

    adding script for copying changes from latest to dsp branch (#1821)
    * adding script for copying changes from latest to dsp branch
    
    * clean the script
    
    * add missing path in config files
    
    * modify script so as we can configure as described in the ticket
    
    * copy files in root folder needed to build the project. Do not fail if no changes
    
    * adjust configuration to avoid errors when compiling pr
    
    * more adjustments in config file
    
    * add lumo folder to be copied to ds/foundation
    
    * more changes to the config file

---
+++ scripts/pick/cherry-pick-config.js
@@ -0,0 +1,113 @@
+
+const config = {
+  source: {
+    // paths in 'latest' to copy, they should be relative to the root folder
+    // All their content are copied unless exceptions in the "ignore" section
+    // they are copied in the same path unless exceptions in "rename" section
+    "copy": [
+      // folders
+      "articles",
+      "src",
+      "dspublisher",
+      "frontend",
+      // files
+      "pom.xml",
+      ".eslintrc.js",
+      ".gitignore",
+      ".npmrc",
+      ".prettierrc",
+      "tsconfig.json",
+      "types.d.ts",
+      "webpack.config.js",
+      "webpack.dspublisher.js",
+      "package.json"
+    ],
+    // paths in 'latest' to ignore (since they shouldn't be copied to dsp)
+    "ignore": [
+      /(^|\/)\..+/, // hidden files
+
+      "articles/guide",
+      "articles/advanced",
+      "articles/configuration",
+      "articles/tutorial",
+      "articles/tools",
+      "articles/api.adoc",
+      "articles/security",
+      "articles/contributing",
+      "articles/create-ui",
+      "articles/binding-data",
+      "articles/integrations",
+      "articles/testing",
+
+      "articles/index.asciidoc",
+      "articles/overview.asciidoc",
+      "articles/contributing-docs",
+      "articles/upgrading",
+      "articles/application",
+      "articles/routing",
+      "articles/production",
+      "articles/_images",
+      "articles/_vaadin-version.adoc",
+      "articles/_commercial-banner.asciidoc",
+      "articles/_terminal.asciidoc",
+      "articles/404.asciidoc",
+      "articles/_figma-banner.adoc",
+
+      "articles/styling/_images",
+      "articles/styling/_shared.adoc",
+      "articles/styling/advanced",
+      "articles/styling/custom-theme",
+      "articles/styling/getting-started.adoc",
+      "articles/styling/index.adoc",
+      "articles/styling/theme-annotation.adoc",
+      "articles/styling/_images/",
+      "articles/styling/lumo/_images",
+      "articles/styling/lumo/index.adoc",
+      "articles/styling/lumo/variants",
+      "articles/styling/lumo/design-tokens/styling-using-design-tokens.adoc",
+      "articles/styling/lumo/design-tokens/_images",
+      "articles/styling/lumo/design-tokens/index.adoc",
+
+
+      "frontend/demo/fusion",
+      "frontend/demo/upgrade-tool",
+      "frontend/demo/tools",
+      "frontend/demo/flow",
+      "frontend/demo/pwa",
+
+      "src/main/java/com/vaadin/demo/flow",
+      "src/main/java/com/vaadin/demo/pwa",
+      "src/main/java/com/vaadin/demo/sso",
+      "src/main/java/com/vaadin/demo/collaboration",
+
+      "dspublisher/theme",
+    ],
+  },
+  rename: {
+    // paths in 'latest' to copy to different paths in 'dsp'
+    "articles/components": "articles/ds/components",
+    "articles/styling/lumo": "articles/ds/foundation",
+    "articles/styling/lumo/design-tokens": "articles/ds/foundation",
+  },
+  target: {
+    // paths in 'dsp' to keep (since they shouldn't be removed, even if they don't exist in latest)
+    "keep": [
+      "articles/ds/foundation",
+      "articles/ds/_images",
+      "articles/ds/404.asciidoc",
+      "articles/ds/figma.asciidoc",
+      "articles/ds/index.asciidoc",
+      "dspublisher/.env",
+      "dspublisher/docs-theme",
+    ]
+  },
+  // callbacks for changing the content of certain files
+  callback: [
+    {
+      path: 'articles/ds/components/grid/flow.asciidoc',
+      callback: content => content.replace(/.*_items-identities.adoc.*/, '')
+    },
+  ]
+}
+
+exports.config = config;
