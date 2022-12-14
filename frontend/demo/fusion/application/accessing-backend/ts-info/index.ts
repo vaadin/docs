b5b118938cf014d6681ec71a9fabed2f69c54192
Anton Platonov <platosha@gmail.com>
2022-12-14 11:22:24 +0200

    build: fix TypeScript unused definition errors

--- frontend/demo/fusion/application/accessing-backend/ts-info/index.ts
+++
@@ -1,7 +0,0 @@
-// Import and use a declaration from another module
-import type City from './city';
-const cityObject: City = {
-  country: 'Finland',
-  name: 'Turku',
-};
-// Note: cityObject is not exported, thus it is only available in this file
