7214bf0a73a3d3f37c813555ab8c5dda12ffa10e
Sergey Vinogradov <mr.vursen@gmail.com>
2023-01-19 13:08:55 +0200

    fix: use default import syntax for image imports

--- frontend/demo/component/scroller/scroller-both.ts
+++ frontend/demo/component/scroller/scroller-both.ts
@@ -3,7 +3,7 @@ import 'Frontend/demo/init'; // hidden-source-line
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import '@vaadin/scroller';
-import * as img from '../../../../src/main/resources/images/reindeer.jpg';
+import img from '../../../../src/main/resources/images/reindeer.jpg';
 import { applyTheme } from 'Frontend/generated/theme';
 
 @customElement('scroller-both')
