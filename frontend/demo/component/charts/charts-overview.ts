6d3341cfbebf329e206862bcba46ab1a6e0bfbb2
web-padawan <iamkulykov@gmail.com>
2023-02-03 12:42:06 +0200

    chore: set categories and values using properties

--- frontend/demo/component/charts/charts-overview.ts
+++ frontend/demo/component/charts/charts-overview.ts
@@ -60,6 +60,22 @@ export class Example extends LitElement {
   private columnOptions: Options = { yAxis: { title: { text: '' } } };
 
   @state()
+  private months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  @state()
   private pieOptions: Options = {
     tooltip: {
       pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
@@ -114,37 +130,37 @@ export class Example extends LitElement {
     return html`
       <vaadin-chart
         type="column"
-        categories='["Jan", "Feb", "Mar"]'
+        .categories="${['Jan', 'Feb', 'Mar']}"
         .additionalOptions="${this.columnOptions}"
       >
-        <vaadin-chart-series title="Tokyo" values="[49.9, 71.5, 106.4]"></vaadin-chart-series>
-        <vaadin-chart-series title="New York" values="[83.6, 78.8, 98.5]"></vaadin-chart-series>
-        <vaadin-chart-series title="London" values="[48.9, 38.8, 39.3]"></vaadin-chart-series>
+        <vaadin-chart-series title="Tokyo" .values="${[49.9, 71.5, 106.4]}"></vaadin-chart-series>
+        <vaadin-chart-series title="New York" .values="${[83.6, 78.8, 98.5]}"></vaadin-chart-series>
+        <vaadin-chart-series title="London" .values="${[48.9, 38.8, 39.3]}"></vaadin-chart-series>
       </vaadin-chart>
 
       <vaadin-chart
         type="area"
         stacking="normal"
-        categories='["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]'
+        .categories="${this.months}"
+        .additionalOptions="${this.areaOptions}"
         tooltip
         no-legend
-        .additionalOptions="${this.areaOptions}"
       >
         <vaadin-chart-series
           title="United States dollar"
-          values="[135, 125, 89, 124, 105, 81, 111, 94, 95, 129, 98, 84]"
+          .values="${[135, 125, 89, 124, 105, 81, 111, 94, 95, 129, 98, 84]}"
         ></vaadin-chart-series>
         <vaadin-chart-series
           title="Euro"
-          values="[62, 72, 89, 68, 94, 92, 110, 100, 109, 89, 86, 105]"
+          .values="${[62, 72, 89, 68, 94, 92, 110, 100, 109, 89, 86, 105]}"
         ></vaadin-chart-series>
         <vaadin-chart-series
           title="Japanese yen"
-          values="[30, 25, 32, 26, 15, 31, 24, 32, 21, 8, 12, 32]"
+          .values="${[30, 25, 32, 26, 15, 31, 24, 32, 21, 8, 12, 32]}"
         ></vaadin-chart-series>
         <vaadin-chart-series
           title="Poud sterling"
-          values="[32, 21, 8, 12, 32, 21, 12, 30, 25, 19, 26, 15]"
+          .values="${[32, 21, 8, 12, 32, 21, 12, 30, 25, 19, 26, 15]}"
         ></vaadin-chart-series>
       </vaadin-chart>
 
@@ -156,12 +172,12 @@ export class Example extends LitElement {
         <vaadin-chart-series
           type="column"
           title="Column"
-          values="[8, 7, 6, 5, 4, 3, 2, 1]"
+          .values="${[8, 7, 6, 5, 4, 3, 2, 1]}"
           additional-options='{ "pointPlacement": "between" }'
         ></vaadin-chart-series>
-        <vaadin-chart-series type="line" title="Line" values="[1, 2, 3, 4, 5, 6, 7, 8]">
+        <vaadin-chart-series type="line" title="Line" .values="${[1, 2, 3, 4, 5, 6, 7, 8]}">
         </vaadin-chart-series>
-        <vaadin-chart-series type="area" title="Area" values="[1, 8, 2, 7, 3, 6, 4, 5]">
+        <vaadin-chart-series type="area" title="Area" .values="${[1, 8, 2, 7, 3, 6, 4, 5]}">
         </vaadin-chart-series>
       </vaadin-chart>
 
