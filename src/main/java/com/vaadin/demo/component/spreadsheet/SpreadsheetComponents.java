5ec34ce7f2b1d8b70603a6ba84c9958e0e375784
ugur-vaadin <ugur@vaadin.com>
2023-02-17 16:36:08 +0200

    docs: remove unused import

--- src/main/java/com/vaadin/demo/component/spreadsheet/SpreadsheetComponents.java
+++ src/main/java/com/vaadin/demo/component/spreadsheet/SpreadsheetComponents.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 import com.vaadin.flow.component.spreadsheet.SpreadsheetComponentFactory;
 import com.vaadin.flow.component.textfield.TextField;
