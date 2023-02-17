f85dad4f4229402217248bdbb66c77e5c85c2ff0
ugur-vaadin <ugur@vaadin.com>
2023-02-17 14:37:54 +0200

    docs: add custom editor to spreadsheet example

--- frontend/demo/component/spreadsheet/spreadsheet-imports.ts
+++ frontend/demo/component/spreadsheet/spreadsheet-imports.ts
@@ -7,5 +7,6 @@ import '@vaadin/flow-frontend/vaadin-spreadsheet/vaadin-spreadsheet.js';
 import '@vaadin/flow-frontend/vaadin-spreadsheet/spreadsheet-export.js';
 import '@vaadin/button';
 import '@vaadin/checkbox-group';
+import '@vaadin/text-field';
 
 // This file only has the required imports for the Java-only example
--- src/main/java/com/vaadin/demo/component/spreadsheet/SpreadsheetComponents.java
+++ src/main/java/com/vaadin/demo/component/spreadsheet/SpreadsheetComponents.java
@@ -8,16 +8,24 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 import com.vaadin.flow.component.spreadsheet.SpreadsheetComponentFactory;
+import com.vaadin.flow.component.textfield.TextField;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 
 @Route("spreadsheet-components")
 public class SpreadsheetComponents extends Div {
 
-    public SpreadsheetComponents()  {
+    private Button customComponent;
+    private TextField customEditor;
+
+    public SpreadsheetComponents() {
         Spreadsheet spreadsheet = new Spreadsheet();
-        spreadsheet.setColumnWidth(1, 80);
-        Button customComponent = new Button("Click");
+
+        spreadsheet.setColumnWidth(1, 120);
+        spreadsheet.createCell(1, 1, "Button");
+
+        spreadsheet.setColumnWidth(2, 120);
+        spreadsheet.createCell(1, 2, "Text Field");
 
         // tag::snippet[]
         spreadsheet.setSpreadsheetComponentFactory(
@@ -25,31 +33,57 @@ public class SpreadsheetComponents extends Div {
 
                     @Override
                     public Component getCustomComponentForCell(Cell cell,
-                                                               int rowIndex, int columnIndex,
-                                                               Spreadsheet spreadsheet, Sheet sheet) {
-                        if (rowIndex == 1 && columnIndex == 1) {
-                            return customComponent;
+                            int rowIndex, int columnIndex,
+                            Spreadsheet spreadsheet, Sheet sheet) {
+                        if (rowIndex != 2 || columnIndex != 1) {
+                            return null;
                         }
-                        return null;
+                        if (customComponent == null) {
+                            initCustomComponent();
+                        }
+                        return customComponent;
                     }
 
                     @Override
                     public Component getCustomEditorForCell(Cell cell,
-                                                            int rowIndex, int columnIndex,
-                                                            Spreadsheet spreadsheet, Sheet sheet) {
-                        return null;
+                            int rowIndex, int columnIndex,
+                            Spreadsheet spreadsheet, Sheet sheet) {
+                        if (rowIndex != 2 || columnIndex != 2) {
+                            return null;
+                        }
+                        if (customEditor == null) {
+                            initCustomEditor(rowIndex, columnIndex,
+                                    spreadsheet);
+                        }
+                        return customEditor;
                     }
 
                     @Override
                     public void onCustomEditorDisplayed(Cell cell, int rowIndex,
-                                                        int columnIndex, Spreadsheet spreadsheet,
-                                                        Sheet sheet, Component customEditor) {
+                            int columnIndex, Spreadsheet spreadsheet,
+                            Sheet sheet, Component editor) {
+                        if (cell == null) {
+                            return;
+                        }
+                        ((TextField) editor)
+                                .setValue(cell.getStringCellValue());
                     }
                 });
         // end::snippet[]
 
         spreadsheet.setHeight("400px");
         add(spreadsheet);
+    }
+
+    private void initCustomComponent() {
+        customComponent = new Button("Click");
+    }
+
+    private void initCustomEditor(int rowIndex, int columnIndex,
+            Spreadsheet spreadsheet) {
+        customEditor = new TextField();
+        customEditor.addValueChangeListener(e -> spreadsheet.refreshCells(
+                spreadsheet.createCell(rowIndex, columnIndex, e.getValue())));
     }
 
     public static class Exporter extends DemoExporter<SpreadsheetComponents> { // hidden-source-line
