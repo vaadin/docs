4409171c00009a9ef95dcc4546b5d674bc220bef
Serhii Kulykov <iamkulykov@gmail.com>
2023-02-17 16:38:35 +0200

    docs: use proper error messages to fix examples a11y findings (#2179)

--- frontend/demo/component/timepicker/time-picker-min-max.ts
+++ frontend/demo/component/timepicker/time-picker-min-max.ts
@@ -1,12 +1,16 @@
 import 'Frontend/demo/init'; // hidden-source-line
 
 import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import '@vaadin/time-picker';
+import type { TimePickerChangeEvent } from '@vaadin/time-picker';
 import { applyTheme } from 'Frontend/generated/theme';
 
 @customElement('time-picker-min-max')
 export class Example extends LitElement {
+  @state()
+  protected errorMessage = '';
+
   protected override createRenderRoot() {
     const root = super.createRenderRoot();
     // Apply custom theme (only supported if your app uses one)
@@ -24,6 +28,17 @@ export class Example extends LitElement {
         min="08:00"
         max="16:00"
         .step="${60 * 30}"
+        error-message="${this.errorMessage}"
+        @change="${(event: TimePickerChangeEvent) => {
+          const { min, max, value } = event.target;
+          if (value < min) {
+            this.errorMessage = 'Too early, choose another time';
+          } else if (value > max) {
+            this.errorMessage = 'Too late, choose another time';
+          } else {
+            this.errorMessage = '';
+          }
+        }}"
       ></vaadin-time-picker>
       <!-- end::snippet[] -->
     `;
