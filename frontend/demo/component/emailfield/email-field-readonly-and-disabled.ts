b549fedb115292de0caed5dc16181f226e7798ad
rolfsmeds <rolf@vaadin.com>
2023-03-27 08:34:02 +0300

    Email field docs refac

---
+++ frontend/demo/component/emailfield/email-field-readonly-and-disabled.ts
@@ -0,0 +1,31 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import '@vaadin/horizontal-layout';
+import '@vaadin/email-field';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('email-field-readonly-and-disabled')
+export class Example extends LitElement {
+  protected override createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  protected override render() {
+    return html`
+      <vaadin-horizontal-layout theme="spacing">
+        <!-- tag::snippet[] -->
+        <vaadin-email-field readonly label="Read-only" value="example@example.com">
+        </vaadin-email-field>
+
+        <vaadin-email-field disabled label="Disabled">
+        </vaadin-email-field>
+        <!-- end::snippet[] -->
+      </vaadin-horizontal-layout>
+    `;
+  }
+}
