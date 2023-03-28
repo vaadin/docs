ae6449eaf18e5ef702ee63837865d85f43c92e2c
rolfsmeds <rolf@vaadin.com>
2023-03-28 10:11:06 +0300

    Select docs refac

---
+++ frontend/demo/component/select/select-basic-features.ts
@@ -0,0 +1,57 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import { html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import '@vaadin/select';
+import '@vaadin/icon';
+import '@vaadin/icons';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('select-basic-features')
+export class Example extends LitElement {
+  protected override createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  protected override render() {
+    return html`
+        <!-- tag::snippet[] -->
+        <vaadin-select
+          label="Label"
+          helper-text="Helper text"
+          placeholder="Placeholder"
+          .items="${this.items}">
+          <vaadin-tooltip slot="tooltip" text="Tooltip text"></vaadin-tooltip>
+          <vaadin-icon slot="prefix" icon="vaadin:vaadin-h"></vaadin-icon>
+        </vaadin-select>
+        <!-- end::snippet[] -->
+    `;
+  }
+
+  @state()
+  private items = [
+    {
+      label: 'Most recent first',
+      value: 'recent',
+    },
+    {
+      label: 'Rating: high to low',
+      value: 'rating-desc',
+    },
+    {
+      label: 'Rating: low to high',
+      value: 'rating-asc',
+    },
+    {
+      label: 'Price: high to low',
+      value: 'price-desc',
+    },
+    {
+      label: 'Price: low to high',
+      value: 'price-asc',
+    },
+  ];
+}
