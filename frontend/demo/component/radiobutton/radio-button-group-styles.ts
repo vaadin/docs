0a9fa9342c7e0cdd3305f8f4d3729fd8c21a1f7b
rolfsmeds <rolf@vaadin.com>
2023-04-03 17:38:59 +0300

    Added RBG and CBG docs refac

---
+++ frontend/demo/component/radiobutton/radio-button-group-styles.ts
@@ -0,0 +1,31 @@
+import 'Frontend/demo/init'; // hidden-source-line
+
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import '@vaadin/radio-group';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('radio-button-group-styles')
+export class Example extends LitElement {
+  protected override createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  protected override render() {
+    return html`
+        <!-- tag::snippet[] -->
+        <vaadin-radio-button-group theme="helper-above-field"
+          label="Label" helper-text="Helper text">
+          <!-- end::snippet[] -->
+          <vaadin-radio-button value="1" label="Item 1"></vaadin-radio-button>
+          <vaadin-radio-button value="2" label="Item 2"></vaadin-radio-button>
+          <vaadin-radio-button value="3" label="Item 3"></vaadin-radio-button>
+          <!-- tag::snippet[] -->
+        </vaadin-radio-button-group>
+        <!-- end::snippet[] -->
+    `;
+  }
+}
