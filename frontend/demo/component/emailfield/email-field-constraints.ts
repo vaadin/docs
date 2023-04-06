edd521dae9bc7bbb46c72e1bc6f881dd3eaf7d95
rolfsmeds <rolf@vaadin.com>
2023-04-06 10:32:31 +0300

    Removed min/max and allowed-chars from EmailField docs

--- frontend/demo/component/emailfield/email-field-constraints.ts
+++ frontend/demo/component/emailfield/email-field-constraints.ts
@@ -20,8 +20,6 @@ export class Example extends LitElement {
       <vaadin-email-field
         pattern="^.+@example\\.com$"
         required
-        min-length="13"
-        max-length="20"
         label="Email address"
         error-message="Enter a valid example.com email address"
         helper-text="Only example.com addresses allowed"
