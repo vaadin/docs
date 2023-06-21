8cd5f297f6c8f033601799349407c0087bbe7696
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-06-21 11:28:13 +0300

    feat: setup react live examples for hilla (#2513)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
