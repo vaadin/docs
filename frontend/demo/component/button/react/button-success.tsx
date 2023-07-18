55ffdf4b39609f326eb9344bde7917c8beacf1be
Ahsin Siddique <ahsin.siddique@gmail.com>
2023-07-18 22:52:37 +0800

    fix button success minor issues

--- frontend/demo/component/button/react/button-success.tsx
+++ frontend/demo/component/button/react/button-success.tsx
@@ -1,18 +1,18 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from '@hilla/react-components/Button.js';
 import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout.js';
 
 function Example() {
   return (
     <>
-      {/* tag::snippet[] */}
-      <HorizontalLayout theme="spacing" style={{ alignItems: 'baseline' }}>
+      <HorizontalLayout theme="spacing">
+        {/* tag::snippet[] */}
         <Button theme="primary success">Primary</Button>
         <Button theme="secondary success">Secondary</Button>
         <Button theme="tertiary success">Tertiary</Button>
+        {/* end::snippet[] */}
       </HorizontalLayout>
-      {/* end::snippet[] */}
     </>
   );
 }
