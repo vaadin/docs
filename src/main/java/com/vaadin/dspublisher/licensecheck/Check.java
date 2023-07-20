32bfe690e33ce93b5414a9cae498a43e6dee1bde
Jouni Koivuviita <jouni@vaadin.com>
2023-07-20 13:18:30 +0300

    align license check DSP version with dspublisher-scripts.js

--- src/main/java/com/vaadin/dspublisher/licensecheck/Check.java
+++ src/main/java/com/vaadin/dspublisher/licensecheck/Check.java
@@ -6,7 +6,7 @@ import com.vaadin.pro.licensechecker.LicenseChecker;
 public class Check {
     public static void main(String[] args) {
         try {
-            LicenseChecker.checkLicense("vaadin-dspublisher", "2.1.1",
+            LicenseChecker.checkLicense("vaadin-dspublisher", "2.1.3",
                     (BuildType) null);
         } catch (Exception e) {
             System.exit(1);
