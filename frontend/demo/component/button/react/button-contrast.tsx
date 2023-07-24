831d3745106644be3361a23674f517124d08d952
Ahsin Siddique <ahsin.siddique@gmail.com>
2023-07-24 22:46:12 +0800

    docs: add button contrast and sizes react examples

---
+++ frontend/demo/component/button/react/button-contrast.tsx
@@ -0,0 +1,20 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React from 'react';
+import { Button } from '@hilla/react-components/Button.js';
+import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout.js';
+
+function Example() {
+  return (
+    <>
+      <HorizontalLayout theme="spacing">
+        {/* tag::snippet[] */}
+        <Button theme="primary contrast">Primary</Button>
+        <Button theme="secondary contrast">Secondary</Button>
+        <Button theme="tertiary contrast">Tertiary</Button>
+        {/* end::snippet[] */}
+      </HorizontalLayout>
+    </>
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
