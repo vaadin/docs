717c5ff54c293e128b811f45f6263e764cc4e1f5
Tomi Virkki <virkki@vaadin.com>
2023-08-02 09:57:00 +0300

    chore: remove unnecessary wrapper from button-images example

--- frontend/demo/component/button/react/button-images.tsx
+++ frontend/demo/component/button/react/button-images.tsx
@@ -2,17 +2,16 @@ import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-lin
 import React from 'react';
 import { Button } from '@hilla/react-components/Button.js';
 import img from '../../../../../src/main/resources/images/vaadin-logo-dark.png';
-import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout.js';
 
 function Example() {
   return (
-    <HorizontalLayout theme="spacing">
+    <>
       {/* tag::snippet[] */}
       <Button theme="icon">
         <img src={img} width="100" alt="Vaadin logo" />
       </Button>
       {/* end::snippet[] */}
-    </HorizontalLayout>
+    </>
   );
 }
 
