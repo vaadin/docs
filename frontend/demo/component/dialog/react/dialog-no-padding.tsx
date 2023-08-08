c86d22d68a6c3fee678a67181545155e864555de
Tomi Virkki <virkki@vaadin.com>
2023-08-08 17:50:37 +0300

    docs: add React examples for dialog

---
+++ frontend/demo/component/dialog/react/dialog-no-padding.tsx
@@ -0,0 +1,52 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React, { useState } from 'react';
+import { Button } from '@hilla/react-components/Button.js';
+import { Dialog } from '@hilla/react-components/Dialog.js';
+import { Grid } from '@hilla/react-components/Grid.js';
+import { GridColumn } from '@hilla/react-components/GridColumn.js';
+import { GridSelectionColumn } from '@hilla/react-components/GridSelectionColumn.js';
+
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
+import { getPeople } from 'Frontend/demo/domain/DataService';
+
+function Example(): JSX.Element {
+  const [dialogOpened, setDialogOpened] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  React.useEffect(() => {
+    getPeople({ count: 50 }).then((result) => setPeople(result.people));
+  }, []);
+
+  return (
+    <>
+      {/* tag::snippet[] */}
+      <Dialog
+        theme="no-padding"
+        header-title="Filter reports by users:"
+        opened={dialogOpened}
+        onOpenedChanged={({ detail }) => setDialogOpened(detail.value)}
+        footerRenderer={() => (
+          <Button theme="primary" onClick={() => setDialogOpened(false)}>
+            Filter
+          </Button>
+        )}
+      >
+        <Grid items={people} style={{ width: '500px', maxWidth: '100%' }}>
+          <GridSelectionColumn />
+          <GridColumn header="Name">
+            {({ item }) => (
+              <>
+                {item.firstName} {item.lastName}
+              </>
+            )}
+          </GridColumn>
+        </Grid>
+      </Dialog>
+
+      {/* end::snippet[] */}
+      <Button onClick={() => setDialogOpened(true)}>Show dialog</Button>
+    </>
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
---
+++ frontend/demo/component/dialog/react/dialog-resizable.tsx
@@ -0,0 +1,55 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React, { useState } from 'react';
+import { Button } from '@hilla/react-components/Button.js';
+import { Dialog } from '@hilla/react-components/Dialog.js';
+import { VerticalLayout } from '@hilla/react-components/VerticalLayout.js';
+import { Grid } from '@hilla/react-components/Grid.js';
+import { GridColumn } from '@hilla/react-components/GridColumn.js';
+import { getPeople } from 'Frontend/demo/domain/DataService';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
+
+function Example() {
+  const [dialogOpened, setDialogOpened] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  React.useEffect(() => {
+    getPeople({ count: 50 }).then((result) => setPeople(result.people));
+  }, []);
+
+  function open() {
+    setDialogOpened(true);
+  }
+
+  return (
+    <>
+      {/* tag::snippet[] */}
+      <Dialog
+        headerTitle="Employee list"
+        resizable
+        draggable
+        opened={dialogOpened}
+        onOpenedChanged={(event) => {
+          setDialogOpened(event.detail.value);
+        }}
+      >
+        <VerticalLayout
+          theme="spacing"
+          style={{ maxWidth: '100%', minWidth: '300px', height: '100%', alignItems: 'stretch' }}
+        >
+          <Grid items={people}>
+            <GridColumn path="firstName" title="First name" />
+            <GridColumn path="lastName" title="Last name" />
+            <GridColumn path="email" title="Email" />
+            <GridColumn path="profession" title="Profession" />
+            <GridColumn path="membership" title="Membership" />
+          </Grid>
+        </VerticalLayout>
+      </Dialog>
+      {/* end::snippet[] */}
+
+      <Button onClick={open}>Show dialog</Button>
+    </>
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
