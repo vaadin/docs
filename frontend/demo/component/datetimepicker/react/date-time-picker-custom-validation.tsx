78f6d1246dd783a8373b9a536272df4cd25beb64
Tomi Virkki <virkki@vaadin.com>
2023-08-08 17:49:32 +0300

    docs: add React examples for date-time-picker

---
+++ frontend/demo/component/datetimepicker/react/date-time-picker-custom-validation.tsx
@@ -0,0 +1,19 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React from 'react';
+import { DateTimePicker } from '@hilla/react-components/DateTimePicker.js';
+
+function Example() {
+  const errorMessage = 'The selected day of week or time is not available';
+
+  return (
+    // tag::snippet[]
+    <DateTimePicker
+      label="Appointment date and time"
+      helperText="Open Mondays-Fridays, 8:00-12:00, 13:00-16:00"
+      step={60 * 30}
+    />
+    // end::snippet[]
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
---
+++ frontend/demo/component/datetimepicker/react/date-time-picker-min-max.tsx
@@ -0,0 +1,37 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React, { useState } from 'react';
+import { DateTimePicker } from '@hilla/react-components/DateTimePicker.js';
+import { addDays, format, isAfter, isBefore, parseISO } from 'date-fns';
+
+// tag::snippet[]
+function Example() {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [value, setValue] = useState(() => addDays(new Date(), 7));
+  const [minDate] = useState(new Date());
+  const [maxDate] = useState(() => addDays(new Date(), 60));
+
+  return (
+    <DateTimePicker
+      label="Appointment date and time"
+      helperText="Must be within 60 days from today"
+      value={format(value, "yyyy-MM-dd'T'HH:00:00")}
+      min={format(minDate, "yyyy-MM-dd'T'HH:00:00")}
+      max={format(maxDate, "yyyy-MM-dd'T'HH:00:00")}
+      errorMessage={errorMessage}
+      onValueChanged={({ detail: { value: newValue } }) => {
+        const date = parseISO(newValue ?? '');
+        setValue(date);
+        if (isBefore(date, minDate)) {
+          setErrorMessage('Too early, choose another date and time');
+        } else if (isAfter(date, maxDate)) {
+          setErrorMessage('Too late, choose another date and time');
+        } else {
+          setErrorMessage('');
+        }
+      }}
+    />
+  );
+}
+// end::snippet[]
+
+export default reactExample(Example); // hidden-source-line
