65288492dac9b32f5bda2c9595cc4571bb0ba801
Tomi Virkki <virkki@vaadin.com>
2023-08-08 17:52:24 +0300

    docs: add React examples for grid-pro

---
+++ frontend/demo/component/gridpro/react/grid-pro-highlight-editable-cells.tsx
@@ -0,0 +1,27 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React, { useState, useEffect } from 'react';
+import { GridPro } from '@hilla/react-components/GridPro.js';
+import { getPeople } from 'Frontend/demo/domain/DataService';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
+import { GridColumn } from '@hilla/react-components/GridColumn.js';
+import { GridProEditColumn } from '@hilla/react-components/GridProEditColumn.js';
+
+function Example() {
+  const [items, setItems] = useState<Person[]>([]);
+  useEffect(() => {
+    getPeople().then(({ people }) => setItems(people));
+  }, []);
+
+  return (
+    // tag::snippet[]
+    <GridPro theme="highlight-editable-cells" items={items}>
+      <GridColumn path="firstName" />
+      <GridColumn path="lastName" />
+      <GridColumn path="membership" />
+      <GridProEditColumn path="email" header="Email (Editable)" />
+    </GridPro>
+    // end::snippet[]
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
