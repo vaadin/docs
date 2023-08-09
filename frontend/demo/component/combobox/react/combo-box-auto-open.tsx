11e3a1bde86f42d0aa89aef90b96cb0a9e0589ee
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-08-09 16:21:17 +0300

    docs: add React examples for combo-box (#2640)

--- frontend/demo/component/combobox/react/combo-box-auto-open.tsx
+++ frontend/demo/component/combobox/react/combo-box-auto-open.tsx
@@ -1,18 +1,25 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React, { useEffect, useState } from 'react';
 import { ComboBox } from '@hilla/react-components/ComboBox.js';
-import { getCountries } from 'Frontend/demo/domain/DataService';
 import type Country from 'Frontend/generated/com/vaadin/demo/domain/Country';
+import { getCountries } from 'Frontend/demo/domain/DataService';
 
 function Example() {
   const [items, setItems] = useState<Country[]>([]);
+
   useEffect(() => {
-    getCountries().then((data) => setItems(data));
+    getCountries().then((countries) => setItems(countries));
   }, []);
 
   return (
     // tag::snippet[]
-    <ComboBox label="Country" item-label-path="name" item-value-path="id" items={items} />
+    <ComboBox
+      autoOpenDisabled
+      label="Country"
+      itemLabelPath="name"
+      itemValuePath="id"
+      items={items}
+    ></ComboBox>
     // end::snippet[]
   );
 }
