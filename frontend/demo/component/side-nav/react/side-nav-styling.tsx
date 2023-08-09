ee335e7a9a96953ba8b344bfd21c365b0d634077
Tomi Virkki <virkki@vaadin.com>
2023-08-09 11:50:50 +0300

    docs: add React examples for side-nav

---
+++ frontend/demo/component/side-nav/react/side-nav-styling.tsx
@@ -0,0 +1,43 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import { patchSideNavNavigation } from './side-nav-helper'; // hidden-source-line
+import React, { useEffect, useRef } from 'react';
+import { SideNav, type SideNavElement } from '@hilla/react-components/SideNav.js';
+import { SideNavItem } from '@hilla/react-components/SideNavItem.js';
+import { Icon } from '@hilla/react-components/Icon.js';
+import '@vaadin/icons';
+
+function Example() {
+  const sideNavRef = useRef<SideNavElement>(null);
+
+  useEffect(() => {
+    if (sideNavRef.current) {
+      // Example-specific workaround
+      patchSideNavNavigation(sideNavRef.current);
+    }
+  }, []);
+
+  return (
+    <div className="side-nav-sample">
+      <div>
+        {/* tag::snippet[] */}
+        <SideNav style={{ width: '100%' }}>
+          <SideNavItem path="/dashboard">
+            <Icon icon="vaadin:dashboard" slot="prefix" />
+            Dashboard
+          </SideNavItem>
+          <SideNavItem path="/inbox">
+            <Icon icon="vaadin:envelope" slot="prefix" />
+            Inbox
+          </SideNavItem>
+          <SideNavItem path="https://vaadin.com" className="external">
+            <Icon icon="vaadin:vaadin-h" slot="prefix" />
+            Vaadin website
+          </SideNavItem>
+        </SideNav>
+        {/* end::snippet[] */}
+      </div>
+    </div>
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
