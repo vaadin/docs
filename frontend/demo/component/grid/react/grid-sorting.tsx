7d96abe78ec8b4bbad27954265810ce3fda9c494
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-08-10 11:51:48 +0300

    docs: add rest of the grid react examples (#2634)

--- frontend/demo/component/grid/react/grid-sorting.tsx
+++ frontend/demo/component/grid/react/grid-sorting.tsx
@@ -1,25 +1,12 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@hilla/react-components/Grid.js';
-import { GridColumn } from '@hilla/react-components/GridColumn.js';
+import { GridSortColumn } from '@hilla/react-components/GridSortColumn.js';
 import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 import { getPeople } from 'Frontend/demo/domain/DataService';
-import { format } from 'date-fns';
-
-function randomDate() {
-  const futureDate = new Date(Date.now() + Math.floor(Math.random() * 10000000000));
-  return format(futureDate, 'P');
-}
-
-function randomAmount() {
-  return Intl.NumberFormat('en', { style: 'currency', currency: 'EUR' }).format(
-    Math.floor(Math.random() * 1000000)
-  );
-}
 
 function Example() {
   const [items, setItems] = useState<Person[]>([]);
-
   useEffect(() => {
     getPeople().then(({ people }) =>
       setItems(
@@ -34,11 +21,11 @@ function Example() {
   return (
     // tag::snippet[]
     <Grid items={items}>
-      <GridColumn path="displayName" header="Name" />
-      <GridColumn header="Due">{() => <span>{randomDate()}</span>}</GridColumn>
-      <GridColumn header="Amount" textAlign="end">
-        {() => <span style={{ fontVariantNumeric: 'tabular-nums' }}>{randomAmount()}</span>}
-      </GridColumn>
+      <GridSortColumn path="id" />
+      <GridSortColumn header="Name" path="displayName" />
+      <GridSortColumn path="email" />
+      <GridSortColumn path="profession" />
+      <GridSortColumn path="birthday" />
     </Grid>
     // end::snippet[]
   );
