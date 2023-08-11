cab3059a52391b9d18ecfe849b5665a69b42bb83
Tomi Virkki <virkki@vaadin.com>
2023-08-11 09:32:26 +0300

    fix patterns

--- frontend/demo/component/textarea/react/text-area-constraints.tsx
+++ frontend/demo/component/textarea/react/text-area-constraints.tsx
@@ -10,7 +10,7 @@ function Example() {
       minlength={5}
       maxlength={50}
       pattern="^[A-Z]([A-Za-z0-9,-\s])*\.$"
-      allowedCharPattern="[A-Za-z0-9,.-\s]"
+      allowedCharPattern="[A-Za-z0-9,.\-\s]"
       label="Sentence"
       helperText="Must be one complete sentence ending in a period, between 5 and 50 characters long"
       style={{ width: '100%' }}
