978d46bdcd543316dae3c7305afc5552ad73cd22
Serhii Kulykov <iamkulykov@gmail.com>
2023-08-14 12:32:20 +0300

    Update frontend/demo/component/menubar/react/menu-bar-basic.tsx

--- frontend/demo/component/menubar/react/menu-bar-basic.tsx
+++ frontend/demo/component/menubar/react/menu-bar-basic.tsx
@@ -1,6 +1,6 @@
-import { MenuBar, type MenuBarItem } from '@hilla/react-components/MenuBar.js';
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React, { useState } from 'react';
+import { MenuBar, type MenuBarItem } from '@hilla/react-components/MenuBar.js';
 
 function Example() {
   // tag::snippet[]
