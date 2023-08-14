c40c959a9455ef507b7583517c4d8b5df0583701
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-08-14 11:23:20 +0300

    docs: add React examples for date-time-picker (#2690)

--- frontend/demo/component/datetimepicker/react/date-time-picker-custom-validation.tsx
+++ frontend/demo/component/datetimepicker/react/date-time-picker-custom-validation.tsx
@@ -1,16 +1,16 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React from 'react';
-import { TimePicker } from '@hilla/react-components/TimePicker.js';
+import { DateTimePicker } from '@hilla/react-components/DateTimePicker.js';
 
 function Example() {
   // This is a placeholder file. Binder support for React is not yet implemented. See https://github.com/vaadin/hilla/issues/587
+  const errorMessage = 'The selected day of week or time is not available';
+
   return (
     // tag::snippet[]
-    <TimePicker
-      label="Appointment time"
-      helperText="Open 8:00-12:00, 13:00-16:00"
-      min="08:00"
-      max="16:00"
+    <DateTimePicker
+      label="Appointment date and time"
+      helperText="Open Mondays-Fridays, 8:00-12:00, 13:00-16:00"
       step={60 * 30}
     />
     // end::snippet[]
