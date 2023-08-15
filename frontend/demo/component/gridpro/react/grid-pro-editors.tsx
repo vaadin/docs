fd31f902578145e5ac84aba7cd21cf38db7e64fe
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-08-15 12:12:34 +0300

    docs: add React examples for grid-pro (#2687)

---
+++ frontend/demo/component/gridpro/react/grid-pro-editors.tsx
@@ -0,0 +1,44 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React, { useEffect, useState } from 'react';
+import { GridPro } from '@hilla/react-components/GridPro.js';
+import { GridProEditColumn } from '@hilla/react-components/GridProEditColumn.js';
+import { getPeople } from 'Frontend/demo/domain/DataService';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
+
+function Example() {
+  const [items, setItems] = useState<Person[]>([]);
+  useEffect(() => {
+    getPeople().then(({ people }) => setItems(people));
+  }, []);
+
+  return (
+    // tag::snippet[]
+    <GridPro items={items} enterNextRow>
+      <GridProEditColumn path="firstName" />
+
+      <GridProEditColumn
+        path="membership"
+        editorType="select"
+        editorOptions={['Regular', 'Premium', 'VIP']}
+      />
+
+      <GridProEditColumn path="subscriber" editorType="checkbox" />
+
+      {/*
+        Unfortunately custom editors don't currently work with React-based GridPro
+        See: https://github.com/vaadin/react-components/issues/135
+
+        <GridProEditColumn
+          path="birthday"
+          editModeRenderer={({ item: { birthday } }) => (
+            <DatePicker style={{ width: '100%' }} value={birthday} />
+          )}
+        >
+          {({ item: { birthday } }) => format(parseISO(birthday), 'MM/dd/yyyy')}
+        </GridProEditColumn> */}
+    </GridPro>
+    // end::snippet[]
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
---
+++ frontend/demo/component/gridpro/react/grid-pro-prevent-save.tsx
@@ -0,0 +1,56 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React from 'react';
+import { GridPro, type GridProItemPropertyChangedEvent } from '@hilla/react-components/GridPro.js';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
+import { Notification } from '@vaadin/notification';
+import { GridProEditColumn } from '@hilla/react-components/GridProEditColumn.js';
+import { getPeople } from 'Frontend/demo/domain/DataService';
+
+function Example() {
+  const [items, setItems] = React.useState<Person[]>([]);
+
+  React.useEffect(() => {
+    getPeople().then(({ people }) => setItems(people));
+  }, []);
+
+  const showErrorNotification = (msg: string) => {
+    Notification.show(msg, { position: 'bottom-center', theme: 'error' });
+  };
+
+  const itemPropertyListener = (event: GridProItemPropertyChangedEvent<Person>) => {
+    switch (event.detail.path) {
+      case 'address.phone':
+        if (typeof event.detail.value === 'string' && !/^[0-9-]+$/.test(event.detail.value)) {
+          // Incorrect phone
+          event.preventDefault();
+          showErrorNotification('Enter a valid phone number');
+        }
+        break;
+      case 'email':
+        if (
+          typeof event.detail.value === 'string' &&
+          !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(event.detail.value)
+        ) {
+          // Incorrect email
+          event.preventDefault();
+          showErrorNotification('Enter a valid email address');
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    // tag::snippet[]
+    <GridPro items={items} onItemPropertyChanged={itemPropertyListener}>
+      <GridProEditColumn path="firstName" />
+      <GridProEditColumn path="lastName" />
+      <GridProEditColumn path="email" />
+      <GridProEditColumn path="address.phone" />
+    </GridPro>
+    // end::snippet[]
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
