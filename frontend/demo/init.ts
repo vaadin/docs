6011fefca021446a53f3e65c40a862a35f2bc2e3
Tomi Virkki <virkki@vaadin.com>
2023-08-15 13:43:33 +0300

    feat: clone side nav examples

--- frontend/demo/init.ts
+++ frontend/demo/init.ts
@@ -1,6 +1,11 @@
 import client from 'Frontend/generated/connect-client.default';
 import { applyTheme } from 'Frontend/generated/theme';
 
+// TODO: For some reason the feature flag for side-nav doesn't work. Need to enable manually.
+window.Vaadin ||= {};
+window.Vaadin.featureFlags ||= {};
+window.Vaadin.featureFlags.sideNavComponent = true;
+
 // Apply the theme, so that overlay elements styles and custom property overrides work as expected
 applyTheme(document);
 
