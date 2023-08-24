7dec54b6844076d18c2b115c43718eabb503b80d
Bruno Rezende <brunovianarezende@gmail.com>
2023-08-24 17:09:14 -0300

    Example for Pie, Gauge and Solid Gauge charts

---
+++ src/main/java/com/vaadin/demo/component/charts/charttypes/ChartTypeGauge.java
@@ -0,0 +1,83 @@
+package com.vaadin.demo.component.charts.charttypes;
+
+import com.vaadin.demo.DemoExporter;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.Configuration;
+import com.vaadin.flow.component.charts.model.Labels;
+import com.vaadin.flow.component.charts.model.ListSeries;
+import com.vaadin.flow.component.charts.model.Pane;
+import com.vaadin.flow.component.charts.model.PlotBand;
+import com.vaadin.flow.component.charts.model.PlotOptionsGauge;
+import com.vaadin.flow.component.charts.model.SeriesTooltip;
+import com.vaadin.flow.component.charts.model.TickPosition;
+import com.vaadin.flow.component.charts.model.YAxis;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("chart-type-gauge")
+public class ChartTypeGauge extends Div {
+    public ChartTypeGauge() {
+        // tag::snippet[]
+        Chart chart = new Chart(ChartType.GAUGE);
+
+        final Configuration configuration = chart.getConfiguration();
+        configuration.getChart().setType(ChartType.GAUGE);
+        configuration.setTitle("Speedometer");
+        configuration.getChart().setWidth(500);
+
+        Pane pane = configuration.getPane();
+        pane.setStartAngle(-150);
+        pane.setEndAngle(150);
+
+        YAxis yAxis = new YAxis();
+        yAxis.setTitle("km/h");
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+        yAxis.setTickLength(10);
+        yAxis.setTickPixelInterval(30);
+        yAxis.setTickPosition(TickPosition.INSIDE);
+        yAxis.setMinorTickLength(10);
+        yAxis.setMinorTickInterval("auto");
+        yAxis.setMinorTickPosition(TickPosition.INSIDE);
+
+        Labels labels = new Labels();
+        labels.setStep(2);
+        labels.setRotation("auto");
+        yAxis.setLabels(labels);
+
+        PlotBand[] bands = new PlotBand[3];
+        bands[0] = new PlotBand();
+        bands[0].setFrom(0);
+        bands[0].setTo(120);
+        bands[0].setClassName("band-0");
+        bands[1] = new PlotBand();
+        bands[1].setFrom(120);
+        bands[1].setTo(160);
+        bands[1].setClassName("band-1");
+        bands[2] = new PlotBand();
+        bands[2].setFrom(160);
+        bands[2].setTo(200);
+        bands[2].setClassName("band-2");
+        yAxis.setPlotBands(bands);
+
+        configuration.addyAxis(yAxis);
+
+        final ListSeries series = new ListSeries("Speed", 89);
+
+        PlotOptionsGauge plotOptionsGauge = new PlotOptionsGauge();
+        SeriesTooltip tooltip = new SeriesTooltip();
+        tooltip.setValueSuffix(" km/h");
+        plotOptionsGauge.setTooltip(tooltip);
+        series.setPlotOptions(plotOptionsGauge);
+
+        configuration.addSeries(series);
+        // end::snippet[]
+
+        add(new VerticalLayout(chart));
+    }
+
+    public static class Exporter extends DemoExporter<ChartTypeGauge> { // hidden-source-line
+    } // hidden-source-line
+}
