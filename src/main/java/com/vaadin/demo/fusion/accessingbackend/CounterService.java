ba0ebfdd84b1f2d1b2edf7e2df764bb71ac49e6f
Soroosh Taefi <taefi.soroosh@gmail.com>
2023-09-13 11:31:34 +0300

    feat: add note about @BrowserCallable (#2796)
    * feat: add note about @BrowserCallable
    
    Related to vaadin/hilla#1221
    
    Fixes: vaadin/hilla#1237
    
    * Fix `since` tag
    
    * apply suggestion from code review
    
    Co-authored-by: Luciano Vernaschi <cromoteca@users.noreply.github.com>
    
    * Edited only sub-section added.
    
    ---------
    
    Co-authored-by: Luciano Vernaschi <luciano@vaadin.com>
    Co-authored-by: Luciano Vernaschi <cromoteca@users.noreply.github.com>
    Co-authored-by: russelljtdyer <6652767+russelljtdyer@users.noreply.github.com>

---
+++ src/main/java/com/vaadin/demo/fusion/accessingbackend/CounterService.java
@@ -0,0 +1,21 @@
+package com.vaadin.demo.fusion.accessingbackend;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+// tag::snippet[]
+import dev.hilla.BrowserCallable;
+
+/**
+ * A service that counts numbers.
+ */
+@BrowserCallable
+@AnonymousAllowed
+public class CounterService {
+
+    /**
+     * A method that adds one to the argument.
+     */
+    public int addOne(int number) {
+        return number + 1;
+    }
+}
+// end::snippet[]
