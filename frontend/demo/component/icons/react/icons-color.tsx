b12a8e5fef5ed8d3a6dfe7134271f06820bb2933
Diego Cardoso <diego@vaadin.com>
2023-09-15 11:21:09 +0300

    docs: implement remaining react examples

--- frontend/demo/component/icons/react/icons-color.tsx
+++ frontend/demo/component/icons/react/icons-color.tsx
@@ -1,11 +1,17 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React from 'react';
+import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout.js';
+import { Icon } from '@hilla/react-components/Icon.js';
+import codeBranch from '../../../../../src/main/resources/icons/code-branch.svg';
 
 function Example() {
   return (
-    // tag::snippet[]
-    <p>placeholder</p>
-    // end::snippet[]
+    <HorizontalLayout theme="spacing" className="items-center">
+      {/* tag::snippet[] */}
+      <Icon src={codeBranch} style={{ color: 'red' }} />
+      <Icon iconClass="fa fa-user" style={{ color: 'red' }} />
+      {/* end::snippet[] */}
+    </HorizontalLayout>
   );
 }
 
--- frontend/demo/component/icons/react/icons-sizing.tsx
+++ frontend/demo/component/icons/react/icons-sizing.tsx
@@ -1,10 +1,22 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React from 'react';
+import { HorizontalLayout } from '@hilla/react-components/HorizontalLayout.js';
+import { Icon } from '@hilla/react-components/Icon.js';
+import codeBranch from '../../../../../src/main/resources/icons/code-branch.svg';
 
 function Example() {
   return (
     // tag::snippet[]
-    <p>placeholder</p>
+    <HorizontalLayout theme="spacing" className="items-end">
+      {/* tag::snippet[] */}
+      <Icon src={codeBranch} />
+      <Icon
+        src={codeBranch}
+        style={{ height: 'var(--lumo-icon-size-l)', width: 'var(--lumo-icon-size-l)' }}
+      />
+      <Icon src={codeBranch} style={{ height: '48px', width: '48px' }} />
+      {/* end::snippet[] */}
+    </HorizontalLayout>
     // end::snippet[]
   );
 }
