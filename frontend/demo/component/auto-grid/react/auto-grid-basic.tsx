f7c231867c96b70fd943d48909bb2bf57d89767f
Anton Platonov <anton@vaadin.com>
2023-09-28 16:01:48 +0300

    Apply suggestions from code review
    Co-authored-by: Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>

--- frontend/demo/component/auto-grid/react/auto-grid-basic.tsx
+++ frontend/demo/component/auto-grid/react/auto-grid-basic.tsx
@@ -1,17 +1,15 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
-import React from 'react';
+import React from 'react'; // hidden-source-line
+// tag::snippet[]
 import { AutoGrid } from '@hilla/react-grid';
 // tag::apply-backend[]
 import { ProductService } from 'Frontend/generated/endpoints';
 import ProductModel from 'Frontend/generated/com/vaadin/demo/fusion/crud/ProductModel';
 
 function Example() {
-  return (
-    // tag::snippet[]
-    <AutoGrid service={ProductService} model={ProductModel}/>
-    // end::snippet[]
-  );
+  return <AutoGrid service={ProductService} model={ProductModel} />;
 }
 // end::apply-backend[]
+// end::snippet[]
 
 export default reactExample(Example); // hidden-source-line
