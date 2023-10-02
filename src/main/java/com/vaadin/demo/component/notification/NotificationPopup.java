f53323ecea524d4b5c96a94bbba78444143d4546
Matti Tahvonen <matti@vaadin.com>
2023-10-02 11:57:38 +0300

    Moved noice of the example to a separate class

--- src/main/java/com/vaadin/demo/component/notification/NotificationPopup.java
+++ src/main/java/com/vaadin/demo/component/notification/NotificationPopup.java
@@ -1,14 +1,13 @@
 package com.vaadin.demo.component.notification;
 
-import com.vaadin.flow.component.Text;
+import com.vaadin.demo.DemoExporter;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.contextmenu.ContextMenu;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.Route;
-import com.vaadin.demo.DemoExporter; // hidden-source-line
 
 import java.util.Arrays;
 
@@ -17,15 +16,8 @@ public class NotificationPopup extends Div {
 
     public NotificationPopup() {
         // tag::snippet[]
-        Span numberOfNotifications = new Span("4");
-        numberOfNotifications.getElement().getThemeList().addAll(
-                Arrays.asList("badge", "error", "primary", "small", "pill"));
-        numberOfNotifications.getStyle().set("position", "absolute")
-                .set("transform", "translate(-40%, -85%)");
-
-        Button bellBtn = new Button(VaadinIcon.BELL_O.create());
-        bellBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        bellBtn.getElement().appendChild(numberOfNotifications.getElement());
+        var bellBtn = new MessagesButton();
+        bellBtn.setUnreadMessages(4);
 
         ContextMenu menu = new ContextMenu();
         menu.setOpenOnClick(true);
@@ -38,6 +30,30 @@ public class NotificationPopup extends Div {
 
         add(bellBtn);
     }
+
+    // tag::messagesBtn[]
+
+    public class MessagesButton extends Button {
+
+        private final Span numberOfNotifications;
+
+        public MessagesButton() {
+            super(VaadinIcon.BELL_O.create());
+            numberOfNotifications = new Span();
+            numberOfNotifications.getStyle()
+                    .setPosition(Style.Position.ABSOLUTE)
+                    .setTransform("translate(-40%, -85%)");
+            numberOfNotifications.getElement().getThemeList().addAll(
+                    Arrays.asList("badge", "error", "primary", "small", "pill"));
+            getElement().appendChild(numberOfNotifications.getElement());
+        }
+
+        public void setUnreadMessages(int unread) {
+            numberOfNotifications.setText(unread + "");
+        }
+
+    }
+    // end::messagesBtn[]
 
     public static class Exporter extends DemoExporter<NotificationPopup> { // hidden-source-line
     } // hidden-source-line
