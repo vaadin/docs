c4abbb51fd1e2e487b801d514585646b9953c709
Tomi Virkki <virkki@vaadin.com>
2023-10-03 10:30:25 +0300

    chore: add clarifying comments to icon examples

--- frontend/demo/component/icons/react/svg-standalone.tsx
+++ frontend/demo/component/icons/react/svg-standalone.tsx
@@ -5,6 +5,8 @@ import { Icon } from '@hilla/react-components/Icon.js';
 
 function Example() {
   return (
+    // As an alternative to importing the SVG file, you can set the src property to be any relative or absolute URL.
+    // For example, if you have the icons in your "myapp" application theme, you could set the value to "/themes/myapp/code-branch.svg"
     // tag::snippet[]
     <Icon src={codeBranchIcon} />
     // end::snippet[]
