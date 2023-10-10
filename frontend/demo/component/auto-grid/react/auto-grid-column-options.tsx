9269b3e32a93789234c33dbf9ffda92b5f5812bb
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-10-10 17:42:12 +0200

    docs: auto-grid column options (#2872)
    * docs: auto-grid column options
    
    * First pass at editing: added text only.
    
    * Second pass at editing: added text only.
    
    ---------
    
    Co-authored-by: russelljtdyer <6652767+russelljtdyer@users.noreply.github.com>

---
+++ frontend/demo/component/auto-grid/react/auto-grid-column-options.tsx
@@ -0,0 +1,31 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import { autoGridHostStyles } from './auto-grid-host-styles'; // hidden-source-line
+import React from 'react';
+import { AutoGrid } from '@hilla/react-grid';
+import { ProductService } from 'Frontend/generated/endpoints';
+import ProductModel from 'Frontend/generated/com/vaadin/demo/fusion/crud/ProductModel';
+import Product from 'Frontend/generated/com/vaadin/demo/fusion/crud/Product';
+
+// tag::snippet[]
+function PriceRenderer({ item }: { item: Product }) {
+  const { price } = item;
+  const color = price > 4 ? 'red' : 'green';
+  return <span style={{ fontWeight: 'bold', color }}>{price}</span>;
+}
+
+function Example() {
+  return (
+    <AutoGrid
+      service={ProductService}
+      model={ProductModel}
+      columnOptions={{
+        price: {
+          renderer: PriceRenderer,
+        },
+      }}
+    />
+  );
+}
+// end::snippet[]
+
+export default reactExample(Example, autoGridHostStyles); // hidden-source-line
