52c864c6482601818d46299eb77760afda94638d
taefi <taefi.soroosh@gmail.com>
2023-10-12 10:01:56 +0300

    add headers and refine some statements

--- frontend/demo/component/auto-grid/react/auto-grid-custom-columns.tsx
+++ frontend/demo/component/auto-grid/react/auto-grid-custom-columns.tsx
@@ -1,7 +1,7 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import { autoGridHostStyles } from './auto-grid-host-styles'; // hidden-source-line
 import React from 'react';
-import { AutoGrid } from '@hilla/react-grid';
+import { AutoGrid } from '@hilla/react-crud';
 import { ProductService } from 'Frontend/generated/endpoints';
 import ProductModel from 'Frontend/generated/com/vaadin/demo/fusion/crud/ProductModel';
 import { GridColumn } from "@hilla/react-components/GridColumn";
