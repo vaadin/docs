7cbd3d5dbbc626178ea7723ed82b8311dacc8115
Jouni Koivuviita <jouni@vaadin.com>
2023-10-24 10:21:19 +0300

    chore: use DSP v2.2.0-rc.1

--- src/main/java/com/vaadin/dspublisher/licensecheck/Check.java
+++ src/main/java/com/vaadin/dspublisher/licensecheck/Check.java
@@ -6,7 +6,7 @@ import com.vaadin.pro.licensechecker.LicenseChecker;
 public class Check {
     public static void main(String[] args) {
         try {
-            LicenseChecker.checkLicense("vaadin-dspublisher", "2.2.0-beta.3",
+            LicenseChecker.checkLicense("vaadin-dspublisher", "2.2.0-rc.1",
                     (BuildType) null);
         } catch (Exception e) {
             System.exit(1);
