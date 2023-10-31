3c75db9564ac4e78b3997c86666aee4a05fbca69
taefi <taefi.soroosh@gmail.com>
2023-10-31 23:44:31 +0200

    second draft

---
+++ frontend/demo/component/auto-form/react/auto-form-template-id-version.tsx
@@ -0,0 +1,25 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React from 'react'; // hidden-source-line
+// tag::snippet[]
+import { ExperimentalAutoForm as AutoForm } from '@hilla/react-crud';
+// tag::apply-backend[]
+import { EmployeeService } from 'Frontend/generated/endpoints';
+import ProductModel from 'Frontend/generated/com/vaadin/demo/fusion/crud/ProductModel';
+
+function Example() {
+  return <AutoForm service={EmployeeService} model={ProductModel}
+                               customLayoutRenderer={{
+                                 template: [
+                                   ['firstName', 'lastName', 'gender'],
+                                   ['dateOfBirth', 'email'],
+                                   ['startDate', 'endDate', 'active'],
+                                   ['description'],
+                                   ['id', 'version'],
+                                ],
+                              }}
+        />;
+}
+// end::apply-backend[]
+// end::snippet[]
+
+export default reactExample(Example); // hidden-source-line
