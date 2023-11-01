5591f4555740a6c21a951a66989a05178679330f
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-11-01 10:16:20 +0200

    Make TS example consistent with Java code

--- frontend/demo/auth/auth.ts
+++ frontend/demo/auth/auth.ts
@@ -2,7 +2,9 @@ import { configureAuth } from '@hilla/react-auth';
 import { UserInfoService } from 'Frontend/generated/endpoints';
 
 // Configure auth to use `UserInfoService.getUserInfo`
-const auth = configureAuth(UserInfoService.getUserInfo);
+const auth = configureAuth(UserInfoService.getUserInfo, {
+  getRoles: (userInfo: any) => (userInfo as any).authorities as readonly string[],
+});
 
 // Export auth provider and useAuth hook, which are automatically
 // typed to the result of `UserInfoService.getUserInfo`
