aa4590a39b51443c67cbae2cd5aa2dc38570130e
Soroosh Taefi <taefi.soroosh@gmail.com>
2023-11-20 14:33:43 +0200

    fix: set id for new items in CrudService mock (#2974)
    * set id for new items
    
    * Update frontend/demo/services/CrudService.ts
    
    Co-authored-by: Luciano Vernaschi <luciano@cromoteca.com>
    
    * prettier
    
    ---------
    
    Co-authored-by: Luciano Vernaschi <luciano@cromoteca.com>

--- frontend/demo/services/CrudService.ts
+++ frontend/demo/services/CrudService.ts
@@ -123,7 +123,10 @@ export class CrudMockService<T extends AbstractEntity> implements CrudService<T>
     if (existingIndex >= 0) {
       this.items[existingIndex] = item;
     } else {
-      this.items.push(item);
+      this.items.push({
+        ...item,
+        id: this.items.map((item) => item.id).reduce((a, b) => Math.max(a, b), 0) + 1,
+      });
     }
     return Promise.resolve(item);
   }
