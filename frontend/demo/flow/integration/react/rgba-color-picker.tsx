6fcc7a5550b3eddac8adc0f9d3c2d34cf659a8aa
Anton Platonov <platosha@gmail.com>
2024-02-22 09:48:15 +0200

    split Java example into more steps

--- frontend/demo/flow/integration/react/rgba-color-picker.tsx
+++ frontend/demo/flow/integration/react/rgba-color-picker.tsx
@@ -1,6 +1,6 @@
 import {type ReactElement} from 'react';
 import {type RgbaColor, RgbaColorPicker} from "react-colorful";
-import {ReactAdapterElement, type RenderHooks} from "Frontend/generated/flow/ReactAdapter.js";
+import {ReactAdapterElement, type RenderHooks} from "Frontend/generated/flow/ReactAdapter";
 
 class RgbaColorPickerElement extends ReactAdapterElement {
   protected override render(hooks: RenderHooks): ReactElement | null { // <1>
--- src/main/java/com/vaadin/demo/flow/integration/react/RgbaColorPicker.java
+++ src/main/java/com/vaadin/demo/flow/integration/react/RgbaColorPicker.java
@@ -7,16 +7,24 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.react.ReactAdapterComponent;
 
+// tag::annotations[]
 @NpmPackage(value = "react-colorful", version = "5.6.1") // <1>
 @JsModule("./demo/flow/integration/react/rgba-color-picker.tsx") // <2>
 @Tag("rgba-color-picker") // <3>
+// tag::accessors[]
+// tag::event[]
+// tag::constructor[]
 public class RgbaColorPicker extends ReactAdapterComponent {
 
+    // end::annotations[]
+    // end::accessors[]
+    // end::event[]
     public RgbaColorPicker() {
-
         setColor(new RgbaColor(255, 0, 0, 0.5));
     }
+    // end::constructor[]
 
+    // tag::accessors[]
     public RgbaColor getColor() {
         return getState("color", RgbaColor.class);
     }
@@ -24,8 +32,18 @@ public class RgbaColorPicker extends ReactAdapterComponent {
     public void setColor(RgbaColor color) {
         setState("color", color);
     }
+    // end::accessors[]
 
+    // tag::event[]
     public void addColorChangeListener(Consumer<RgbaColor> listener) {
         addStateChangeListener("color", RgbaColor.class, listener);
     }
+    // tag::annotations[]
+    // tag::accessors[]
+    // tag::constructor[]
+
 }
+// end::annotations[]
+// end::accessors[]
+// end::constructor[]
+// end::event[]
