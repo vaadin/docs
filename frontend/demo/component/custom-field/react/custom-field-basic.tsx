1ba374463751f49c63af29c755f9e82e9d90b99a
web-padawan <iamkulykov@gmail.com>
2024-02-29 16:16:19 +0200

    docs: replace title attribute usage with accessibleName

--- frontend/demo/component/custom-field/react/custom-field-basic.tsx
+++ frontend/demo/component/custom-field/react/custom-field-basic.tsx
@@ -37,9 +37,9 @@ function Example() {
       required
       onValueChanged={validate}
     >
-      <DatePicker placeholder="Start date"></DatePicker>
+      <DatePicker accessibleName="Start date" placeholder="Start date" />
       &ndash;
-      <DatePicker placeholder="End date"></DatePicker>
+      <DatePicker accessibleName="End date" placeholder="End date" />
     </CustomField>
     // end::snippet[]
   );
