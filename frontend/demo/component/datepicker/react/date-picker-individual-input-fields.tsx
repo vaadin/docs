3fd1263a8cf1a1941ac9828da79d82cbc5562663
Anton Platonov <platosha@gmail.com>
2024-04-19 16:32:29 +0300

    fix: date-picker individual input example infinite loop

--- frontend/demo/component/datepicker/react/date-picker-individual-input-fields.tsx
+++ frontend/demo/component/datepicker/react/date-picker-individual-input-fields.tsx
@@ -1,6 +1,6 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React, { useEffect } from 'react';
-import { useSignal } from '@vaadin/hilla-react-signals';
+import { useSignal, useComputed } from '@vaadin/hilla-react-signals';
 import { useSignals } from '@preact/signals-react/runtime'; // hidden-source-line
 import { ComboBox } from '@vaadin/react-components/ComboBox.js';
 import { HorizontalLayout } from '@vaadin/react-components/HorizontalLayout.js';
@@ -8,7 +8,7 @@ import getDaysInMonth from 'date-fns/getDaysInMonth';
 
 function Example() {
   useSignals(); // hidden-source-line
-  const months = [
+  const months = useComputed<string []>(() => [
     'January',
     'February',
     'March',
@@ -21,30 +21,30 @@ function Example() {
     'October',
     'November',
     'December',
-  ];
+  ]);
 
-  const years = Array.from({ length: 100 }, (_, k) => new Date().getFullYear() - 99 + k);
+  const years = useComputed<number []>(() => Array.from(
+    { length: 100 },
+    (_, k) => new Date().getFullYear() - 99 + k),
+  );
 
   const selectedYear = useSignal<number | undefined>(undefined);
   const selectedMonth = useSignal<string | undefined>(undefined);
   const selectedDay = useSignal<number | undefined>(undefined);
-  const selectableDays = useSignal<number[]>([]);
-
-  useEffect(() => {
-    if (!selectedYear || !selectedMonth) {
-      selectableDays.value = [];
-      return;
+  const selectableDays = useComputed<number []>(() => {
+    if (!selectedYear.value || !selectedMonth.value) {
+      return [];
     }
 
     const startOfMonth = new Date(
       selectedYear.value ?? 0,
-      months.indexOf(selectedMonth.value ?? 'January'),
+      months.value.indexOf(selectedMonth.value ?? 'January'),
       1
     );
     const lengthOfMonth = getDaysInMonth(startOfMonth);
 
-    selectableDays.value = Array.from({ length: lengthOfMonth }, (_, k) => k + 1);
-  }, [selectedYear, selectedMonth]);
+    return Array.from({ length: lengthOfMonth }, (_, k) => k + 1);
+  });
 
   return (
     // tag::snippet[]
@@ -52,8 +52,8 @@ function Example() {
       <ComboBox
         label="Year"
         style={{ width: '6em' }}
-        items={years}
-        value={String(selectedYear)}
+        items={years.value}
+        value={String(selectedYear.value)}
         onValueChanged={(e) => {
           selectedYear.value = parseInt(e.detail.value);
           selectedMonth.value = undefined;
@@ -64,9 +64,9 @@ function Example() {
       <ComboBox
         label="Month"
         style={{ width: '9em' }}
-        items={months}
+        items={months.value}
         value={selectedMonth.value}
-        disabled={!selectedYear}
+        disabled={!selectedYear.value}
         onValueChanged={(e) => {
           selectedMonth.value = e.detail.value;
           selectedDay.value = undefined;
@@ -77,10 +77,11 @@ function Example() {
         label="Day"
         style={{ width: '5em' }}
         items={selectableDays.value}
-        value={String(selectedDay)}
+        value={String(selectedDay.value)}
         disabled={!selectedYear.value || !selectedMonth.value}
         onValueChanged={(e) => {
-          selectedDay.value = parseInt(e.detail.value);
+          const day = Number.parseInt(e.detail.value);
+          selectedDay.value = !Number.isNaN(day) ? day : undefined;
         }}
       />
     </HorizontalLayout>
