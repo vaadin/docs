d2cc9c08c7892f115f663bd317b68b96cb5c6c9b
Mikhail Shabarov <mikhail@vaadin.com>
2024-04-19 12:14:07 +0300

    fix: Add tag to a file links

--- frontend/demo/flow/integration/react/rgba-color-picker.tsx
+++ frontend/demo/flow/integration/react/rgba-color-picker.tsx
@@ -1,3 +1,4 @@
+// tag::class[]
 import {type ReactElement} from 'react';
 import {type RgbaColor, RgbaColorPicker} from "react-colorful";
 import {ReactAdapterElement, type RenderHooks} from "Frontend/generated/flow/ReactAdapter";
@@ -18,3 +19,4 @@ customElements.define(
   "rgba-color-picker",
   RgbaColorPickerElement
 ); // <4>
+// end::class[]
