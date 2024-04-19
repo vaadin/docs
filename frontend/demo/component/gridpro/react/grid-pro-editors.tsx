23955b2debab68ce025ca0b7a9059c783f4dfc2d
Sascha Ißbrücker <sissbruecker@vaadin.com>
2024-04-19 14:03:13 +0200

    chore: restore custom editor example

--- frontend/demo/component/gridpro/react/grid-pro-editors.tsx
+++ frontend/demo/component/gridpro/react/grid-pro-editors.tsx
@@ -1,7 +1,9 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React, { useEffect, useState } from 'react';
+import { DatePicker } from '@vaadin/react-components/DatePicker.js';
 import { GridPro } from '@vaadin/react-components/GridPro.js';
 import { GridProEditColumn } from '@vaadin/react-components/GridProEditColumn.js';
+import { format, parseISO } from 'date-fns';
 import { getPeople } from 'Frontend/demo/domain/DataService';
 import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 
@@ -15,27 +17,20 @@ function Example() {
     // tag::snippet[]
     <GridPro items={items} enterNextRow>
       <GridProEditColumn path="firstName" />
-
       <GridProEditColumn
         path="membership"
         editorType="select"
         editorOptions={['Regular', 'Premium', 'VIP']}
       />
-
       <GridProEditColumn path="subscriber" editorType="checkbox" />
-
-      {/*
-        Unfortunately custom editors don't currently work with React-based GridPro
-        See: https://github.com/vaadin/react-components/issues/135
-
-        <GridProEditColumn
-          path="birthday"
-          editModeRenderer={({ item: { birthday } }) => (
-            <DatePicker style={{ width: '100%' }} value={birthday} />
-          )}
-        >
-          {({ item: { birthday } }) => format(parseISO(birthday), 'MM/dd/yyyy')}
-        </GridProEditColumn> */}
+      <GridProEditColumn
+        path="birthday"
+        editModeRenderer={({ item: { birthday } }) => (
+          <DatePicker style={{ width: '100%' }} value={birthday} />
+        )}
+      >
+        {({ item: { birthday } }) => format(parseISO(birthday), 'MM/dd/yyyy')}
+      </GridProEditColumn>
     </GridPro>
     // end::snippet[]
   );
