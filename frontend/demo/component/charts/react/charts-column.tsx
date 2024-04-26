001064911e1acdd5a21b7e22aa2826ce1f3d8fd8
web-padawan <iamkulykov@gmail.com>
2024-04-26 13:33:22 +0300

    docs: add Charts subpage for React usage

---
+++ frontend/demo/component/charts/react/charts-column.tsx
@@ -0,0 +1,18 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React from 'react';
+import { Chart } from '@vaadin/react-components/Chart.js';
+import { ChartSeries } from '@vaadin/react-components/ChartSeries.js';
+
+function Example() {
+  return (
+    // tag::snippet[]
+    <Chart title="Column Chart" type="column" categories={['Jan', 'Feb', 'Mar']}>
+      <ChartSeries title="Tokyo" values={[49.9, 71.5, 106.4]} />
+      <ChartSeries title="New York" values={[83.6, 78.8, 98.5]} />
+      <ChartSeries title="London" values={[48.9, 38.8, 39.3]} />
+    </Chart>
+    // end::snippet[]
+  );
+}
+
+export default reactExample(Example); // hidden-source-line
