45017a6d71a6c2111fd50b44b8a52c9ff2cd439e
Sergey Vinogradov <mr.vursen@gmail.com>
2024-04-26 15:45:05 +0300

    docs: extract file handling into a separate article

--- frontend/demo/auth/index.tsx
+++ frontend/demo/auth/index.tsx
@@ -2,13 +2,13 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes.js';
+// import { router } from './routes.js';
 import { AuthProvider } from './auth';
 
 function App() {
-    return <AuthProvider>
-        <RouterProvider router={router} />
-    </AuthProvider>;
+    // return <AuthProvider>
+    //     <RouterProvider router={router} />
+    // </AuthProvider>;
 }
 
 createRoot(document.getElementById('outlet')!).render(createElement(App));
