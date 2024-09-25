c46f47a35bb4f0e837a5a520fd1d71ba2d2d7baf
Sergey Vinogradov <mr.vursen@gmail.com>
2024-09-25 16:55:39 +0400

    move imports back under their tag annotations

--- frontend/demo/component/auto-grid/react/auto-grid-basic.tsx
+++ frontend/demo/component/auto-grid/react/auto-grid-basic.tsx
@@ -2,8 +2,8 @@ import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-lin
 import React from 'react'; // hidden-source-line
 // tag::snippet[]
 import { AutoGrid } from '@vaadin/hilla-react-crud';
-import ProductModel from 'Frontend/generated/com/vaadin/demo/fusion/crud/ProductModel';
 // tag::apply-backend[]
+import ProductModel from 'Frontend/generated/com/vaadin/demo/fusion/crud/ProductModel';
 import { ProductService } from 'Frontend/generated/endpoints';
 import { autoGridHostStyles } from './auto-grid-host-styles'; // hidden-source-line
 
