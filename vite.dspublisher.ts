dc23b4e6e5e561985c780cc52d4ea265b2b4a7f3
Tomi Virkki <tomivirkki@users.noreply.github.com>
2024-10-14 16:07:04 +0300

    Update vite.dspublisher.ts
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>

--- vite.dspublisher.ts
+++ vite.dspublisher.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import type { UserConfig } from 'vite';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
