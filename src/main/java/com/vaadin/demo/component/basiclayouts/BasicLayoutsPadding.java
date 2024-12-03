0826b8db29c1bc07cc951fb2d8c4405e247c86d5
Sascha Ißbrücker <sissbruecker@vaadin.com>
2024-12-03 16:05:19 +0100

    refactor: vertical layout example improvements

--- src/main/java/com/vaadin/demo/component/basiclayouts/BasicLayoutsPadding.java
+++ src/main/java/com/vaadin/demo/component/basiclayouts/BasicLayoutsPadding.java
@@ -3,37 +3,49 @@ package com.vaadin.demo.component.basiclayouts;
 import com.vaadin.demo.DemoExporter; // hidden-source-line
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.router.Route;
 
 @Route("basic-layouts/padding")
 public class BasicLayoutsPadding extends Div {
 
-    private static final String ENABLED_OPTION = "Enabled";
-    private static final String DISABLED_OPTION = "Disabled";
-
     public BasicLayoutsPadding() {
+        HorizontalLayout parent = new HorizontalLayout();
+        parent.getStyle().setBorder("0");
+        add(parent);
+
+        Div wrapper = new Div();
+        wrapper.setWidthFull();
+        wrapper.add(new Paragraph("Vertical layout without padding:"));
+        parent.add(wrapper);
+
         // tag::snippet[]
-        VerticalLayout layout = new VerticalLayout();
-        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        layout.add(new Button("Button 1"));
-        layout.add(new Button("Button 2"));
-        layout.add(new Button("Button 3"));
-
-        RadioButtonGroup<String> radioButtonGroup = new RadioButtonGroup<>();
-        radioButtonGroup.setLabel("Padding");
-        radioButtonGroup.setItems(ENABLED_OPTION, DISABLED_OPTION);
-        radioButtonGroup.setValue(ENABLED_OPTION);
-        radioButtonGroup.addValueChangeListener(
-                e -> layout.setPadding(ENABLED_OPTION.equals(e.getValue())));
+        // VerticalLayout has padding enabled by default, use setPadding to disable it
+        VerticalLayout layoutWithoutPadding = new VerticalLayout();
+        layoutWithoutPadding.setPadding(false);
         // end::snippet[]
+        layoutWithoutPadding.setAlignItems(FlexComponent.Alignment.STRETCH);
+        layoutWithoutPadding.add(new Button("Button 1"));
+        layoutWithoutPadding.add(new Button("Button 2"));
+        layoutWithoutPadding.add(new Button("Button 3"));
+        wrapper.add(layoutWithoutPadding);
 
-        this.setClassName("basic-layouts-example");
-        layout.setClassName("height-4xl");
+        wrapper = new Div();
+        wrapper.setWidthFull();
+        wrapper.add(new Paragraph("Vertical layout with padding:"));
+        parent.add(wrapper);
 
-        this.add(layout, radioButtonGroup);
+        VerticalLayout layoutWithPadding = new VerticalLayout();
+        layoutWithPadding.setAlignItems(FlexComponent.Alignment.STRETCH);
+        layoutWithPadding.add(new Button("Button 1"));
+        layoutWithPadding.add(new Button("Button 2"));
+        layoutWithPadding.add(new Button("Button 3"));
+        wrapper.add(layoutWithPadding);
+
+        setClassName("basic-layouts-example");
     }
 
     public static class Exporter // hidden-source-line
