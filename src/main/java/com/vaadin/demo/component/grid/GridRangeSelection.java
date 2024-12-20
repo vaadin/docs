671199b32731031f1939debf9c1ebc33cd2fa656
Sergey Vinogradov <mr.vursen@gmail.com>
2024-12-20 13:52:15 +0400

    use selectionModel.selectItems instead of asMultiSelect.select

--- src/main/java/com/vaadin/demo/component/grid/GridRangeSelection.java
+++ src/main/java/com/vaadin/demo/component/grid/GridRangeSelection.java
@@ -1,8 +1,6 @@
 package com.vaadin.demo.component.grid;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.vaadin.demo.domain.Person;
 import com.vaadin.flow.component.grid.Grid;
@@ -21,7 +19,6 @@ public class GridRangeSelection extends Div {
     public GridRangeSelection() {
         // tag::snippet[]
         Grid<Person> grid = new Grid<>(Person.class, false);
-        grid.setSelectionMode(Grid.SelectionMode.MULTI);
         grid.addColumn(Person::getFirstName).setHeader("First name");
         grid.addColumn(Person::getLastName).setHeader("Last name");
         grid.addColumn(Person::getEmail).setHeader("Email");
@@ -29,40 +26,40 @@ public class GridRangeSelection extends Div {
         List<Person> people = DataService.getPeople();
         grid.setItems(people);
 
-        ((GridMultiSelectionModel<Person>) grid.getSelectionModel())
-                .addClientItemToggleListener(event -> {
-                    Person item = event.getItem();
+        GridMultiSelectionModel<Person> selectionModel = (GridMultiSelectionModel<Person>) grid
+                .setSelectionMode(Grid.SelectionMode.MULTI);
 
-                    // If the anchor point isn't set, set it to the current item
-                    if (rangeStartItem == null) {
-                        rangeStartItem = item;
-                    }
+        selectionModel.addClientItemToggleListener(event -> {
+            Person item = event.getItem();
 
-                    if (event.isShiftKey()) {
-                        // Calculcate the range of items between the anchor
-                        // point and the current item
-                        GridListDataView<Person> dataView = grid
-                                .getListDataView();
-                        int rangeStart = dataView.getItemIndex(rangeStartItem)
-                                .get();
-                        int rangeEnd = dataView.getItemIndex(item).get();
-                        Set<Person> rangeItems = dataView.getItems()
-                                .skip(Math.min(rangeStart, rangeEnd))
-                                .limit(Math.abs(rangeStart - rangeEnd) + 1)
-                                .collect(Collectors.toSet());
+            // If the anchor point isn't set, set it to the current item
+            if (rangeStartItem == null) {
+                rangeStartItem = item;
+            }
 
-                        // Update the selection state of items within the range
-                        // based on the state of the current item
-                        if (event.isSelected()) {
-                            grid.asMultiSelect().select(rangeItems);
-                        } else {
-                            grid.asMultiSelect().deselect(rangeItems);
-                        }
-                    }
+            if (event.isShiftKey()) {
+                // Calculcate the range of items between the anchor
+                // point and the current item
+                GridListDataView<Person> dataView = grid.getListDataView();
+                int rangeStart = dataView.getItemIndex(rangeStartItem).get();
+                int rangeEnd = dataView.getItemIndex(item).get();
+                Person[] rangeItems = dataView.getItems()
+                        .skip(Math.min(rangeStart, rangeEnd))
+                        .limit(Math.abs(rangeStart - rangeEnd) + 1)
+                        .toArray(Person[]::new);
 
-                    // Update the anchor point to the current item
-                    rangeStartItem = item;
-                });
+                // Update the selection state of items within the range
+                // based on the state of the current item
+                if (event.isSelected()) {
+                    selectionModel.selectItems(rangeItems);
+                } else {
+                    selectionModel.deselectItems(rangeItems);
+                }
+            }
+
+            // Update the anchor point to the current item
+            rangeStartItem = item;
+        });
         // end::snippet[]
 
         add(grid);
