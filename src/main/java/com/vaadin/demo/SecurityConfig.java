fc2c0e651933b61845891002e9be28f849e4fe6a
Luciano Vernaschi <luciano@vaadin.com>
2025-02-04 11:28:58 +0100

    Add a download example

--- src/main/java/com/vaadin/demo/SecurityConfig.java
+++ src/main/java/com/vaadin/demo/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -12,6 +13,12 @@ public class SecurityConfig extends VaadinWebSecurity {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+
+        // tag::download[]
+        // Restrict access to FileDownloadEndpoint to authenticated users
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(new AntPathRequestMatcher("/download/**")).authenticated());
+        // end::download[]
     }
 
 }
