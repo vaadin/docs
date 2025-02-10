e23621331e9dc907eb2d4cdb0bc6ec6c56027738
Břetislav Wajtr <bretislav@vaadin.com>
2025-02-10 10:40:26 +0200

    docs: documentation of the new Vaadin Charts Gantt Chart feature (#4078)
    ---------
    
    Co-authored-by: Russell J.T. Dyer <6652767+russelljtdyer@users.noreply.github.com>
    Co-authored-by: Russell JT Dyer <russelljtdyer@users.noreply.github.com>

---
+++ src/main/java/com/vaadin/demo/component/charts/charttypes/gantt/GanttYAxisAsGridDemo.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2000-2024 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.demo.component.charts.charttypes.gantt;
+
+import com.vaadin.demo.DemoExporter;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Route("chart-gantt-y-axis-as-grid")
+public class GanttYAxisAsGridDemo extends Div {
+
+    private static final Instant TODAY = Instant.now().truncatedTo(ChronoUnit.DAYS);
+
+    // tag::snippet[]
+    public GanttYAxisAsGridDemo() {
+        Chart chart = new Chart(ChartType.GANTT);
+
+        final Configuration configuration = chart.getConfiguration();
+        configuration.setTitle("Gantt Chart Grid Demo");
+
+        XAxis xAxis = configuration.getxAxis();
+        xAxis.setTickPixelInterval(70);
+
+        YAxis yAxis = configuration.getyAxis();
+        yAxis.setType(AxisType.CATEGORY);
+
+        AxisGrid grid = new AxisGrid();
+        grid.setEnabled(true);
+        grid.setColumns(List.of(
+                createProjectColumn(),
+                createEstDaysColumn(),
+                createStartDateColumn(),
+                createEndDateColumn())
+        );
+        yAxis.setGrid(grid);
+
+        PlotOptionsGantt plotOptionsGantt = new PlotOptionsGantt();
+        configuration.setPlotOptions(plotOptionsGantt);
+
+        final GanttSeries projectDevelopmentSeries = createProjectDevelopmentSeries();
+        PlotOptionsGantt seriesPlotOptions = new PlotOptionsGantt();
+        projectDevelopmentSeries.setPlotOptions(seriesPlotOptions);
+        configuration.addSeries(projectDevelopmentSeries);
+
+        add(chart);
+    }
+
+    private XAxis createProjectColumn() {
+        XAxis column = new XAxis();
+        column.setTitle("Project1");
+        final Labels label = new Labels();
+        label.setFormat("{point.name}");
+        column.setLabels(label);
+        return column;
+    }
+    // end::snippet[]
+
+    private XAxis createEstDaysColumn() {
+        XAxis column = new XAxis();
+        column.setTitle("Est. Days");
+        final Labels label = new Labels();
+        label.setUseHTML(true);
+        label.setFormatter("function () { var point = this.point,days = (1000 * 60 * 60 * 24)," + "    number = (point.x2 - point.x) / days; " + "    return '<div style=\"width: 50px; text-align: center\">' + Math.round(number * 100) / 100 + '</div>'; }");
+        column.setLabels(label);
+        return column;
+    }
+
+    private XAxis createStartDateColumn() {
+        XAxis column = new XAxis();
+        column.setTitle("Start Date");
+        final Labels label = new Labels();
+        label.setFormat("{point.start:%e. %b}");
+        column.setLabels(label);
+        return column;
+    }
+
+    private XAxis createEndDateColumn() {
+        XAxis column = new XAxis();
+        column.setTitle("End Date");
+        column.setOffset(30);
+        final Labels label = new Labels();
+        label.setFormat("{point.end:%e. %b}");
+        column.setLabels(label);
+        return column;
+    }
+
+    private GanttSeries createProjectDevelopmentSeries() {
+        GanttSeries series = new GanttSeries();
+        series.setName("Project 1");
+
+        GanttSeriesItem item;
+
+        item = new GanttSeriesItem("Start prototype", todayPlus(1), todayPlus(3));
+        item.setY(0);
+        item.setCustom(new TaskCustomData("Richards"));
+        series.add(item);
+
+        item = new GanttSeriesItem("Develop", todayPlus(2), todayPlus(5));
+        item.setY(1);
+        item.setCustom(new TaskCustomData("Oystein"));
+        series.add(item);
+
+        item = new GanttSeriesItem("Test prototype", todayPlus(5), todayPlus(7));
+        item.setY(2);
+        item.setCustom(new TaskCustomData("Torstein"));
+        series.add(item);
+
+        item = new GanttSeriesItem("Run acceptance tests", todayPlus(8), todayPlus(12));
+        item.setY(3);
+        item.setCustom(new TaskCustomData("Halliburton"));
+        series.add(item);
+
+        return series;
+    }
+
+    private Instant todayPlus(int days) {
+        return TODAY.plus(days, ChronoUnit.DAYS);
+    }
+
+    @SuppressWarnings("unused")
+    static class TaskCustomData extends AbstractConfigurationObject {
+        private String assignee;
+
+        public TaskCustomData(String assignee) {
+            this.assignee = assignee;
+        }
+
+        public String getAssignee() {
+            return assignee;
+        }
+
+        public void setAssignee(String assignee) {
+            this.assignee = assignee;
+        }
+    }
+
+    public static class Exporter extends DemoExporter<GanttYAxisAsGridDemo> { // hidden-source-line
+    } // hidden-source-line
+}
