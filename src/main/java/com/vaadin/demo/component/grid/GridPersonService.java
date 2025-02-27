32465c18a476148c9e69abdb516a302834c3fd39
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-02-27 11:02:38 +0100

    docs: update React grid data provider example (#4133)
    * docs: update React grid data provider example
    
    * fix mock not being used
    
    * make it work without a JPA repository
    
    * use new grid data provider hook
    
    * remove count implementation

---
+++ src/main/java/com/vaadin/demo/component/grid/GridPersonService.java
@@ -0,0 +1,27 @@
+package com.vaadin.demo.component.grid;
+
+import com.vaadin.demo.domain.Person;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.BrowserCallable;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+// tag::snippet[]
+@BrowserCallable
+@AnonymousAllowed
+public class GridPersonService {
+    private final GridPersonRepository personRepository;
+
+    public GridPersonService(GridPersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public @NonNull List<@NonNull Person> list(Pageable pageable, String filter) {
+        // Implement your data fetching logic here
+        // For this example, we're using a Spring Data repository
+        return personRepository.findByFullNameContainingIgnoreCaseOrProfessionContainingIgnoreCase(filter, filter, pageable);
+    }
+}
+// end::snippet[]
