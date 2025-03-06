5c639cc5fff6ae1fc32841899ea667c2c501d57b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-03-06 09:19:59 +0100

    docs: custom item data for context menu and menu bar (#4158)
    Co-authored-by: Russell J.T. Dyer <6652767+russelljtdyer@users.noreply.github.com>

---
+++ frontend/demo/component/contextmenu/react/context-menu-custom-item-data.tsx
@@ -0,0 +1,44 @@
+import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
+import React from 'react';
+import { ContextMenu, type ContextMenuItem } from '@vaadin/react-components';
+
+function Example() {
+  // tag::snippet[]
+  const items: Array<ContextMenuItem<{ value: string }>> = [
+    {
+      text: 'Copy as plain text',
+      value:
+        'Context Menu\n\nContext Menu is a component that you can attach to any component to display a context menu.',
+    },
+    {
+      text: 'Copy as HTML',
+      value:
+        '<h1>Context Menu</h1><p>Context Menu is a component that you can attach to any component to display a context menu.</p>',
+    },
+    {
+      text: 'Copy as Markdown',
+      value:
+        '# Context Menu\n\nContext Menu is a component that you can attach to any component to display a context menu.',
+    },
+  ];
+
+  return (
+    <ContextMenu
+      items={items}
+      onItemSelected={(event) => {
+        const value = event.detail.value.value;
+        if (value) {
+          navigator.clipboard.writeText(value);
+        }
+      }}
+    >
+      <h1>Context Menu</h1>
+      <p>
+        Context Menu is a component that you can attach to any component to display a context menu.
+      </p>
+    </ContextMenu>
+  );
+  // end::snippet[]
+}
+
+export default reactExample(Example); // hidden-source-line
---
+++ frontend/demo/component/menubar/menu-bar-custom-item-data.ts
@@ -0,0 +1,58 @@
+import 'Frontend/demo/init'; // hidden-source-line
+import '@vaadin/menu-bar';
+import { html, LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import type { MenuBarItem, MenuBarItemSelectedEvent } from '@vaadin/menu-bar';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('menu-bar-custom-item-data')
+export class Example extends LitElement {
+  protected override createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  // tag::snippet[]
+  @state()
+  private items: Array<MenuBarItem<{ value?: string }>> = [
+    {
+      text: 'Copy',
+      children: [
+        {
+          text: 'Copy as plain text',
+          value:
+            'Menu Bar\n\nMenu Bar is a horizontal button bar with hierarchical drop-down menus.',
+        },
+        {
+          text: 'Copy as HTML',
+          value:
+            '<h1>Menu Bar</h1><p>Menu Bar is a horizontal button bar with hierarchical drop-down menus.</p>',
+        },
+        {
+          text: 'Copy as Markdown',
+          value:
+            '# Menu Bar\n\nMenu Bar is a horizontal button bar with hierarchical drop-down menus.',
+        },
+      ],
+    },
+  ];
+
+  protected override render() {
+    return html`
+      <vaadin-menu-bar
+        .items="${this.items}"
+        @item-selected="${this.itemSelected}"
+      ></vaadin-menu-bar>
+    `;
+  }
+
+  itemSelected(e: MenuBarItemSelectedEvent<MenuBarItem<{ value?: string }>>) {
+    const value = e.detail.value.value;
+    if (value) {
+      navigator.clipboard.writeText(value);
+    }
+  }
+  // end::snippet[]
+}
