88ca2fbc3dd83bad3cec8c0912b8147bd92b4a29
Veera Paananen <veeraipaananen@gmail.com>
2025-03-19 13:53:06 +0200

    Fetch from staging CDN, remove Google font import

--- dspublisher/theme/init-browser.ts
+++ dspublisher/theme/init-browser.ts
@@ -44,19 +44,19 @@ class Header extends LitElement {
 
     return html`
       <link
-        rel="preload"
-        as="style"
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@600;700&display=swap"
+        crossorigin
+        rel="stylesheet"
+        href="https://cdn.vaadin.com/staging/website/antlers/v2/assets/fonts/nbinternationalpro/stylesheet.css"
       />
       <link
         rel="preload"
         as="style"
-        href="https://cdn.vaadin.com/website/antlers/v2/assets/icons/css/line-awesome.min.css"
+        href="https://cdn.vaadin.com/staging/website/antlers/v2/assets/icons/css/line-awesome.min.css"
       />
       <link
         rel="preload"
         as="style"
-        href="https://cdn.vaadin.com/website/hubspot-theme/v2/haas/css/haas.css"
+        href="https://cdn.vaadin.com/staging/website/hubspot-theme/v2/haas/css/haas.css"
       />
 
       <div id="haas-container"></div>
--- dspublisher/theme/init.js
+++ dspublisher/theme/init.js
@@ -7,22 +7,22 @@ import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function fetchAntlersStyleSheets() {
-  // Full collection of available style sheets: https://gitlab.vaadin.com/vaadincom/antlers
+  // Full collection of available stylesheets: https://gitlab.vaadin.com/vaadincom/antlers
   const antlersStyleSheets = [
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/custom-properties.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/reset.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/layout.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/typography.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/1-foundation/theme.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/2-components/button.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/2-components/dropdown.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/2-components/links.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/2-components/lists.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/2-components/tag.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/3-patterns/navigation.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/5-sections/addsearch.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/5-sections/cookie.css',
-    'https://cdn.vaadin.com/website/antlers/v2/assets/css/3-patterns/card.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/1-foundation/custom-properties.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/1-foundation/reset.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/1-foundation/layout.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/1-foundation/typography.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/1-foundation/theme.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/2-components/button.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/2-components/dropdown.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/2-components/links.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/2-components/lists.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/2-components/tag.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/3-patterns/navigation.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/5-sections/addsearch.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/5-sections/cookie.css',
+    'https://cdn.vaadin.com/staging/website/antlers/v2/assets/css/3-patterns/card.css',
   ];
 
   // No try-catch, let the build fail if there is an error fetching the style sheets
@@ -47,7 +47,7 @@ async function fetchAntlersStyleSheets() {
     });
 
     let localStyleSheetPath =
-      './antlers/' + url.split('https://cdn.vaadin.com/website/antlers/v2/')[1];
+      './antlers/' + url.split('https://cdn.vaadin.com/staging/website/antlers/v2/')[1];
 
     const localStyleSheetDir = localStyleSheetPath.split('/').slice(0, -1).join('/');
     fs.mkdirSync(path.resolve(__dirname, localStyleSheetDir), { recursive: true });
