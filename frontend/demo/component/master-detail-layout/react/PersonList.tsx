731d07ff88bc9d9254bb3d18fc16f52a4e91936e
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-04-14 16:15:59 +0200

    add basic examples

---
+++ frontend/demo/component/master-detail-layout/react/PersonList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Grid, GridColumn, VerticalLayout } from '@vaadin/react-components';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
+
+interface PersonListProps {
+  people: Person[];
+  selectedPerson: Person | null;
+
+  onSelect(person: Person | null): void;
+}
+
+function PersonList({ people, selectedPerson, onSelect }: PersonListProps) {
+  return (
+    <VerticalLayout theme="padding spacing" style={{ height: '100%' }}>
+      <h2>People</h2>
+      <p>Select a person to view their details:</p>
+      <Grid
+        items={people}
+        style={{ height: '100%' }}
+        selectedItems={selectedPerson ? [selectedPerson] : []}
+        onActiveItemChanged={(e) => {
+          onSelect(e.detail.value ?? null);
+        }}
+      >
+        <GridColumn path="firstName" />
+        <GridColumn path="lastName" />
+        <GridColumn path="email" />
+        <GridColumn path="profession" />
+      </Grid>
+    </VerticalLayout>
+  );
+}
+
+export default PersonList;
--- frontend/demo/component/master-detail-layout/react/master-detail-layout-vertical.tsx
+++ frontend/demo/component/master-detail-layout/react/master-detail-layout-vertical.tsx
@@ -1,14 +1,58 @@
-import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
-import React from 'react';
-import { MasterDetailLayout } from '@vaadin/react-components';
+import React, { useEffect } from 'react';
+import { useSignals } from '@preact/signals-react/runtime'; // hidden-source-line
+import { useSignal } from '@vaadin/hilla-react-signals';
+import { MasterDetailLayout, SplitLayout } from '@vaadin/react-components';
+import PersonDetail from 'Frontend/demo/component/master-detail-layout/react/PersonDetail';
+import PersonList from 'Frontend/demo/component/master-detail-layout/react/PersonList';
+import { masterDetailLayoutExample } from 'Frontend/demo/component/master-detail-layout/react/wrapper'; // hidden-source-line
+import { getPeople } from 'Frontend/demo/domain/DataService';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 
 function Example() {
+  useSignals(); // hidden-source-line
+  const items = useSignal<Person[]>([]);
+  const selectedPerson = useSignal<Person | null>(null);
+  useEffect(() => {
+    getPeople().then(({ people }) => {
+      items.value = people;
+    });
+  }, []);
+
   return (
-    <MasterDetailLayout orientation="vertical">
-      <MasterDetailLayout.Master></MasterDetailLayout.Master>
-      <MasterDetailLayout.Detail></MasterDetailLayout.Detail>
-    </MasterDetailLayout>
+    <SplitLayout orientation="vertical" style={{ height: '100%' }}>
+      {/* tag::snippet[] */}
+      <MasterDetailLayout
+        orientation="vertical"
+        masterMinSize="400px"
+        detailSize="250px"
+        stackThreshold="400px"
+      >
+        {/* end::snippet[] */}
+        <MasterDetailLayout.Master>
+          <PersonList
+            people={items.value}
+            selectedPerson={selectedPerson.value}
+            onSelect={(person) => {
+              selectedPerson.value = person;
+            }}
+          />
+        </MasterDetailLayout.Master>
+        <MasterDetailLayout.Detail>
+          {selectedPerson.value ? (
+            <PersonDetail
+              person={selectedPerson.value}
+              onClose={() => {
+                selectedPerson.value = null;
+              }}
+            />
+          ) : null}
+        </MasterDetailLayout.Detail>
+        {/* tag::snippet[] */}
+      </MasterDetailLayout>
+      {/* end::snippet[] */}
+      <div style={{ flex: '0', backgroundColor: 'var(--lumo-contrast-20pct)' }}></div>
+    </SplitLayout>
   );
 }
 
-export default reactExample(Example); // hidden-source-line
+export default masterDetailLayoutExample(Example); // hidden-source-line
