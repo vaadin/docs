7a566dfdc23c67c33ceaa6fffa5b8547478d3f62
Manuel Carrasco Moñino <manolo@vaadin.com>
2025-04-28 07:02:46 +0100

    refactor style check scripts (#4270)
    * move scripts to its own folder, and remove score
    
    * Adding changes to an adoc

--- scripts/style-check/style-check.sh
+++ scripts/style-check/style-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR=$(dirname "$0")
+
 if [[ -z $OPENAI_API_KEY ]]; then
  echo "Please set the OPENAI_API_KEY environment variable." >&2
  exit 1
@@ -22,14 +24,12 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-scripts/pr_file_pull.sh $1 $2 >&2
-
+bash $DIR/get-file-diferences.sh $1 $2 >&2
 
 if compgen -G *.new *.old > /dev/null; then
-  SYSTEM_PROMPT=$(cat scripts/system_prompt.txt)
-
-  scripts/multi_file_chat.sh "$SYSTEM_PROMPT" "Please provide a short review of the differences between the provided files. *Only* list things that need improvement. Do *not* repeat or review unchanged text. Rate the quality of all the changes on a s
-  cale from 1 to 10. Write the lowest score out of ten in the last line but do not refer to it as 'lowest score' just 'score'. Output your review using markdown" *.old *.new
+  SYSTEM_PROMPT=$(cat $DIR/system-prompt.txt)
+  USER_PROMPT=$(cat $DIR/user-prompt.txt)
+  bash $DIR/send-openai-request.sh "$SYSTEM_PROMPT" "$USER_PROMPT" *.old *.new
 else
    echo '{"choices":[{"message":{"content":""}}]}'
 fi
