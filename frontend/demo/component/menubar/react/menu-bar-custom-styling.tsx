4631df2cff0f5a84df6321086e96e5894664b9a3
Tatu Lund <tatu@vaadin.com>
2025-04-28 09:58:19 +0300

    Update menu-bar-custom-styling.tsx

--- frontend/demo/component/menubar/react/menu-bar-custom-styling.tsx
+++ frontend/demo/component/menubar/react/menu-bar-custom-styling.tsx
@@ -1,10 +1,10 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React from 'react';
-import { MenuBar } from '@vaadin/react-components/MenuBar.js';
+import { MenuBar, MenuBarItem } from '@vaadin/react-components/MenuBar.js';
 
 function Example() {
   // tag::snippet[]
-  const items = [
+  const items: Array<MenuBarItem> = [
     { text: 'View', className: 'bg-primary text-primary-contrast' },
     { text: 'Edit' },
     {
