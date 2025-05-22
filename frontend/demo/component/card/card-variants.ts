093d454eb227ca03fe3ca9db307ca0a590bc9014
rolfsmeds <rolf@vaadin.com>
2025-05-22 16:23:21 +0300

    Moved variants to Styling tab and combined the 3 main variants to a single sample.

---
+++ frontend/demo/component/card/card-variants.ts
@@ -0,0 +1,43 @@
+import 'Frontend/demo/init'; // hidden-source-line
+import '@vaadin/card';
+import { css, html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { applyTheme } from 'Frontend/generated/theme';
+
+@customElement('card-variants')
+export class Example extends LitElement {
+  protected override createRenderRoot() {
+    const root = super.createRenderRoot();
+    // Apply custom theme (only supported if your app uses one)
+    applyTheme(root);
+    return root;
+  }
+
+  protected override render() {
+    return html`
+      <div class="card-variant-layout">
+        <!-- tag::snippet[] -->
+        <vaadin-card card-title="Default">
+          <div>This is the default card style.</div>
+        </vaadin-card>
+
+        <vaadin-card card-title="Outlined" theme="outlined">
+          <div>Adds a solid outline around the card.</div>
+        </vaadin-card>
+
+        <vaadin-card card-title="Elevated" theme="elevated">
+          <div>This variant works better on a shaded background.</div>
+        </vaadin-card>
+        <!-- end::snippet[] -->
+      </div>
+    `;
+  }
+
+  static styles = css`
+    .card-variant-layout {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(12ch, 1fr));
+      gap: var(--lumo-space-m);
+    }
+  `;
+}
