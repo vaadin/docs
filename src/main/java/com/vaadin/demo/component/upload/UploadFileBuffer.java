a981908f2f95fea081869adae5fd5ee5375b032b
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2025-06-18 14:23:00 +0300

    chore: Fix broken imports

--- src/main/java/com/vaadin/demo/component/upload/UploadFileBuffer.java
+++ src/main/java/com/vaadin/demo/component/upload/UploadFileBuffer.java
@@ -4,11 +4,7 @@ import java.io.File;
 
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.upload.Upload;
-import com.vaadin.flow.component.upload.receivers.FileBuffer;
-import com.vaadin.flow.component.upload.receivers.FileData;
-import com.vaadin.flow.component.upload.receivers.MultiFileBuffer;
 import com.vaadin.flow.server.streams.FileUploadHandler;
-import com.vaadin.flow.server.streams.InMemoryUploadHandler;
 import com.vaadin.flow.server.streams.UploadHandler;
 
 public class UploadFileBuffer extends Div {
@@ -20,8 +16,8 @@ public class UploadFileBuffer extends Div {
                 (metadata, file) -> {
                     System.out.printf("File saved to: %s%n",
                             file.getAbsolutePath());
-                }, fileName -> new File(System.getProperty("java.io.tmpdir"),
-                        fileName));
+                }, metadata -> new File(System.getProperty("java.io.tmpdir"),
+                        metadata.fileName()));
         Upload fileUpload = new Upload(fileHandler);
         // end::snippet[]
 
