32952ab353b00a7e8512d4586852af903eb0fc9c
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2025-06-18 14:42:58 +0300

    chore: Fix broken imports (#4384)

--- src/main/java/com/vaadin/demo/flow/advanced/download/InputStreamDownloadView.java
+++ src/main/java/com/vaadin/demo/flow/advanced/download/InputStreamDownloadView.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
