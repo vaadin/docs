01054655f7b637e89d9b1d7cd78b158b3256c9f8
Tomi Virtanen <tltv@vaadin.com>
2025-07-22 11:59:58 +0300

    Update hilla upgrade guide and rest of the examples
    Updated for VaadinSecurityConfigurer.

--- src/main/java/com/vaadin/demo/fusion/security/stateless/SecurityConfig.java
+++ src/main/java/com/vaadin/demo/fusion/security/stateless/SecurityConfig.java
@@ -1,61 +1,67 @@
 package com.vaadin.demo.fusion.security.stateless;
 
-import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
-import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
-import com.vaadin.flow.spring.security.stateless.VaadinStatelessSecurityConfigurer;
-
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
-import static com.vaadin.flow.spring.security.RequestUtil.antMatchers;
-
 // tag::stateless-configure[]
 @EnableWebSecurity
 @Configuration
-@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
 @Profile("this-is-just-a-demo-class") // hidden-source-line
-public class SecurityConfig {
+public class SecurityConfig extends VaadinWebSecurity {
 
     @Value("${my.app.auth.secret}")
     private String authSecret;
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         // end::stateless-configure[]
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(antMatchers("/admin-only/**")).hasAnyRole("admin")
-                .requestMatchers(antMatchers("/public/**")).permitAll()
-        );
+        // Delegating the responsibility of general configurations
+        // of http security to the super class. It's configuring
+        // the followings: Vaadin's CSRF protection by ignoring
+        // framework's internal requests, null request cache,
+        // ignoring public views annotated with @AnonymousAllowed,
+        // restricting access to other views/endpoints, and enabling
+        // ViewAccessChecker authorization.
+        // You can add any possible extra configurations of your own
+        // here (the following is just an example):
+
+        // http.rememberMe().alwaysRemember(false);
+
+        // Configure your static resources with public access before calling
+        // super.configure(HttpSecurity) as it adds final anyRequest matcher
+        http.authorizeHttpRequests()
+                .requestMatchers(new AntPathRequestMatcher("/admin-only/**"))
+                .hasAnyRole("admin");
+        http.authorizeHttpRequests()
+                .requestMatchers(new AntPathRequestMatcher("/public/**"))
+                .permitAll();
 
         // tag::stateless-configure[]
+        super.configure(http);
+
         // Disable creating and using sessions in Spring Security
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Register your login view to the view access checker mechanism
-        http.with(VaadinSecurityConfigurer.vaadin(),
-                configurer -> configurer.loginView("/login"));
+        setLoginView(http, "/login");
 
         // Enable stateless authentication
-        http.with(new VaadinStatelessSecurityConfigurer<>(),
-                cfg -> cfg.withSecretKey().secretKey(
-                     new SecretKeySpec(Base64.getDecoder().decode(authSecret), // <1>
-                          JwsAlgorithms.HS256) // <2>
-               ).and().issuer("com.example.application") // <3>
+        setStatelessAuthentication(http,
+                new SecretKeySpec(Base64.getDecoder().decode(authSecret), // <1>
+                        JwsAlgorithms.HS256), // <2>
+                "com.example.application" // <3>
         );
-
-        return http.build();
     }
 }
 // end::stateless-configure[]
