bcba9fc9a9a90568da880d59f706ca0b662b52de
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-10-06 13:33:02 +0200

    chore: replace theme annotation with custom injection mechanism (#4635)
    * replace theme annotation
    
    * reuse docs styles from DSP bundle
    
    * move adding stylesheets to theme.ts
    
    * remove unnecessary user color definitions

--- frontend/demo/example-resources.ts
+++ frontend/demo/example-resources.ts
@@ -1,6 +1,7 @@
 import './init-flow-namespace';
-import { applyTheme } from 'Frontend/generated/theme';
-// See Vite config 'apply-theme-fallback'
+import { applyTheme } from 'Frontend/demo/theme';
+// Expose function to inject styles into shadow roots to web components exported from Flow.
+// See Flow application Vite config (apply-docs-theme plugin)
 // This file ends up in the DSP bundle
 // @ts-expect-error See vite.config.ts
 window.__applyTheme = { applyTheme };
