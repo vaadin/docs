1172bc64993e65585c888e595f8be93f000883e5
Herberts Markuns <herberts@vaadin.com>
2025-10-21 16:04:37 +0300

    Introduce Flex Layout page

---
+++ src/main/java/com/vaadin/demo/component/flexlayout/FlexLayoutDirection.java
@@ -0,0 +1,51 @@
+package com.vaadin.demo.component.flexlayout;
+
+import com.vaadin.demo.DemoExporter;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("flex-layout-direction")
+public class FlexLayoutDirection extends Div {
+
+    public FlexLayoutDirection() {
+        // tag::snippet[]
+        FlexLayout layout = new FlexLayout();
+        layout.setFlexDirection(FlexLayout.FlexDirection.COLUMN);
+        // end::snippet[]
+
+        Div item1 = new Div("Item 1");
+        item1.setClassName("example-item");
+
+        Div item2 = new Div("Item 2");
+        item2.setClassName("example-item");
+
+        Div item3 = new Div("Item 3");
+        item3.setClassName("example-item");
+
+        Div item4 = new Div("Item 4");
+        item4.setClassName("example-item");
+
+        Div item5 = new Div("Item 5");
+        item5.setClassName("example-item");
+
+        Div item6 = new Div("Item 6");
+        item6.setClassName("example-item");
+
+        layout.add(item1, item2, item3, item4, item5, item6);
+
+        layout.setWidthFull();
+        layout.getStyle().setGap("5px");
+
+        SplitLayout splitLayout = new SplitLayout(layout, new Div());
+        add(splitLayout);
+
+        this.setClassName("basic-layouts-example");
+        this.add(splitLayout);
+    }
+
+    public static class Exporter extends // hidden-source-line
+            DemoExporter<FlexLayoutDirection> { // hidden-source-line
+    } // hidden-source-line
+}
