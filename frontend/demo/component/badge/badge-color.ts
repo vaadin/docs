7b10aa7c6297d3210b04f601caede75bcb594be1
Herberts <80950643+HerbertsVaadin@users.noreply.github.com>
2025-11-03 10:35:18 +0200

    Add warning theme examples for Badges (#4718)

--- frontend/demo/component/badge/badge-color.ts
+++ frontend/demo/component/badge/badge-color.ts
@@ -21,12 +21,14 @@ export class Example extends LitElement {
         <vaadin-horizontal-layout theme="spacing">
           <span theme="badge">Pending</span>
           <span theme="badge success">Confirmed</span>
+          <span theme="badge warning">Warning</span>
           <span theme="badge error">Denied</span>
           <span theme="badge contrast">On hold</span>
         </vaadin-horizontal-layout>
         <vaadin-horizontal-layout theme="spacing">
           <span theme="badge primary">Pending</span>
           <span theme="badge success primary">Confirmed</span>
+          <span theme="badge warning primary">Warning</span>
           <span theme="badge error primary">Denied</span>
           <span theme="badge contrast primary">On hold</span>
         </vaadin-horizontal-layout>
