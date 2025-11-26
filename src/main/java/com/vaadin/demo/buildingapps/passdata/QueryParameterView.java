1a0d036ce7585d66870ea7397cafa150f2d8309f
Petter Holmstrom <petter@vaadin.com>
2025-11-26 17:42:40 +0200

    Edit Query Parameters (WIP)

---
+++ src/main/java/com/vaadin/demo/buildingapps/passdata/QueryParameterView.java
@@ -0,0 +1,21 @@
+package com.vaadin.demo.buildingapps.passdata;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.Route;
+
+// tag::snippet[]
+@Route("building-apps/pass-data/query-parameter")
+public class QueryParameterView extends VerticalLayout
+        implements BeforeEnterObserver {
+
+    // TODO Implement me
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'beforeEnter'");
+    }
+}
+// end::snippet[]
