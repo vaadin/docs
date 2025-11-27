2872f82a700173036ff5f16fe9145d6f426c0408
Petter Holmstrom <petter@vaadin.com>
2025-11-27 10:43:33 +0200

    Add Query Parameters example

--- src/main/java/com/vaadin/demo/buildingapps/passdata/QueryParameterView.java
+++ src/main/java/com/vaadin/demo/buildingapps/passdata/QueryParameterView.java
@@ -1,6 +1,8 @@
 package com.vaadin.demo.buildingapps.passdata;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
@@ -10,12 +12,29 @@ import com.vaadin.flow.router.Route;
 public class QueryParameterView extends VerticalLayout
         implements BeforeEnterObserver {
 
-    // TODO Implement me
+    private final TextField filterField;
+
+    QueryParameterView() {
+        filterField = new TextField("Filter");
+        add(filterField);
+
+        filterField.addValueChangeListener(event -> {
+            var queryParameters = UI.getCurrent()
+                    .getActiveViewLocation()
+                    .getQueryParameters()
+                    .merging("filter", event.getValue());
+            UI.getCurrent().navigate(
+                    QueryParameterView.class,
+                    queryParameters);
+        });
+    }
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'beforeEnter'");
+        event.getLocation()
+                .getQueryParameters()
+                .getSingleParameter("filter")
+                .ifPresent(filterField::setValue);
     }
 }
 // end::snippet[]
