0b5f7247121a4066d0e4b865b840bf4e253032c7
Petter Holmstrom <petter@vaadin.com>
2025-11-27 14:18:13 +0200

    Restructure

---
+++ src/main/java/com/vaadin/demo/buildingapps/routerlayout/MainLayout.java
@@ -0,0 +1,55 @@
+package com.vaadin.demo.buildingapps.routerlayout;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.dom.Style;
+import com.vaadin.flow.router.Layout;
+import com.vaadin.flow.server.menu.MenuConfiguration;
+import com.vaadin.flow.server.menu.MenuEntry;
+
+// tag::snippet[]
+@Layout("/building-apps/router-layout")
+public class MainLayout extends AppLayout {
+
+    MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addToDrawer(createHeader(), new Scroller(createSideNav()));
+    }
+
+    private Component createHeader() {
+        var appLogo = VaadinIcon.COGS.create();
+        appLogo.setColor("#009900");
+
+        var appName = new Span("My App");
+        appName.getStyle().setFontWeight(Style.FontWeight.BOLD);
+
+        var header = new VerticalLayout(appLogo, appName);
+        header.setAlignItems(FlexComponent.Alignment.CENTER);
+        return header;
+    }
+
+    private SideNav createSideNav() {
+        var nav = new SideNav();
+        MenuConfiguration.getMenuEntries()
+                .forEach(entry -> nav.addItem(createSideNavItem(entry)));
+        return nav;
+    }
+
+    private SideNavItem createSideNavItem(MenuEntry menuEntry) {
+        var item = new SideNavItem(menuEntry.title(), menuEntry.path());
+        item.setMatchNested(true);
+        if (menuEntry.icon() != null) {
+            item.setPrefixComponent(new Icon(menuEntry.icon()));
+        }
+        return item;
+    }
+}
+// end::snippet[]
