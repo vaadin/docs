71a2674391751e8d1417b9b4d77976a66c5ef8d3
Sergey Vinogradov <mr.vursen@gmail.com>
2025-11-28 14:03:41 +0400

    lumo/aura theme switcher

--- frontend/demo/foundation/lumo-tokens.ts
+++ frontend/demo/foundation/lumo-tokens.ts
@@ -1,5 +1,5 @@
-import propsStyles from '@vaadin/vaadin-lumo-styles/src/props/index.css?inline';
 import colorScheme from '@vaadin/vaadin-lumo-styles/src/global/color-scheme.css?inline';
+import propsStyles from '@vaadin/vaadin-lumo-styles/src/props/index.css?inline';
 import utilityStyles from '@vaadin/vaadin-lumo-styles/utility.css?inline';
 import { addStylesheet } from 'Frontend/demo/theme';
 
@@ -17,12 +17,12 @@ ${darkThemeProps.join('\n')}
 `;
 
 // Include styles in the page
-addStylesheet(propsStyles);
-addStylesheet(utilityStyles);
-addStylesheet(darkThemeStyles);
+// addStylesheet(propsStyles);
+// addStylesheet(utilityStyles);
+// addStylesheet(darkThemeStyles);
 
 // Notify DSP CustomPropertyPreview React component after custom properties have been loaded
-window.dispatchEvent(new CustomEvent('custom-properties-changed'));
+// window.dispatchEvent(new CustomEvent('custom-properties-changed'));
 
 // Dummy element so that this module can be included as a rendered example in a docs page
 export default class LumoTokens extends HTMLElement {}
--- frontend/demo/init.ts
+++ frontend/demo/init.ts
@@ -3,19 +3,19 @@ import './init-flow-namespace';
 import './init-flow-components';
 import '../generated/activate-vaadin-featureflags.js';
 import '../generated/vaadin-featureflags';
-import { applyTheme } from 'Frontend/demo/theme';
+// import { applyTheme } from 'Frontend/demo/theme';
 import client from 'Frontend/generated/connect-client.default';
 
 // Apply the theme to the document, so that notification styles work as expected,
 // as the notification container is added to the document body.
-applyTheme(document);
+// applyTheme(document);
 
 // @ts-expect-error Inserted by DS Publisher
 client.prefix = __VAADIN_CONNECT_PREFIX__; // eslint-disable-line no-undef
 
-document.body.style.setProperty('--docs-example-render-font-family', 'var(--lumo-font-family)');
-document.body.style.setProperty('--docs-example-render-color', 'var(--lumo-body-text-color)');
-document.body.style.setProperty('--docs-example-render-background-color', 'var(--lumo-base-color)');
+// document.body.style.setProperty('--docs-example-render-font-family', 'var(--lumo-font-family)');
+// document.body.style.setProperty('--docs-example-render-color', 'var(--lumo-body-text-color)');
+// document.body.style.setProperty('--docs-example-render-background-color', 'var(--lumo-base-color)');
 
 // Ensures standalone UI sample pags have a lang attribute
 document.documentElement.setAttribute('lang', 'en');
