7483ba7c1a97cb99523a0c5cb08a1291194204b5
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-12-01 11:33:20 +0100

    refactor: don't inject Lumo theme globally

--- frontend/demo/init.ts
+++ frontend/demo/init.ts
@@ -3,12 +3,7 @@ import './init-flow-namespace';
 import './init-flow-components';
 import '../generated/activate-vaadin-featureflags.js';
 import '../generated/vaadin-featureflags';
-import { applyTheme } from 'Frontend/demo/theme';
 import client from 'Frontend/generated/connect-client.default';
-
-// Apply the theme to the document, so that notification styles work as expected,
-// as the notification container is added to the document body.
-applyTheme(document);
 
 // @ts-expect-error Inserted by DS Publisher
 client.prefix = __VAADIN_CONNECT_PREFIX__; // eslint-disable-line no-undef
