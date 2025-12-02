b22c899dc333ef9d296c1119a7967823dd0b2664
web-padawan <iamkulykov@gmail.com>
2025-12-02 15:36:14 +0200

    docs: message-list Aura styling docs and properties

--- frontend/demo/component/messages/message-list-class-name.ts
+++ frontend/demo/component/messages/message-list-class-name.ts
@@ -4,10 +4,10 @@ import { format, subDays, subMinutes } from 'date-fns';
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { getPeople } from 'Frontend/demo/domain/DataService';
-import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 import { applyTheme } from 'Frontend/demo/theme';
+import type Person from 'Frontend/generated/com/vaadin/demo/domain/Person';
 
-@customElement('message-list-component-with-theme')
+@customElement('message-list-class-name')
 export class Example extends LitElement {
   private person: Person | undefined;
   private isoMinutes = 'yyyy-MM-dd HH:mm';
