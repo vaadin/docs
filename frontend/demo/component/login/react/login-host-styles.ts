7a4586971690f4bf3b6d333e34828b91a6537d7e
Diego Cardoso <diego@vaadin.com>
2025-12-09 14:26:16 +0100

    remove duplicated styles

--- frontend/demo/component/login/react/login-host-styles.ts
+++ frontend/demo/component/login/react/login-host-styles.ts
@@ -5,12 +5,10 @@ export const loginHostStyles = css`
     background-color: var(--lumo-contrast-5pct, rgba(0, 0, 0, 0.2));
     display: flex !important;
     justify-content: center;
-    padding: var(--lumo-space-l, var(--vaadin-padding-l));
     padding: var(--vaadin-padding-l);
   }
 
   vaadin-login-form {
-    background-color: var(--lumo-base-color, var(--vaadin-background-color));
     background-color: var(--vaadin-background-color);
   }
 `;
