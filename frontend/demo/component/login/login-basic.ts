22496c8e78493f741aa3332997168888a797b98f
Diego Cardoso <diego@vaadin.com>
2025-12-09 13:04:58 +0100

    docs: login Aura style variants
    - Adds theme switcher to the basic example
    - Adds listing with CSS properties for Login Form and Login Overlay
    
    Part of #4801

--- frontend/demo/component/login/login-basic.ts
+++ frontend/demo/component/login/login-basic.ts
@@ -8,10 +8,16 @@ import { applyTheme } from 'Frontend/demo/theme';
 export class Example extends LitElement {
   static override styles = css`
     :host {
-      background-color: var(--lumo-contrast-5pct);
+      background-color: var(--lumo-contrast-5pct, rgba(0, 0, 0, 0.2));
       display: flex !important;
       justify-content: center;
-      padding: var(--lumo-space-l);
+      padding: var(--lumo-space-l, var(--vaadin-padding-l));
+      padding: var(--vaadin-padding-l);
+    }
+
+    vaadin-login-form {
+      background-color: var(--lumo-base-color, var(--vaadin-background-color));
+      background-color: var(--vaadin-background-color);
     }
   `;
 
--- frontend/demo/component/login/react/login-host-styles.ts
+++ frontend/demo/component/login/react/login-host-styles.ts
@@ -2,9 +2,15 @@ import { css } from 'lit';
 
 export const loginHostStyles = css`
   :host {
-    background-color: var(--lumo-contrast-5pct);
+    background-color: var(--lumo-contrast-5pct, rgba(0, 0, 0, 0.2));
     display: flex !important;
     justify-content: center;
-    padding: var(--lumo-space-l);
+    padding: var(--lumo-space-l, var(--vaadin-padding-l));
+    padding: var(--vaadin-padding-l);
+  }
+
+  vaadin-login-form {
+    background-color: var(--lumo-base-color, var(--vaadin-background-color));
+    background-color: var(--vaadin-background-color);
   }
 `;
