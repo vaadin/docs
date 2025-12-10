7ffec873328fde58cd0238a93adba578868a4845
web-padawan <iamkulykov@gmail.com>
2025-12-10 13:20:41 +0200

    docs: Aura side nav style variants and properties

--- frontend/demo/component/side-nav/react/side-nav-example-styles.ts
+++ frontend/demo/component/side-nav/react/side-nav-example-styles.ts
@@ -3,13 +3,13 @@ import { css } from 'lit';
 const styles = css`
   .side-nav-sample {
     overflow: hidden;
-    background: var(--lumo-shade-5pct);
+    background: var(--lumo-shade-5pct, rgba(0, 0, 0, 0.2));
   }
 
   .side-nav-sample > * {
-    background: var(--lumo-base-color);
-    box-shadow: var(--lumo-box-shadow-s);
-    padding: var(--lumo-space-m);
+    background: var(--vaadin-background-color);
+    box-shadow: var(--lumo-box-shadow-s, 0 8px 24px -4px rgba(0, 0, 0, 0.3));
+    padding: var(--vaadin-padding-m);
     width: 15em;
   }
 `;
