8724eec52eb9da06321cb0f554871ff67bf63a98
web-padawan <iamkulykov@gmail.com>
2025-12-10 15:52:41 +0200

    refactor: cleanup error and warning variants examples

--- frontend/demo/component/notification/react/notification-error.tsx
+++ frontend/demo/component/notification/react/notification-error.tsx
@@ -1,55 +1,17 @@
-import '@vaadin/vaadin-lumo-styles/vaadin-iconset';
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React from 'react';
-import { useSignals } from '@preact/signals-react/runtime'; // hidden-source-line
-import { useSignal } from '@vaadin/hilla-react-signals';
-import { Button } from '@vaadin/react-components/Button.js';
-import { HorizontalLayout } from '@vaadin/react-components/HorizontalLayout.js';
-import { Icon } from '@vaadin/react-components/Icon.js';
 import { Notification } from '@vaadin/react-components/Notification.js';
 
 function Example() {
-  useSignals(); // hidden-source-line
-  const notificationOpened = useSignal(true);
+  // tag::snippet[]
+  const notification = Notification.show('Failed to generate report', {
+    position: 'middle',
+    duration: 0,
+    theme: 'error',
+  });
+  // end::snippet[]
 
-  return (
-    <>
-      <Button
-        disabled={notificationOpened.value}
-        onClick={() => {
-          notificationOpened.value = true;
-        }}
-      >
-        Show notification
-      </Button>
-
-      {/* tag::snippet[] */}
-      {/* The duration is set to 0-sec to prevent the notification from auto-close. */}
-      <Notification
-        theme="error"
-        duration={0}
-        position="middle"
-        opened={notificationOpened.value}
-        onClosed={() => {
-          notificationOpened.value = false;
-        }}
-      >
-        <HorizontalLayout theme="spacing" style={{ alignItems: 'center' }}>
-          <div>Failed to generate report</div>
-          <Button
-            theme="tertiary-inline"
-            onClick={() => {
-              notificationOpened.value = false;
-            }}
-            aria-label="Close"
-          >
-            <Icon icon="lumo:cross" />
-          </Button>
-        </HorizontalLayout>
-      </Notification>
-      {/* end::snippet[] */}
-    </>
-  );
+  return <></>;
 }
 
 export default reactExample(Example); // hidden-source-line
