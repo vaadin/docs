822b6451343286fc8461d0995a132e858fff163f
Serhii Kulykov <iamkulykov@gmail.com>
2025-12-10 17:59:55 +0200

    docs: notification variants and Aura style properties (#4954)

--- frontend/demo/component/notification/notification-warning.ts
+++ frontend/demo/component/notification/notification-warning.ts
@@ -1,76 +1,17 @@
 import 'Frontend/demo/init'; // hidden-source-line
-import '@vaadin/button';
-import '@vaadin/horizontal-layout';
-import '@vaadin/icon';
-import '@vaadin/notification';
-import '@vaadin/vaadin-lumo-styles/vaadin-iconset';
-import { css, html, LitElement } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
-import type { NotificationLitRenderer } from '@vaadin/notification/lit.js';
-import { notificationRenderer } from '@vaadin/notification/lit.js';
-import { applyTheme } from 'Frontend/demo/theme';
+import { LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { Notification } from '@vaadin/notification';
 
 @customElement('notification-warning')
 export class Example extends LitElement {
-  @state()
-  private notificationOpened = true;
-
-  protected override createRenderRoot() {
-    const root = super.createRenderRoot();
-    applyTheme(root);
-    return root;
-  }
-
-  static override styles = css`
-    :host {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: calc(100vh - var(--docs-space-l) * 2);
-    }
-  `;
-
-  protected override render() {
-    return html`
-      <vaadin-button .disabled="${this.notificationOpened}" @click="${this.open}">
-        Show notification
-      </vaadin-button>
-
-      <!-- tag::snippet[] -->
-      <!-- The duration is set to 0-sec to prevent the notification from auto-close. -->
-      <vaadin-notification
-        theme="warning"
-        duration="0"
-        position="middle"
-        .opened="${this.notificationOpened}"
-        @closed="${() => {
-          this.notificationOpened = false;
-        }}"
-        ${notificationRenderer(this.renderer, [])}
-      ></vaadin-notification>
-      <!-- end::snippet[] -->
-    `;
-  }
-
-  // tag::renderer[]
-  renderer: NotificationLitRenderer = () => html`
-    <vaadin-horizontal-layout theme="spacing" style="align-items: center;">
-      <div>
-        Your session will expire in 5 minutes due to inactivity.<br />
-        Close this warning to continue working.
-      </div>
-      <vaadin-button theme="tertiary-inline" @click="${this.close}" aria-label="Close">
-        <vaadin-icon icon="lumo:cross"></vaadin-icon>
-      </vaadin-button>
-    </vaadin-horizontal-layout>
-  `;
-  // end::renderer[]
-
-  private open() {
-    this.notificationOpened = true;
-  }
-
-  private close() {
-    this.notificationOpened = false;
+  firstUpdated() {
+    // tag::snippet[]
+    const notification = Notification.show('Your session is about to expire', {
+      position: 'middle',
+      duration: 0,
+      theme: 'warning',
+    });
+    // end::snippet[]
   }
 }
