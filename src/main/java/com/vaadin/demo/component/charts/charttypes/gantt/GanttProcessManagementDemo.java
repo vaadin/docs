8a7103d71c538aaa02045a537b2a0391c10c8e4a
Serhii Kulykov <iamkulykov@gmail.com>
2026-01-02 13:11:12 +0200

    chore: remove unnecessary copyright headers from Java examples (#5040)

--- src/main/java/com/vaadin/demo/component/charts/charttypes/gantt/GanttProcessManagementDemo.java
+++ src/main/java/com/vaadin/demo/component/charts/charttypes/gantt/GanttProcessManagementDemo.java
@@ -1,11 +1,3 @@
-/**
- * Copyright 2000-2024 Vaadin Ltd.
- *
- * This program is available under Vaadin Commercial License and Service Terms.
- *
- * See {@literal <https://vaadin.com/commercial-license-and-service-terms>} for the full
- * license.
- */
 package com.vaadin.demo.component.charts.charttypes.gantt;
 
 import com.vaadin.demo.DemoExporter; // hidden-source-line
