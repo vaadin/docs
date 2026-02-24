74ac9ad55894d6c3818b0f7c82134c753d194386
web-padawan <iamkulykov@gmail.com>
2026-02-24 15:10:10 +0200

    docs: add missing i18n to file format React example

--- frontend/demo/component/upload/react/upload-file-format.tsx
+++ frontend/demo/component/upload/react/upload-file-format.tsx
@@ -1,9 +1,21 @@
 import { reactExample } from 'Frontend/demo/react-example'; // hidden-source-line
 import React from 'react';
+import { useSignals } from '@preact/signals-react/runtime'; // hidden-source-line
+import { useSignal } from '@vaadin/hilla-react-signals';
 import { Notification } from '@vaadin/react-components/Notification.js';
 import { Upload } from '@vaadin/react-components/Upload.js';
 
 function Example() {
+  useSignals(); // hidden-source-line
+
+  const uploadI18n = useSignal({
+    addFiles: { one: 'Upload Report...' },
+    dropFiles: { one: 'Drop report here' },
+    error: {
+      incorrectFileType: 'The provided file does not have the correct format (PDF document).',
+    },
+  });
+
   return (
     <>
       {/* tag::snippet[] */}
@@ -14,6 +26,7 @@ function Example() {
       <Upload
         accept="application/pdf,.pdf"
         maxFiles={1}
+        i18n={uploadI18n.value}
         onFileReject={(event) => {
           Notification.show(event.detail.error, { position: 'middle', theme: 'error' });
         }}
