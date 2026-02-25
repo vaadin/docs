fe8c5e386a06f2d19fe4125024d55382fb9f48a4
ugur-vaadin <ugur@vaadin.com>
2026-02-25 15:02:40 +0200

    fix: add import ts files

---
+++ frontend/demo/component/aicomponents/ai-orchestrator-basic.ts
@@ -0,0 +1,5 @@
+import 'Frontend/demo/init';
+import '@vaadin/html';
+import '@vaadin/messages';
+
+// This file only has required imports for the Java-only AIOrchestratorBasic example
---
+++ frontend/demo/component/aicomponents/ai-orchestrator-programmatic-prompt.ts
@@ -0,0 +1,6 @@
+import 'Frontend/demo/init';
+import '@vaadin/button';
+import '@vaadin/html';
+import '@vaadin/messages';
+
+// This file only has required imports for the Java-only AIOrchestratorProgrammaticPrompt example
