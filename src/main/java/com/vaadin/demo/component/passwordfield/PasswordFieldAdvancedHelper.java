792eb8ebfcb88ac3f8220d25c6333006a98ff7de
Sergey Vinogradov <mr.vursen@gmail.com>
2026-02-27 15:56:14 +0400

    refactor: update PasswordField examples to support both Lumo and Aura

--- src/main/java/com/vaadin/demo/component/passwordfield/PasswordFieldAdvancedHelper.java
+++ src/main/java/com/vaadin/demo/component/passwordfield/PasswordFieldAdvancedHelper.java
@@ -8,59 +8,47 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.signals.Signal;
+import com.vaadin.flow.signals.local.ValueSignal;
 import com.vaadin.demo.DemoExporter; // hidden-source-line
 
 @Route("password-field-advanced-helper")
 public class PasswordFieldAdvancedHelper extends Div {
 
-    private Icon checkIcon;
-    private Span passwordStrengthText;
+    private final ValueSignal<String> password = new ValueSignal<>("");
+
+    private final Signal<String> strength = Signal.computed(() -> {
+        int passwordLength = password.get().length();
+        if (passwordLength > 9) {
+            return "strong";
+        } else if (passwordLength > 5) {
+            return "moderate";
+        } else {
+            return "weak";
+        }
+    });
 
     public PasswordFieldAdvancedHelper() {
         // tag::snippet[]
         PasswordField passwordField = new PasswordField();
         passwordField.setLabel("Password");
-        passwordField.setRevealButtonVisible(false);
 
-        checkIcon = VaadinIcon.CHECK.create();
-        checkIcon.setVisible(false);
-        checkIcon.getStyle().set("color", "var(--lumo-success-color)");
+        Icon checkIcon = VaadinIcon.CHECK.create();
+        checkIcon.setClassName("strong");
+        checkIcon.bindVisible(strength.map((value) -> value.equals("strong")));
         passwordField.setSuffixComponent(checkIcon);
 
-        Div passwordStrength = new Div();
-        passwordStrengthText = new Span();
-        passwordStrength.add(new Text("Password strength: "),
-                passwordStrengthText);
-        passwordField.setHelperComponent(passwordStrength);
-
-        add(passwordField);
-        // end::snippet[]
+        Span passwordStrength = new Span();
+        Signal.effect(passwordStrength, () -> {
+            passwordStrength.setClassName(strength.get());
+        });
+        passwordField.setHelperComponent(
+                new Div(new Text("Password strength: "), passwordStrength));
 
         passwordField.setValueChangeMode(ValueChangeMode.EAGER);
-        passwordField.addValueChangeListener(e -> {
-            String password = e.getValue();
-            updateHelper(password);
-        });
-
-        updateHelper("");
-    }
-
-    private void updateHelper(String password) {
-        if (password.length() > 9) {
-            passwordStrengthText.setText("strong");
-            passwordStrengthText.getStyle().set("color",
-                    "var(--lumo-success-color)");
-            checkIcon.setVisible(true);
-        } else if (password.length() > 5) {
-            passwordStrengthText.setText("moderate");
-            passwordStrengthText.getStyle().set("color", "#e7c200");
-            checkIcon.setVisible(false);
-        } else {
-            passwordStrengthText.setText("weak");
-            passwordStrengthText.getStyle().set("color",
-                    "var(--lumo-error-color)");
-            checkIcon.setVisible(false);
-        }
+        passwordField.bindValue(password, password::set);
+        add(passwordField);
+        // end::snippet[]
     }
 
     public static class Exporter extends // hidden-source-line
