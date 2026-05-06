6cb12dd4dab016129b8612f11cbf442ff249736b
Herberts Markuns <herberts@vaadin.com>
2026-05-06 10:46:51 +0300

    #4946 Add ValueChangeMode documentation and examples for EmailField

---
+++ src/main/java/com/vaadin/demo/component/emailfield/EmailFieldValueChangeMode.java
@@ -0,0 +1,44 @@
+package com.vaadin.demo.component.emailfield;
+
+import com.vaadin.demo.DemoExporter;
+import com.vaadin.demo.flow.routing.Route;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+
+@Route("email-field-value-change-mode")
+public class EmailFieldValueChangeMode extends VerticalLayout {
+
+    public EmailFieldValueChangeMode() {
+        setPadding(false);
+
+        // tag::snippet[]
+        var emailField = new EmailField("Email Field");
+        var modeSelector = new Select<>("Value Change Mode", ValueChangeMode.values());
+        modeSelector.setValue(emailField.getValueChangeMode());
+        modeSelector.addValueChangeListener(e -> {
+            emailField.clear();
+            emailField.setValueChangeMode(e.getValue());
+        });
+        var serverSideContent = new Span();
+        emailField.addValueChangeListener(e -> serverSideContent.setText(e.getValue()));
+        // end::snippet[]
+
+        modeSelector.setItemLabelGenerator(ValueChangeMode::name);
+
+        var horizontalLayout = new HorizontalLayout(emailField, modeSelector);
+        horizontalLayout.setAlignItems(Alignment.BASELINE);
+
+        var serverSideLayout = new HorizontalLayout(new Span("Server side:"), serverSideContent);
+
+        add(horizontalLayout, serverSideLayout);
+    }
+
+
+    public static class Exporter extends DemoExporter<EmailFieldValueChangeMode> { // hidden-source-line
+    } // hidden-source-line
+}
