8edbc6ee1a882e0a5e2a8c6ba85f42ee5f5c5826
web-padawan <iamkulykov@gmail.com>
2026-06-22 13:47:14 +0300

    chore: fix HTML closing tags formatting

--- frontend/demo/component/breadcrumbs/breadcrumbs-overflow.ts
+++ frontend/demo/component/breadcrumbs/breadcrumbs-overflow.ts
@@ -24,12 +24,12 @@ export class Example extends LitElement {
         <vaadin-breadcrumbs-item path="/">Home</vaadin-breadcrumbs-item>
         <vaadin-breadcrumbs-item path="/catalog">Catalog</vaadin-breadcrumbs-item>
         <vaadin-breadcrumbs-item path="/catalog/electronics">Electronics</vaadin-breadcrumbs-item>
-        <vaadin-breadcrumbs-item path="/catalog/electronics/computers"
-          >Computers</vaadin-breadcrumbs-item
-        >
-        <vaadin-breadcrumbs-item path="/catalog/electronics/computers/laptops"
-          >Laptops</vaadin-breadcrumbs-item
-        >
+        <vaadin-breadcrumbs-item path="/catalog/electronics/computers">
+          Computers
+        </vaadin-breadcrumbs-item>
+        <vaadin-breadcrumbs-item path="/catalog/electronics/computers/laptops">
+          Laptops
+        </vaadin-breadcrumbs-item>
         <vaadin-breadcrumbs-item>Model X1</vaadin-breadcrumbs-item>
       </vaadin-breadcrumbs>
       <!-- end::snippet[] -->
