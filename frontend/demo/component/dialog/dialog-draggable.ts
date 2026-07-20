f4e504c2e0a1f7bc392318aedb64b5f7ddb069c5
web-padawan <iamkulykov@gmail.com>
2026-07-20 15:36:43 +0300

    docs: replace Lit renderers with slots in dialog examples

--- frontend/demo/component/dialog/dialog-draggable.ts
+++ frontend/demo/component/dialog/dialog-draggable.ts
@@ -6,7 +6,6 @@ import '@vaadin/text-area';
 import '@vaadin/text-field';
 import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-import { dialogFooterRenderer, dialogHeaderRenderer, dialogRenderer } from '@vaadin/dialog/lit.js';
 import { applyTheme } from 'Frontend/demo/theme';
 
 @customElement('dialog-draggable')
@@ -31,34 +30,24 @@ export class Example extends LitElement {
         @closed="${() => {
           this.dialogOpened = false;
         }}"
-        ${dialogHeaderRenderer(
-          () => html`
-            <h2
-              class="draggable"
-              style="flex: 1; cursor: move; margin: 0; font-size: 1.5em; font-weight: bold; padding: var(--vaadin-gap-m) 0;"
-            >
-              Add note
-            </h2>
-          `,
-          []
-        )}
-        ${dialogRenderer(
-          () => html`
-            <vaadin-form-layout auto-responsive column-width="18rem" expand-fields>
-              <vaadin-text-field label="Title"></vaadin-text-field>
-              <vaadin-text-area label="Description"></vaadin-text-area>
-            </vaadin-form-layout>
-          `,
-          []
-        )}
-        ${dialogFooterRenderer(
-          () => html`
-            <vaadin-button @click="${this.close}">Cancel</vaadin-button>
-            <vaadin-button theme="primary" @click="${this.close}">Add note</vaadin-button>
-          `,
-          []
-        )}
-      ></vaadin-dialog>
+      >
+        <div slot="header-content">
+          <h2
+            class="draggable"
+            style="flex: 1; cursor: move; margin: 0; font-size: 1.5em; font-weight: bold; padding: var(--vaadin-gap-m) 0;"
+          >
+            Add note
+          </h2>
+        </div>
+        <vaadin-form-layout auto-responsive column-width="18rem" expand-fields>
+          <vaadin-text-field label="Title"></vaadin-text-field>
+          <vaadin-text-area label="Description"></vaadin-text-area>
+        </vaadin-form-layout>
+        <div slot="footer">
+          <vaadin-button @click="${this.close}">Cancel</vaadin-button>
+          <vaadin-button theme="primary" @click="${this.close}">Add note</vaadin-button>
+        </div>
+      </vaadin-dialog>
       <!-- end::snippet[] -->
       <vaadin-button @click="${this.open}">Show dialog</vaadin-button>
     `;
