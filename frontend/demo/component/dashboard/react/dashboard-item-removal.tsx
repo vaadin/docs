b7341b3626d510cc114fef08eb3c6130c9d9a07f
web-padawan <iamkulykov@gmail.com>
2026-08-17 15:15:10 +0300

    chore: use inline comments

--- frontend/demo/component/dashboard/react/dashboard-item-removal.tsx
+++ frontend/demo/component/dashboard/react/dashboard-item-removal.tsx
@@ -5,7 +5,7 @@ import type { DashboardItem, DashboardItemBeforeRemoveEvent } from '@vaadin/reac
 import { Dashboard } from '@vaadin/react-components-pro';
 
 export function Example() {
-  const items = useSignal<DashboardItem[]>([]); // Item definitions
+  const items = useSignal<DashboardItem[]>([/* Item definitions */]);
 
   const itemToRemove = useSignal<DashboardItem | null>(null);
 
